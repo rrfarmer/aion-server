c88d8414f1b8c7bb7098a95facf7e0ec0b96e898
rrfarmer <ryanfarmer@mac.com>
2026-06-09 00:25:18 -0400

    Port AbstractItemAction base (unblocks ItemActions + 33 subtypes); defer CraftingTask task-cone

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/AbstractItemAction.cs
@@ -0,0 +1,17 @@
+using System.Xml.Serialization;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/AbstractItemAction.</summary>
+[XmlType("AbstractItemAction")]
+public abstract class AbstractItemAction
+{
+    /// <summary>
+    /// Check if an item can be used. Returns true if act() can be called.
+    /// </summary>
+    public abstract bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params);
+
+    /// <summary>Performs the item action.</summary>
+    public abstract void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params);
+}
