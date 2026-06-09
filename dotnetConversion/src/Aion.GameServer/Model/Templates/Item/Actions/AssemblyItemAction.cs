fcc23d28076b94b12dbfbb42c9ba49adbddc7a61
rrfarmer <ryanfarmer@mac.com>
2026-06-09 00:54:27 -0400

    Port item-action leaves batch 9 (CosmeticItem/AssemblyItem)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/AssemblyItemAction.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/AssemblyItemAction.</summary>
+[XmlType("AssemblyItemAction")]
+public class AssemblyItemAction : AbstractItemAction
+{
+    [XmlAttribute("item")] private int item;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Model.Templates.Item.AssemblyItem assemblyItem = GetAssemblyItem();
+        if (assemblyItem == null)
+        {
+            return false;
+        }
+        foreach (int itemId in assemblyItem.GetParts())
+        {
+            if (player.GetInventory().GetFirstItemByItemId(itemId) == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId(), 1000, 0, 0), true);
+        ItemUseObserver observer = new AssemblyUseObserver(player, parentItem);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            player.GetObserveController().RemoveObserver(observer);
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            Aion.GameServer.Model.Templates.Item.AssemblyItem assemblyItem = GetAssemblyItem();
+            foreach (int itemId in assemblyItem.GetParts())
+            {
+                if (!player.GetInventory().DecreaseByItemId(itemId, 1))
+                {
+                    return ValueTask.CompletedTask;
+                }
+            }
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, 1, 0), true);
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_ASSEMBLY_ITEM_SUCCEEDED());
+            Aion.GameServer.Services.Item.ItemService.AddItem(player, assemblyItem.GetId(), 1);
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(1000)));
+    }
+
+    // Java parity: anonymous ItemUseObserver in act().
+    private sealed class AssemblyUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item parentItem;
+
+        public AssemblyUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem)
+        {
+            this.player = player;
+            this.parentItem = parentItem;
+        }
+
+        public override void Abort()
+        {
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            player.RemoveItemCoolDown(parentItem.GetItemTemplate().GetUseLimits().GetDelayId());
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_ITEM_CANCELED());
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, 2, 0), true);
+            player.GetObserveController().RemoveObserver(this);
+        }
+    }
+
+    public Aion.GameServer.Model.Templates.Item.AssemblyItem GetAssemblyItem()
+    {
+        return DataManager.ASSEMBLY_ITEM_DATA.GetAssemblyItem(item);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/CosmeticItemAction.cs
@@ -0,0 +1,85 @@
+using System.Xml.Serialization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/CosmeticItemAction.</summary>
+[XmlType("CosmeticItemAction")]
+public class CosmeticItemAction : AbstractItemAction
+{
+    private static readonly ILogger log = NullLogger.Instance;
+
+    [XmlAttribute("name")] private string cosmeticName;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Model.Templates.Cosmeticitems.CosmeticItemTemplate template = DataManager.COSMETIC_ITEMS_DATA.GetCosmeticItemsTemplate(cosmeticName);
+        if (template == null)
+        {
+            return false;
+        }
+        if (!template.GetRace().Equals(player.GetRace()))
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_CANNOT_USE_ITEM_INVALID_RACE());
+            return false;
+        }
+        if (!template.GetGenderPermitted().Equals("ALL"))
+        {
+            if (!player.GetGender().ToString().Equals(template.GetGenderPermitted()))
+            {
+                Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_CANNOT_USE_ITEM_INVALID_GENDER());
+                return false;
+            }
+        }
+        if (player.IsInPlayerMode(Aion.GameServer.Model.Actions.PlayerMode.RIDE))
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_RESTRICTION_RIDE());
+            return false;
+        }
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Model.Templates.Cosmeticitems.CosmeticItemTemplate template = DataManager.COSMETIC_ITEMS_DATA.GetCosmeticItemsTemplate(cosmeticName);
+        Aion.GameServer.Model.GameObjects.Player.PlayerAppearance playerAppearance = player.GetPlayerAppearance();
+        string type = template.GetType_();
+        int id = template.GetId();
+        switch (type)
+        {
+            case "hair_color": playerAppearance.SetHairRGB(id); break;
+            case "face_color": playerAppearance.SetSkinRGB(id); break;
+            case "lip_color": playerAppearance.SetLipRGB(id); break;
+            case "eye_color": playerAppearance.SetEyeRGB(id); break;
+            case "hair_type": playerAppearance.SetHair(id); break;
+            case "face_type": playerAppearance.SetFace(id); break;
+            case "voice_type": playerAppearance.SetVoice(id); break;
+            case "makeup_type": playerAppearance.SetTattoo(id); break;
+            case "tattoo_type": playerAppearance.SetDeco(id); break;
+            case "preset_name":
+            {
+                Aion.GameServer.Model.Templates.Cosmeticitems.CosmeticItemTemplate.Preset preset = template.GetPreset();
+                playerAppearance.SetEyeRGB(preset.GetEyeColor());
+                playerAppearance.SetLipRGB(preset.GetLipColor());
+                playerAppearance.SetHairRGB(preset.GetHairColor());
+                playerAppearance.SetSkinRGB(preset.GetEyeColor());
+                playerAppearance.SetHair(preset.GetHairType());
+                playerAppearance.SetFace(preset.GetFaceType());
+                playerAppearance.SetHeight(preset.GetScale());
+                player.GetAccountData().UpdateBoundingRadius();
+                break;
+            }
+            default:
+            {
+                log.LogWarning("Unhandled cosmetic item type: " + type);
+                return;
+            }
+        }
+        Aion.GameServer.Dao.PlayerAppearanceDAO.Store(player);
+        player.GetInventory().Delete(targetItem);
+        player.GetController().OnChangedPlayerAttributes();
+    }
+}
