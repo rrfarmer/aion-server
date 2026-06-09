f75ea14da1c9dc04a65386c2b3ce2258e497ce3d
rrfarmer <ryanfarmer@mac.com>
2026-06-08 22:13:23 -0400

    Port faithful PlayerAppearance (52 appearance fields + accessors, GetBoundHeight)

---
+++ dotnetConversion/src/Aion.GameServer/Model/GameObjects/Player/PlayerAppearance.cs
@@ -0,0 +1,596 @@
+namespace Aion.GameServer.Model.GameObjects.Player;
+
+/// <summary>Java parity: model/gameobjects/player/PlayerAppearance.</summary>
+public class PlayerAppearance
+{
+    private int face;
+    private int hair;
+    private int deco;
+    private int tattoo;
+    private int faceContour;
+    private int expression;
+    private int jawLine;
+    private int skinRGB;
+    private int hairRGB;
+    private int lipRGB;
+    private int eyeRGB;
+    private int faceShape;
+    private int forehead;
+    private int eyeHeight;
+    private int eyeSpace;
+    private int eyeWidth;
+    private int eyeSize;
+    private int eyeShape;
+    private int eyeAngle;
+    private int browHeight;
+    private int browAngle;
+    private int browShape;
+    private int nose;
+    private int noseBridge;
+    private int noseWidth;
+    private int noseTip;
+    private int cheek;
+    private int lipHeight;
+    private int mouthSize;
+    private int lipSize;
+    private int smile;
+    private int lipShape;
+    private int jawHeigh;
+    private int chinJut;
+    private int earShape;
+    private int headSize;
+    private int neck;
+    private int neckLength;
+    private int shoulders;
+    private int shoulderSize;
+    private int torso;
+    private int chest;
+    private int waist;
+    private int hips;
+    private int armThickness;
+    private int armLength;
+    private int handSize;
+    private int legThickness;
+    private int legLength;
+    private int footSize;
+    private int facialRate;
+    private int voice;
+    private float height;
+
+    public int GetFace()
+    {
+        return face;
+    }
+
+    public void SetFace(int face)
+    {
+        this.face = face;
+    }
+
+    public int GetHair()
+    {
+        return hair;
+    }
+
+    public void SetHair(int hair)
+    {
+        this.hair = hair;
+    }
+
+    public int GetDeco()
+    {
+        return deco;
+    }
+
+    public void SetDeco(int deco)
+    {
+        this.deco = deco;
+    }
+
+    public int GetTattoo()
+    {
+        return tattoo;
+    }
+
+    public void SetTattoo(int tattoo)
+    {
+        this.tattoo = tattoo;
+    }
+
+    public int GetFaceContour()
+    {
+        return faceContour;
+    }
+
+    public void SetFaceContour(int faceContour)
+    {
+        this.faceContour = faceContour;
+    }
+
+    public int GetExpression()
+    {
+        return expression;
+    }
+
+    public void SetExpression(int expression)
+    {
+        this.expression = expression;
+    }
+
+    public int GetJawLine()
+    {
+        return jawLine;
+    }
+
+    public void SetJawLine(int jawLine)
+    {
+        this.jawLine = jawLine;
+    }
+
+    public int GetSkinRGB()
+    {
+        return skinRGB;
+    }
+
+    public void SetSkinRGB(int skinRGB)
+    {
+        this.skinRGB = skinRGB;
+    }
+
+    public int GetHairRGB()
+    {
+        return hairRGB;
+    }
+
+    public void SetHairRGB(int hairRGB)
+    {
+        this.hairRGB = hairRGB;
+    }
+
+    public int GetLipRGB()
+    {
+        return lipRGB;
+    }
+
+    public void SetLipRGB(int lipRGB)
+    {
+        this.lipRGB = lipRGB;
+    }
+
+    public int GetEyeRGB()
+    {
+        return eyeRGB;
+    }
+
+    public void SetEyeRGB(int eyeRGB)
+    {
+        this.eyeRGB = eyeRGB;
+    }
+
+    public int GetFaceShape()
+    {
+        return faceShape;
+    }
+
+    public void SetFaceShape(int faceShape)
+    {
+        this.faceShape = faceShape;
+    }
+
+    public int GetForehead()
+    {
+        return forehead;
+    }
+
+    public void SetForehead(int forehead)
+    {
+        this.forehead = forehead;
+    }
+
+    public int GetEyeHeight()
+    {
+        return eyeHeight;
+    }
+
+    public void SetEyeHeight(int eyeHeight)
+    {
+        this.eyeHeight = eyeHeight;
+    }
+
+    public int GetEyeSpace()
+    {
+        return eyeSpace;
+    }
+
+    public void SetEyeSpace(int eyeSpace)
+    {
+        this.eyeSpace = eyeSpace;
+    }
+
+    public int GetEyeWidth()
+    {
+        return eyeWidth;
+    }
+
+    public void SetEyeWidth(int eyeWidth)
+    {
+        this.eyeWidth = eyeWidth;
+    }
+
+    public int GetEyeSize()
+    {
+        return eyeSize;
+    }
+
+    public void SetEyeSize(int eyeSize)
+    {
+        this.eyeSize = eyeSize;
+    }
+
+    public int GetEyeShape()
+    {
+        return eyeShape;
+    }
+
+    public void SetEyeShape(int eyeShape)
+    {
+        this.eyeShape = eyeShape;
+    }
+
+    public int GetEyeAngle()
+    {
+        return eyeAngle;
+    }
+
+    public void SetEyeAngle(int eyeAngle)
+    {
+        this.eyeAngle = eyeAngle;
+    }
+
+    public int GetBrowHeight()
+    {
+        return browHeight;
+    }
+
+    public void SetBrowHeight(int browHeight)
+    {
+        this.browHeight = browHeight;
+    }
+
+    public int GetBrowAngle()
+    {
+        return browAngle;
+    }
+
+    public void SetBrowAngle(int browAngle)
+    {
+        this.browAngle = browAngle;
+    }
+
+    public int GetBrowShape()
+    {
+        return browShape;
+    }
+
+    public void SetBrowShape(int browShape)
+    {
+        this.browShape = browShape;
+    }
+
+    public int GetNose()
+    {
+        return nose;
+    }
+
+    public void SetNose(int nose)
+    {
+        this.nose = nose;
+    }
+
+    public int GetNoseBridge()
+    {
+        return noseBridge;
+    }
+
+    public void SetNoseBridge(int noseBridge)
+    {
+        this.noseBridge = noseBridge;
+    }
+
+    public int GetNoseWidth()
+    {
+        return noseWidth;
+    }
+
+    public void SetNoseWidth(int noseWidth)
+    {
+        this.noseWidth = noseWidth;
+    }
+
+    public int GetNoseTip()
+    {
+        return noseTip;
+    }
+
+    public void SetNoseTip(int noseTip)
+    {
+        this.noseTip = noseTip;
+    }
+
+    public int GetCheek()
+    {
+        return cheek;
+    }
+
+    public void SetCheek(int cheek)
+    {
+        this.cheek = cheek;
+    }
+
+    public int GetLipHeight()
+    {
+        return lipHeight;
+    }
+
+    public void SetLipHeight(int lipHeight)
+    {
+        this.lipHeight = lipHeight;
+    }
+
+    public int GetMouthSize()
+    {
+        return mouthSize;
+    }
+
+    public void SetMouthSize(int mouthSize)
+    {
+        this.mouthSize = mouthSize;
+    }
+
+    public int GetLipSize()
+    {
+        return lipSize;
+    }
+
+    public void SetLipSize(int lipSize)
+    {
+        this.lipSize = lipSize;
+    }
+
+    public int GetSmile()
+    {
+        return smile;
+    }
+
+    public void SetSmile(int smile)
+    {
+        this.smile = smile;
+    }
+
+    public int GetLipShape()
+    {
+        return lipShape;
+    }
+
+    public void SetLipShape(int lipShape)
+    {
+        this.lipShape = lipShape;
+    }
+
+    public int GetJawHeigh()
+    {
+        return jawHeigh;
+    }
+
+    public void SetJawHeigh(int jawHeigh)
+    {
+        this.jawHeigh = jawHeigh;
+    }
+
+    public int GetChinJut()
+    {
+        return chinJut;
+    }
+
+    public void SetChinJut(int chinJut)
+    {
+        this.chinJut = chinJut;
+    }
+
+    public int GetEarShape()
+    {
+        return earShape;
+    }
+
+    public void SetEarShape(int earShape)
+    {
+        this.earShape = earShape;
+    }
+
+    public int GetHeadSize()
+    {
+        return headSize;
+    }
+
+    public void SetHeadSize(int headSize)
+    {
+        this.headSize = headSize;
+    }
+
+    public int GetNeck()
+    {
+        return neck;
+    }
+
+    public void SetNeck(int neck)
+    {
+        this.neck = neck;
+    }
+
+    public int GetNeckLength()
+    {
+        return neckLength;
+    }
+
+    public void SetNeckLength(int neckLength)
+    {
+        this.neckLength = neckLength;
+    }
+
+    public int GetShoulders()
+    {
+        return shoulders;
+    }
+
+    public void SetShoulders(int shoulders)
+    {
+        this.shoulders = shoulders;
+    }
+
+    public int GetShoulderSize()
+    {
+        return shoulderSize;
+    }
+
+    public void SetShoulderSize(int shoulderSize)
+    {
+        this.shoulderSize = shoulderSize;
+    }
+
+    public int GetTorso()
+    {
+        return torso;
+    }
+
+    public void SetTorso(int torso)
+    {
+        this.torso = torso;
+    }
+
+    public int GetChest()
+    {
+        return chest;
+    }
+
+    public void SetChest(int chest)
+    {
+        this.chest = chest;
+    }
+
+    public int GetWaist()
+    {
+        return waist;
+    }
+
+    public void SetWaist(int waist)
+    {
+        this.waist = waist;
+    }
+
+    public int GetHips()
+    {
+        return hips;
+    }
+
+    public void SetHips(int hips)
+    {
+        this.hips = hips;
+    }
+
+    public int GetArmThickness()
+    {
+        return armThickness;
+    }
+
+    public void SetArmThickness(int armThickness)
+    {
+        this.armThickness = armThickness;
+    }
+
+    public int GetArmLength()
+    {
+        return armLength;
+    }
+
+    public void SetArmLength(int armLength)
+    {
+        this.armLength = armLength;
+    }
+
+    public int GetHandSize()
+    {
+        return handSize;
+    }
+
+    public void SetHandSize(int handSize)
+    {
+        this.handSize = handSize;
+    }
+
+    public int GetLegThickness()
+    {
+        return legThickness;
+    }
+
+    public void SetLegThickness(int legThickness)
+    {
+        this.legThickness = legThickness;
+    }
+
+    public int GetLegLength()
+    {
+        return legLength;
+    }
+
+    public void SetLegLength(int legLength)
+    {
+        this.legLength = legLength;
+    }
+
+    public int GetFootSize()
+    {
+        return footSize;
+    }
+
+    public void SetFootSize(int footSize)
+    {
+        this.footSize = footSize;
+    }
+
+    public int GetFacialRate()
+    {
+        return facialRate;
+    }
+
+    public void SetFacialRate(int facialRate)
+    {
+        this.facialRate = facialRate;
+    }
+
+    public int GetVoice()
+    {
+        return voice;
+    }
+
+    public void SetVoice(int voice)
+    {
+        this.voice = voice;
+    }
+
+    /// <summary>character height is odd, but client needs it</summary>
+    public float GetHeight()
+    {
+        return height;
+    }
+
+    /// <summary>The approximate real height of the character.</summary>
+    public float GetBoundHeight()
+    {
+        return height * 1.75f;
+    }
+
+    public void SetHeight(float height)
+    {
+        this.height = height;
+    }
+}
