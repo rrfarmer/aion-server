05f9ffae7427ea0eda1162a37ce0aa709ab6583a
rrfarmer <ryanfarmer@mac.com>
2026-06-09 01:04:50 -0400

    Port item-action leaves batch 12 (InstanceTimeClear/ToyPetSpawn)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/InstanceTimeClear.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/InstanceTimeClear.</summary>
+[XmlType("InstanceTimeClear")]
+public class InstanceTimeClear : AbstractItemAction
+{
+    [XmlIgnore] private List<int> syncIds;
+
+    // Java parity: @XmlAttribute List<Integer> sync_ids — space-separated.
+    [XmlAttribute("sync_ids")]
+    public string SyncIdsXml
+    {
+        get => syncIds == null ? null : string.Join(" ", syncIds);
+        set
+        {
+            if (value == null) { syncIds = null; return; }
+            string[] parts = value.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            syncIds = new List<int>(parts.Length);
+            foreach (string p in parts)
+                syncIds.Add(int.Parse(p));
+        }
+    }
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        int syncId = (int)@params[0];
+        if (!syncIds.Contains(syncId))
+            return false;
+        int worldId = DataManager.INSTANCE_COOLTIME_DATA.GetWorldId(syncId);
+        Aion.GameServer.Model.GameObjects.Player.PortalCooldown portalCooldown = player.GetPortalCooldownList().GetPortalCooldown(worldId);
+        if (portalCooldown == null || (portalCooldown.GetReuseTime() < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() && portalCooldown.GetEnterCount() == 0))
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_CANT_INSTANCE_COOL_TIME_INIT());
+            return false;
+        }
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        int syncId = (int)@params[0];
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacketAndReceive(player,
+            new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId(), 1000, 0, 0));
+
+        ItemUseObserver observer = new InstanceTimeClearUseObserver(player, parentItem);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            player.GetObserveController().RemoveObserver(observer);
+            if (parentItem.GetActivationCount() > 1)
+            {
+                parentItem.SetActivationCount(parentItem.GetActivationCount() - 1);
+            }
+            else
+            {
+                player.GetInventory().DecreaseByObjectId(parentItem.GetObjectId(), 1);
+            }
+
+            int worldId = DataManager.INSTANCE_COOLTIME_DATA.GetWorldId(syncId);
+            Aion.GameServer.Model.GameObjects.Player.PortalCooldown portalCD = player.GetPortalCooldownList().GetPortalCooldown(worldId);
+            if (portalCD == null || portalCD.GetEnterCount() < 1)
+                return ValueTask.CompletedTask; // don't spam with not needed packets!
+
+            portalCD.DecreaseEnterCount();
+            if (portalCD.GetEnterCount() < 1)
+                player.GetPortalCooldownList().RemovePortalCooldown(worldId);
+
+            player.GetPortalCooldownList().SendEntryInfo(worldId);
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacketAndReceive(player,
+                new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId(), 0, 1, 0));
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(1000)));
+    }
+
+    // Java parity: anonymous ItemUseObserver in act().
+    private sealed class InstanceTimeClearUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item parentItem;
+
+        public InstanceTimeClearUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem)
+        {
+            this.player = player;
+            this.parentItem = parentItem;
+        }
+
+        public override void Abort()
+        {
+            // TODO: abort is invalid. Should we abort all or only the last syncid?
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            player.RemoveItemCoolDown(parentItem.GetItemTemplate().GetUseLimits().GetDelayId());
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_ITEM_CANCELED());
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+                new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, 2, 0), true);
+            player.GetObserveController().RemoveObserver(this);
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/ToyPetSpawnAction.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/ToyPetSpawnAction.</summary>
+[XmlType("ToyPetSpawnAction")]
+public class ToyPetSpawnAction : AbstractItemAction
+{
+    [XmlAttribute("npcid")] protected int npcid;
+    [XmlAttribute("time")] protected int time;
+
+    public int GetNpcId()
+    {
+        return npcid;
+    }
+
+    public int GetTime()
+    {
+        return time;
+    }
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        if (player.IsFlying())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_CANNOT_USE_BINDSTONE_ITEM_WHILE_FLYING());
+            return false;
+        }
+        if (player.IsInInstance())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_CANNOT_REGISTER_BINDSTONE_FAR_FROM_NPC());
+            return false;
+        }
+        if (Aion.GameServer.Services.KiskService.GetInstance().HaveKisk(player.GetObjectId()) && Aion.GameServer.Configs.Main.CustomConfig.ENABLE_KISK_RESTRICTION)
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_BINDSTONE_ALREADY_INSTALLED());
+            return false;
+        }
+        if (!IsPutKiskZone(player))
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_CANNOT_USE_ITEM_INVALID_LOCATION());
+            return false;
+        }
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        // ShowAction
+        player.GetController().CancelUseItem();
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+            new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId(), 10000, 0, 0), true);
+        ItemUseObserver observer = new ToyPetUseObserver(player, parentItem);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+                new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId(), 0, 1, 1), true);
+            player.GetObserveController().RemoveObserver(observer);
+            // RemoveKisk
+            if (!player.GetInventory().DecreaseByObjectId(parentItem.GetObjectId(), 1))
+                return ValueTask.CompletedTask;
+            float x = player.GetX();
+            float y = player.GetY();
+            float z = player.GetZ();
+            byte heading = (byte)((player.GetHeading() + 60) % 120);
+            int worldId = player.GetWorldId();
+            int instanceId = player.GetInstanceId();
+            Aion.GameServer.Model.Templates.Spawns.SpawnTemplate spawn = Aion.GameServer.Spawnengine.SpawnEngine.NewSingleTimeSpawn(worldId, npcid, x, y, z, heading);
+
+            Kisk kisk = Aion.GameServer.Spawnengine.VisibleObjectSpawner.SpawnKisk(spawn, instanceId, player);
+            int objOwnerId = player.GetObjectId();
+            // Schedule Despawn Action
+            Aion.GameServer.Utils.ScheduledTask task = Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct2 =>
+            {
+                kisk.GetController().Delete();
+                return ValueTask.CompletedTask;
+            }, TimeSpan.FromSeconds(kisk.GetRemainingLifetime()));
+            kisk.GetController().AddTask(Aion.GameServer.Model.TaskId.DESPAWN, task);
+
+            // ShowFinalAction
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            Aion.GameServer.Services.KiskService.GetInstance().RegKisk(kisk, objOwnerId);
+
+            if (kisk.GetMaxMembers() > 1)
+                kisk.GetController().OnDialogRequest(player);
+            else
+                Aion.GameServer.Services.KiskService.GetInstance().OnBind(kisk, player);
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(10000)));
+    }
+
+    private bool IsPutKiskZone(Aion.GameServer.Model.GameObjects.Player.Player player)
+    {
+        foreach (Aion.GameServer.World.Zone.ZoneInstance zone in player.FindZones())
+        {
+            if (!zone.CanPutKisk())
+                return false;
+        }
+        return true;
+    }
+
+    // Java parity: anonymous ItemUseObserver in act() (note: Java's abort does not removeObserver).
+    private sealed class ToyPetUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item parentItem;
+
+        public ToyPetUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem)
+        {
+            this.player = player;
+            this.parentItem = parentItem;
+        }
+
+        public override void Abort()
+        {
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            player.RemoveItemCoolDown(parentItem.GetItemTemplate().GetUseLimits().GetDelayId());
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_ITEM_CANCELED());
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+                new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, 2, 0), true);
+        }
+    }
+}
