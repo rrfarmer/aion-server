806138694e267a2c96fb3624b7f34bd905a00e6e
rrfarmer <ryanfarmer@mac.com>
2026-06-09 13:06:52 -0400

    Port TransferablePlayer + SecurityTokenService

---
+++ dotnetConversion/src/Aion.GameServer/Services/Player/SecurityTokenService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Security.Cryptography;
+using Aion.GameServer.Model.Account;
+
+namespace Aion.GameServer.Services.Player;
+
+/// <summary>Java parity: services/player/SecurityTokenService. SecureRandom→RandomNumberGenerator; Base64.getEncoder().encodeToString→Convert.ToBase64String.</summary>
+public class SecurityTokenService
+{
+    private SecurityTokenService()
+    {
+    }
+
+    public static void GenerateToken(Account account)
+    {
+        byte[] token = new byte[16];
+        using (RandomNumberGenerator secureRandom = RandomNumberGenerator.Create())
+        {
+            secureRandom.GetBytes(token);
+        }
+        account.SetSecurityToken(Convert.ToBase64String(token));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Services/Transfers/TransferablePlayer.cs
@@ -0,0 +1,21 @@
+using Aion.GameServer.Model.GameObjects.Player;
+
+namespace Aion.GameServer.Services.Transfers;
+
+/// <summary>Java parity: services/transfers/TransferablePlayer. Java byte → sbyte.</summary>
+public class TransferablePlayer
+{
+    public int playerId;
+    public int accountId;
+    public int targetAccountId;
+    public Player player;
+    public sbyte targetServerId;
+    public int taskId;
+
+    public TransferablePlayer(int playerId, int accountId, int targetAccountId)
+    {
+        this.playerId = playerId;
+        this.accountId = accountId;
+        this.targetAccountId = targetAccountId;
+    }
+}
