3a5a9ca76af2880f12dfbc28766a0d9bb72c37a2
rrfarmer <ryanfarmer@mac.com>
2026-06-09 13:30:31 -0400

    Port SiegeSchedules (private nested base widened to public per CS0060)

---
+++ dotnetConversion/src/Aion.GameServer/Configs/Schedule/SiegeSchedules.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Aion.GameServer.Configs.Schedule;
+
+/// <summary>Java parity: configs/schedule/SiegeSchedules.</summary>
+[XmlRoot("siege_schedule")]
+public class SiegeSchedules
+{
+    [XmlElement("fortress")] private List<Fortress> fortressesList;
+    [XmlElement("agent_fight")] private List<AgentFight> agentFights;
+
+    public List<Fortress> GetFortresses()
+    {
+        return fortressesList;
+    }
+
+    public List<AgentFight> GetAgentFights()
+    {
+        return agentFights;
+    }
+
+    // Java parity: private static abstract; C# CS0060 forbids a private base of public derived → widened to public.
+    public abstract class SiegeSchedule
+    {
+        [XmlAttribute("id")] private int id;
+        [XmlElement("siegeTime")] private List<string> siegeTimes;
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public List<string> GetSiegeTimes()
+        {
+            return siegeTimes;
+        }
+    }
+
+    [XmlRoot("fortress")]
+    public class Fortress : SiegeSchedule
+    {
+    }
+
+    [XmlRoot("agent_fight")]
+    public class AgentFight : SiegeSchedule
+    {
+    }
+
+    // Java parity: JAXBUtil.deserialize (deferred JAXB→XmlSerializer bridge, red).
+    public static SiegeSchedules Load()
+    {
+        return (SiegeSchedules) JAXBUtil.Deserialize(new FileInfo("./config/schedule/siege_schedule.xml"), typeof(SiegeSchedules));
+    }
+}
