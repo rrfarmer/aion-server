47b74962538f36f0040c6453a298bf21d4908009
rrfarmer <ryanfarmer@mac.com>
2026-06-09 00:48:50 -0400

    Port item-action leaves batch 7 (ExtractAction/ApExtractAction)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/ApExtractAction.cs
@@ -0,0 +1,116 @@
+using System.Xml.Serialization;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.Templates.Item.Enums;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/ApExtractAction.</summary>
+public class ApExtractAction : AbstractItemAction
+{
+    [XmlAttribute("target")] protected UseTarget target;
+    [XmlAttribute("rate")] protected float rate;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        if (targetItem == null || !targetItem.CanApExtract())
+            return false;
+        if (parentItem.GetItemTemplate().GetLevel() < targetItem.GetItemTemplate().GetLevel())
+            return false;
+        if (parentItem.GetItemTemplate().GetItemQuality() != targetItem.GetItemTemplate().GetItemQuality())
+            return false;
+
+        // TODO: ApExtractTarget.OTHER, ApExtractTarget.ALL. Find out what should go there
+
+        UseTarget? type = null;
+        switch (targetItem.GetItemTemplate().GetItemGroup())
+        {
+            case ItemGroup.SWORD:
+            case ItemGroup.DAGGER:
+            case ItemGroup.MACE:
+            case ItemGroup.ORB:
+            case ItemGroup.SPELLBOOK:
+            case ItemGroup.BOW:
+            case ItemGroup.GREATSWORD:
+            case ItemGroup.POLEARM:
+            case ItemGroup.STAFF:
+            case ItemGroup.HARP:
+            case ItemGroup.GUN:
+            case ItemGroup.KEYBLADE:
+            case ItemGroup.CANNON:
+                type = UseTarget.WEAPON;
+                break;
+            case ItemGroup.RB_TORSO:
+            case ItemGroup.RB_PANTS:
+            case ItemGroup.RB_SHOULDER:
+            case ItemGroup.RB_GLOVE:
+            case ItemGroup.RB_SHOES:
+            case ItemGroup.CL_TORSO:
+            case ItemGroup.CL_PANTS:
+            case ItemGroup.CL_SHOULDER:
+            case ItemGroup.CL_GLOVE:
+            case ItemGroup.CL_SHOES:
+            case ItemGroup.CH_TORSO:
+            case ItemGroup.CH_PANTS:
+            case ItemGroup.CH_SHOULDER:
+            case ItemGroup.CH_GLOVE:
+            case ItemGroup.CH_SHOES:
+            case ItemGroup.LT_TORSO:
+            case ItemGroup.LT_PANTS:
+            case ItemGroup.LT_SHOULDER:
+            case ItemGroup.LT_GLOVE:
+            case ItemGroup.LT_SHOES:
+            case ItemGroup.PL_TORSO:
+            case ItemGroup.PL_PANTS:
+            case ItemGroup.PL_SHOULDER:
+            case ItemGroup.PL_GLOVE:
+            case ItemGroup.PL_SHOES:
+            case ItemGroup.SHIELD:
+                type = UseTarget.ARMOR;
+                break;
+            case ItemGroup.NECKLACE:
+            case ItemGroup.EARRING:
+            case ItemGroup.RING:
+            case ItemGroup.BELT:
+            case ItemGroup.HEAD:
+                type = UseTarget.ACCESSORY;
+                break;
+            case ItemGroup.NONE:
+                if (targetItem.GetItemTemplate().GetItemGroup() == ItemGroup.WING)
+                {
+                    type = UseTarget.WING;
+                    break;
+                }
+                return false;
+            default:
+                return false;
+        }
+        return (target == UseTarget.EQUIPMENT || target == type);
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Model.Templates.Item.Acquisition acquisition = targetItem.GetItemTemplate().GetAcquisition();
+        if (acquisition == null || acquisition.GetRequiredAp() == 0)
+            return;
+        int ap = (int)(acquisition.GetRequiredAp() * rate);
+        Aion.GameServer.Model.Items.Storage.Storage inventory = player.GetInventory();
+
+        if (inventory.Delete(targetItem) != null)
+        {
+            if (inventory.DecreaseByObjectId(parentItem.GetObjectId(), 1))
+                Aion.GameServer.Services.Abyss.AbyssPointsService.AddAp(player, ap);
+        }
+        else
+            Aion.GameServer.Utils.Audit.AuditLogger.Log(player, "possibly using item AP extraction hack");
+    }
+
+    public UseTarget GetTarget()
+    {
+        return target;
+    }
+
+    public float GetRate()
+    {
+        return rate;
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/ExtractAction.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/ExtractAction.</summary>
+[XmlType("ExtractAction")]
+public class ExtractAction : AbstractItemAction
+{
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        if (targetItem == null)
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_DECOMPOSE_ITEM_NO_TARGET_ITEM());
+            return false;
+        }
+        if (!targetItem.GetItemTemplate().IsArmor() && !targetItem.GetItemTemplate().IsWeapon())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_DECOMPOSE_ITEM_IT_CAN_NOT_BE_DECOMPOSED(targetItem.GetL10n()));
+            return false;
+        }
+        if (targetItem.IsEquipped())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_DECOMPOSE_EQUIP_ITEM_CAN_NOT_BE_DECOMPOSED());
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 5000, 0, 0));
+        player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+        ItemUseObserver observer = new ExtractUseObserver(player, parentItem, targetItem);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            player.GetObserveController().RemoveObserver(observer);
+            bool result = Aion.GameServer.Services.EnchantService.BreakItem(player, targetItem, parentItem);
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, result ? 1 : 2, 0));
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(5000)));
+    }
+
+    // Java parity: anonymous ItemUseObserver in act().
+    private sealed class ExtractUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item parentItem;
+        private readonly Item targetItem;
+
+        public ExtractUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem)
+        {
+            this.player = player;
+            this.parentItem = parentItem;
+            this.targetItem = targetItem;
+        }
+
+        public override void Abort()
+        {
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_DECOMPOSE_ITEM_CANCELED(targetItem.GetL10n()));
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemTemplate().GetTemplateId(), 0, 2, 0));
+            player.GetObserveController().RemoveObserver(this);
+        }
+    }
+}
