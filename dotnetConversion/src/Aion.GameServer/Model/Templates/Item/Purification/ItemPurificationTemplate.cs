1c818b2a3f316090ed48cea3b161991d28a3af6d
rrfarmer <ryanfarmer@mac.com>
2026-06-09 09:18:47 -0400

    Port purification cone (PurificationResult + ItemPurificationTemplate)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Purification/ItemPurificationTemplate.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Aion.GameServer.Model.Templates.Item.Purification;
+
+/// <summary>Java parity: model/templates/item/purification/ItemPurificationTemplate (Ranastic, Navyan, Estrayl).</summary>
+[XmlRoot("ItemPurification")]
+public class ItemPurificationTemplate
+{
+    [XmlElement("purification_result")] private List<PurificationResult> purificationResults;
+    [XmlAttribute("base_item_id")] private int baseItemId;
+
+    public List<PurificationResult> GetPurificationResults()
+    {
+        return purificationResults;
+    }
+
+    public int GetBaseItemId()
+    {
+        return baseItemId;
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Purification/PurificationResult.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Aion.GameServer.Model.Templates.Item.Purification;
+
+/// <summary>Java parity: model/templates/item/purification/PurificationResult (Ranastic, Navyan, Estrayl).</summary>
+[XmlRoot("PurificationResult")]
+public class PurificationResult
+{
+    [XmlAttribute("result_item_id")] private int resultItemId;
+    [XmlAttribute("min_enchant_count")] private int minEnchantCount;
+    [XmlAttribute("necessary_abyss_points")] private int necessaryAbyssPoints;
+    [XmlAttribute("necessary_kinah")] private long necessaryKinah;
+    [XmlElement("req_material")] private List<RequiredMaterial> requiredMaterials;
+
+    public int GetResultItemId()
+    {
+        return resultItemId;
+    }
+
+    public int GetMinEnchantCount()
+    {
+        return minEnchantCount;
+    }
+
+    public int GetNecessaryAbyssPoints()
+    {
+        return necessaryAbyssPoints;
+    }
+
+    public long GetNecessaryKinah()
+    {
+        return necessaryKinah;
+    }
+
+    public List<RequiredMaterial> GetRequiredMaterials()
+    {
+        return requiredMaterials;
+    }
+}
