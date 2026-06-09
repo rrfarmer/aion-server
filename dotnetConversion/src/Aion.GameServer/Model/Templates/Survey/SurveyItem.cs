f4984ec6c488731d1f3384d39ab42dfa859ddfeb
rrfarmer <ryanfarmer@mac.com>
2026-06-09 06:51:29 -0400

    Port SurveyItem leaf; defer JAXBUtil (Tier-4 XML-load pillar)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Survey/SurveyItem.cs
@@ -0,0 +1,12 @@
+namespace Aion.GameServer.Model.Templates.Survey;
+
+/// <summary>Java parity: model/templates/survey/SurveyItem (KID).</summary>
+public class SurveyItem
+{
+    public int ownerId;
+    public int uniqueId;
+    public int itemId;
+    public long count;
+
+    public string html, radio;
+}
