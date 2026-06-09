a5291e3171a21e187141b7b5e5e7628590e862a7
rrfarmer <ryanfarmer@mac.com>
2026-06-09 09:09:37 -0400

    Port WindstreamTemplate (closes windstreams cone)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Windstreams/WindstreamTemplate.cs
@@ -0,0 +1,23 @@
+using System.Xml.Serialization;
+
+namespace Aion.GameServer.Model.Templates.Windstreams;
+
+/// <summary>Java parity: model/templates/windstreams/WindstreamTemplate (LokiReborn).</summary>
+[XmlType("WindFlight")]
+public class WindstreamTemplate
+{
+    [XmlElement("locations")] protected StreamLocations locations;
+    [XmlAttribute("mapid")] protected int mapid;
+
+    /// <summary>Gets the value of the locations property.</summary>
+    public StreamLocations GetLocations()
+    {
+        return locations;
+    }
+
+    /// <summary>Gets the value of the mapid property.</summary>
+    public int GetMapId()
+    {
+        return mapid;
+    }
+}
