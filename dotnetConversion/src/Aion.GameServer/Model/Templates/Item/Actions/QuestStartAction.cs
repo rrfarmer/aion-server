1eab82b620b48281066e96029690d98897681c9f
rrfarmer <ryanfarmer@mac.com>
2026-06-09 00:42:02 -0400

    Port item-action leaves batch 5 (QuestStartAction/SkillLearnAction)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/QuestStartAction.cs
@@ -0,0 +1,32 @@
+using System.Xml.Serialization;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/QuestStartAction.</summary>
+[XmlType("QuestStartAction")]
+public class QuestStartAction : AbstractItemAction
+{
+    [XmlAttribute("questid")] protected int questid;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Questengine.Model.QuestState qs = player.GetQuestStateList().GetQuestState(questid);
+        if (qs == null || qs.IsStartable())
+            return true;
+        else if (qs.GetStatus() != Aion.GameServer.Questengine.Model.QuestStatus.COMPLETE)
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_QUEST_ACQUIRE_ERROR_WORKING_QUEST());
+        else if (!qs.CanRepeat())
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player,
+                Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_QUEST_ACQUIRE_ERROR_NONE_REPEATABLE(DataManager.QUEST_DATA.GetQuestById(questid).GetName()));
+
+        return false;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacketAndReceive(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), parentItem.GetItemId()));
+        Aion.GameServer.Questengine.QuestEngine.GetInstance().OnDialog(new Aion.GameServer.Questengine.Model.QuestEnv(null, player, questid, Aion.GameServer.Model.DialogAction.ASK_QUEST_ACCEPT));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/SkillLearnAction.cs
@@ -0,0 +1,56 @@
+using System.Xml.Serialization;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/SkillLearnAction.</summary>
+[XmlType("SkillLearnAction")]
+public class SkillLearnAction : AbstractItemAction
+{
+    [XmlAttribute("skillid")] protected int skillid;
+    [XmlAttribute("level")] protected int level;
+    [XmlAttribute("class")] protected PlayerClass? playerClass;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        // 1. check player level
+        if (player.GetCommonData().GetLevel() < level)
+            return false;
+
+        PlayerClass pc = player.GetCommonData().GetPlayerClass();
+        if (!ValidateClass(pc))
+            return false;
+
+        // 4. check player race and Race.PC_ALL
+        Race race = parentItem.GetItemTemplate().GetRace();
+        if (player.GetRace() != race && race != Race.PC_ALL)
+            return false;
+        // 5. check whether this skill is already learned
+        if (player.GetSkillList().IsSkillPresent(skillid))
+            return false;
+
+        return true;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        // item animation and message
+        Aion.GameServer.Model.Templates.Item.ItemTemplate itemTemplate = parentItem.GetItemTemplate();
+        player.GetController().CancelUseItem();
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+            new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), itemTemplate.GetTemplateId()), true);
+
+        // add skill
+        Aion.GameServer.Services.SkillLearnService.LearnSkillBook(player, skillid);
+
+        // remove book from inventory (assuming it's not stackable)
+        Item item = player.GetInventory().GetItemByObjId(parentItem.GetObjectId());
+        player.GetInventory().Delete(item);
+    }
+
+    private bool ValidateClass(PlayerClass pc)
+    {
+        return playerClass == null || playerClass == pc || playerClass == pc.GetStartingClass();
+    }
+}
