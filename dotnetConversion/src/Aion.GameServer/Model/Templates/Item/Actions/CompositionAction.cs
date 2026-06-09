0cb0063ebce8d64ae0818d43f8485b10bba362be
rrfarmer <ryanfarmer@mac.com>
2026-06-09 00:57:35 -0400

    Port item-action leaves batch 10 (Composition/Tuning)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/CompositionAction.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/CompositionAction.</summary>
+[XmlType("CompositionAction")]
+public class CompositionAction : AbstractItemAction
+{
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        return false;
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+    }
+
+    public bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item tools, Item first, Item second)
+    {
+        if (!tools.GetItemTemplate().IsCombinationItem())
+            return false;
+
+        if (!first.GetItemTemplate().IsEnchantmentStone())
+            return false;
+
+        if (!second.GetItemTemplate().IsEnchantmentStone())
+            return false;
+
+        if (first.GetItemCount() < 1 || second.GetItemCount() < 1)
+            return false;
+
+        if (first.GetItemTemplate().GetLevel() > 95 || second.GetItemTemplate().GetLevel() > 95)
+            return false;
+
+        return true;
+    }
+
+    public void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item tools, Item first, Item second)
+    {
+        Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), tools.GetObjectId(), tools.GetItemTemplate().GetTemplateId(), 5000, 0, 0));
+        player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+
+        ItemUseObserver observer = new CompositionUseObserver(player, tools);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            player.GetObserveController().RemoveObserver(observer);
+            bool result = player.GetInventory().DecreaseByItemId(tools.GetItemId(), 1);
+            bool result1 = player.GetInventory().DecreaseByItemId(first.GetItemId(), 1);
+            bool result2 = player.GetInventory().DecreaseByItemId(second.GetItemId(), 1);
+            if (result && result1 && result2)
+            {
+                Aion.GameServer.Services.Item.ItemService.AddItem(player, GetItemId(CalcLevel(first.GetItemTemplate().GetLevel(), second.GetItemTemplate().GetLevel())), 1);
+            }
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), tools.GetObjectId(), tools.GetItemTemplate().GetTemplateId(), 0, 1, 0));
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(5000)));
+    }
+
+    private int CalcLevel(int first, int second)
+    {
+        int value = ((first + second) / 2);
+        if (value < 11)
+        {
+            value = Aion.Commons.Utils.Rnd.Get(1, 20);
+        }
+        else
+        {
+            int random = Aion.Commons.Utils.Rnd.Get(1, 10);
+            int bit = Aion.Commons.Utils.Rnd.Get(0, 1);
+            value = (bit == 0 ? value - random : value + random);
+        }
+        return value;
+    }
+
+    public int GetItemId(int value)
+    {
+        return 166000000 + value;
+    }
+
+    // Java parity: anonymous ItemUseObserver in act().
+    private sealed class CompositionUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item tools;
+
+        public CompositionUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item tools)
+        {
+            this.player = player;
+            this.tools = tools;
+        }
+
+        public override void Abort()
+        {
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), tools.GetObjectId(), tools.GetItemTemplate().GetTemplateId(), 0, 2, 0));
+            player.GetObserveController().RemoveObserver(this);
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Templates/Item/Actions/TuningAction.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Observer;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects;
+
+namespace Aion.GameServer.Model.Templates.Item.Actions;
+
+/// <summary>Java parity: model/templates/item/actions/TuningAction.</summary>
+[XmlType("TuningAction")]
+public class TuningAction : AbstractItemAction
+{
+    [XmlAttribute("target")] private UseTarget target;
+    [XmlAttribute("no_reduce")] private bool shouldNotReduceTuneCount;
+
+    public override bool CanAct(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        if (targetItem.IsEquipped())
+            return false;
+        if (!targetItem.IsIdentified())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_DIDNT_IDENTIFY(targetItem.GetL10n()));
+            return false;
+        }
+        if (!targetItem.GetItemTemplate().CanTune())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_CANNOT_REIDENTIFY(targetItem.GetL10n()));
+            return false;
+        }
+        if (target == UseTarget.WEAPON && !targetItem.GetItemTemplate().IsWeapon()
+            || target == UseTarget.ARMOR && !targetItem.GetItemTemplate().IsArmor())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_WRONG_SELECT(parentItem.GetL10n(), targetItem.GetL10n()));
+            return false;
+        }
+        if (targetItem.GetItemTemplate().GetLevel() > parentItem.GetItemTemplate().GetLevel())
+        {
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_WRONG_LEVEL(parentItem.GetL10n(), targetItem.GetL10n()));
+            return false;
+        }
+
+        return shouldNotReduceTuneCount || targetItem.GetTuneCount() < targetItem.GetItemTemplate().GetMaxTuneCount();
+    }
+
+    public override void Act(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, params object[] @params)
+    {
+        int tuningScrollItemId = parentItem.GetItemId();
+        int tuningScrollObjectId = parentItem.GetObjectId();
+        Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player,
+            new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), parentItem.GetObjectId(), tuningScrollItemId, 5000, 12, 0), true);
+        ItemUseObserver observer = new TuneUseObserver(player, parentItem, targetItem, tuningScrollItemId, tuningScrollObjectId);
+        player.GetObserveController().Attach(observer);
+        player.GetController().AddTask(Aion.GameServer.Model.TaskId.ITEM_USE, Aion.GameServer.Utils.ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            player.GetObserveController().RemoveObserver(observer);
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), tuningScrollObjectId, tuningScrollItemId, 0, 13, 0), true);
+            if (!player.GetInventory().DecreaseByObjectId(tuningScrollObjectId, 1))
+                return ValueTask.CompletedTask;
+
+            int newOptionalSockets, newEnchantBonus, newStatBonusId;
+            if (shouldNotReduceTuneCount) // only tune attributes (bonus stats)
+            {
+                newOptionalSockets = targetItem.GetOptionalSockets();
+                newEnchantBonus = targetItem.GetEnchantBonus();
+            }
+            else
+            {
+                targetItem.SetTuneCount(targetItem.GetTuneCount() + 1);
+                player.GetInventory().SetPersistentState(IPersistable.PersistentState.UPDATE_REQUIRED);
+                newOptionalSockets = Aion.Commons.Utils.Rnd.Get(0, targetItem.GetItemTemplate().GetOptionSlotBonus());
+                newEnchantBonus = Aion.Commons.Utils.Rnd.Get(0, targetItem.GetItemTemplate().GetMaxEnchantBonus());
+            }
+            newStatBonusId = GetRandomStatBonusIdFor(targetItem);
+            Aion.GameServer.Model.Items.PendingTuneResult result = new Aion.GameServer.Model.Items.PendingTuneResult(newOptionalSockets, newEnchantBonus, newStatBonusId, shouldNotReduceTuneCount);
+            targetItem.SetPendingTuneResult(result);
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmTuneResult(targetItem, tuningScrollItemId, result));
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_SUCCEED(targetItem.GetL10n()));
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(5000)));
+    }
+
+    public static int GetRandomStatBonusIdFor(Item item)
+    {
+        return DataManager.ITEM_RANDOM_BONUSES.SelectRandomBonusNumber(Aion.GameServer.Model.Templates.Item.Bonuses.StatBonusType.INVENTORY, item.GetItemTemplate().GetStatBonusSetId());
+    }
+
+    // Java parity: anonymous ItemUseObserver in act().
+    private sealed class TuneUseObserver : ItemUseObserver
+    {
+        private readonly Aion.GameServer.Model.GameObjects.Player.Player player;
+        private readonly Item parentItem;
+        private readonly Item targetItem;
+        private readonly int tuningScrollItemId;
+        private readonly int tuningScrollObjectId;
+
+        public TuneUseObserver(Aion.GameServer.Model.GameObjects.Player.Player player, Item parentItem, Item targetItem, int tuningScrollItemId, int tuningScrollObjectId)
+        {
+            this.player = player;
+            this.parentItem = parentItem;
+            this.targetItem = targetItem;
+            this.tuningScrollItemId = tuningScrollItemId;
+            this.tuningScrollObjectId = tuningScrollObjectId;
+        }
+
+        public override void Abort()
+        {
+            player.GetController().CancelTask(Aion.GameServer.Model.TaskId.ITEM_USE);
+            player.RemoveItemCoolDown(parentItem.GetItemTemplate().GetUseLimits().GetDelayId());
+            Aion.GameServer.Utils.PacketSendUtility.SendPacket(player, Aion.GameServer.Network.Aion.ServerPackets.SmSystemMessage.STR_MSG_ITEM_REIDENTIFY_CANCELED(targetItem.GetL10n()));
+            Aion.GameServer.Utils.PacketSendUtility.BroadcastPacket(player, new Aion.GameServer.Network.Aion.ServerPackets.SmItemUsageAnimation(player.GetObjectId(), tuningScrollObjectId, tuningScrollItemId, 0, 14, 0), true);
+            player.GetObserveController().RemoveObserver(this);
+        }
+    }
+}
