e84c9b48a5e774c01cdec03c207ea1ca7b4524bb
rrfarmer <ryanfarmer@mac.com>
2026-06-09 02:20:30 -0400

    Port TeamMoveUpdater (FIFO group/alliance movement updater)

---
+++ dotnetConversion/src/Aion.GameServer/Taskmanager/Tasks/TeamMoveUpdater.cs
@@ -0,0 +1,45 @@
+using Aion.GameServer.Taskmanager;
+
+namespace Aion.GameServer.Taskmanager.Tasks;
+
+/// <summary>
+/// Supports PlayerGroup and PlayerAlliance movement updating.
+/// Java parity: taskmanager/tasks/TeamMoveUpdater (Sarynth).
+/// </summary>
+public sealed class TeamMoveUpdater : AbstractFIFOPeriodicTaskManager<Aion.GameServer.Model.GameObjects.Player.Player>
+{
+    private static class SingletonHolder
+    {
+        internal static readonly TeamMoveUpdater INSTANCE = new TeamMoveUpdater();
+    }
+
+    public static TeamMoveUpdater GetInstance()
+    {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public TeamMoveUpdater()
+        : base(2000)
+    {
+    }
+
+    protected override void CallTask(Aion.GameServer.Model.GameObjects.Player.Player player)
+    {
+        if (player.IsOnline())
+        {
+            if (player.IsInGroup())
+            {
+                Aion.GameServer.Model.Team.Group.PlayerGroupService.UpdateGroup(player, Aion.GameServer.Model.Team.Common.Legacy.GroupEvent.MOVEMENT);
+            }
+            else if (player.IsInAlliance())
+            {
+                Aion.GameServer.Model.Team.Alliance.PlayerAllianceService.UpdateAlliance(player, Aion.GameServer.Model.Team.Common.Legacy.PlayerAllianceEvent.MOVEMENT);
+            }
+        }
+    }
+
+    protected override string GetCalledMethodName()
+    {
+        return "teamMoveUpdate()";
+    }
+}
