95bde39f725867e8f42ecf7ba3b56bb4a3033be3
rrfarmer <ryanfarmer@mac.com>
2026-06-09 12:11:40 -0400

    Port gameobjects GroupGate (gameobjects model cone complete)

---
+++ dotnetConversion/src/Aion.GameServer/Model/GameObjects/GroupGate.cs
@@ -0,0 +1,22 @@
+using Aion.GameServer.Controllers;
+using Aion.GameServer.Controllers.Effect;
+using Aion.GameServer.Model.Templates.Spawns;
+using Aion.GameServer.World.Knownlist;
+
+namespace Aion.GameServer.Model.GameObjects;
+
+/// <summary>Java parity: model/gameobjects/GroupGate.</summary>
+public class GroupGate : SummonedObject<Creature>
+{
+    public GroupGate(NpcController controller, SpawnTemplate spawnTemplate, Creature creator)
+        : base(controller, spawnTemplate, (sbyte) 1, creator)
+    {
+        SetKnownlist(new PlayerAwareKnownList(this));
+        SetEffectController(new EffectController(this));
+    }
+
+    public override NpcObjectType GetNpcObjectType()
+    {
+        return NpcObjectType.GROUPGATE;
+    }
+}
