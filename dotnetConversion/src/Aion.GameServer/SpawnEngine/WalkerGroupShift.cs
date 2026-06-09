3a1d927866e7db003dac4fca2bb5ca61da07fc6f
rrfarmer <ryanfarmer@mac.com>
2026-06-09 16:11:22 -0400

    Port WalkerGroupShift (spawnengine clean leaf)

---
+++ dotnetConversion/src/Aion.GameServer/SpawnEngine/WalkerGroupShift.cs
@@ -0,0 +1,34 @@
+namespace Aion.GameServer.Spawnengine;
+
+/// <summary>Java parity: spawnengine/WalkerGroupShift (Rolandas).</summary>
+public class WalkerGroupShift
+{
+    private float sagittalShift; // left and right (sides)
+    private float coronalShift; // or dorsoventral (back and front)
+
+    public const float DISTANCE = 2; // 2 meters distance by default
+
+    public WalkerGroupShift(float leftRight, float backFront)
+    {
+        sagittalShift = leftRight;
+        coronalShift = backFront;
+    }
+
+    /// <summary>left and right (sides)</summary>
+    public float GetSagittalShift()
+    {
+        return sagittalShift;
+    }
+
+    /// <summary>dorsoventral (back and front)</summary>
+    public float GetCoronalShift()
+    {
+        return coronalShift;
+    }
+
+    public void Set(WalkerGroupShift shift)
+    {
+        this.sagittalShift = shift.sagittalShift;
+        this.coronalShift = shift.coronalShift;
+    }
+}
