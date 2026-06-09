8de98dfc5eb5a1092d98d024a6b39df50a5e0e24
rrfarmer <ryanfarmer@mac.com>
2026-06-09 14:14:43 -0400

    Port ActionItemUseOperation (xmlQuest operations cone fully complete)

---
+++ dotnetConversion/src/Aion.GameServer/Questengine/Handlers/Models/XmlQuest/Operations/ActionItemUseOperation.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion.ServerPackets;
+using Aion.GameServer.Questengine.Model;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Questengine.Handlers.Models.XmlQuest.Operations;
+
+/// <summary>Java parity: .../operations/ActionItemUseOperation. Anonymous Runnable → Schedule(ct-lambda).</summary>
+[XmlType("ActionItemUseOperation")]
+public class ActionItemUseOperation : QuestOperation
+{
+    [XmlElement("finish")] protected QuestOperations finish;
+
+    public override void DoOperate(QuestEnv env)
+    {
+        Player player = env.GetPlayer();
+        Npc npc;
+        if (env.GetVisibleObject() is Npc visibleNpc)
+            npc = visibleNpc;
+        else
+            return;
+        int defaultUseTime = 3000;
+        PacketSendUtility.SendPacket(player, new SM_USE_OBJECT(player.GetObjectId(), npc.GetObjectId(), defaultUseTime, 1));
+        PacketSendUtility.BroadcastPacket(player, new SM_EMOTION(player, EmotionType.START_QUESTLOOT, 0, npc.GetObjectId()), true);
+        ThreadPoolManager.GetInstance().Schedule(ct =>
+        {
+            PacketSendUtility.SendPacket(player, new SM_USE_OBJECT(player.GetObjectId(), npc.GetObjectId(), defaultUseTime, 0));
+            finish.Operate(env);
+            return ValueTask.CompletedTask;
+        }, TimeSpan.FromMilliseconds(defaultUseTime));
+    }
+}
