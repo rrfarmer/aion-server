45a623300f66c2401e1a8482889fdb235662dc35
rrfarmer <ryanfarmer@mac.com>
2026-06-09 13:19:32 -0400

    Port PricesConfig + InGameShopConfig

---
+++ dotnetConversion/src/Aion.GameServer/Configs/Main/InGameShopConfig.cs
@@ -0,0 +1,14 @@
+namespace Aion.GameServer.Configs.Main;
+
+/// <summary>Java parity: configs/main/InGameShopConfig.</summary>
+public static class InGameShopConfig
+{
+    /// <summary>Property key: gameserver.ingameshop.enable</summary>
+    public static bool ENABLE_IN_GAME_SHOP = false;
+
+    /// <summary>Property key: gameserver.ingameshop.gift</summary>
+    public static bool ENABLE_GIFT_OTHER_RACE = false;
+
+    /// <summary>Property key: gameserver.ingameshop.allow.gift</summary>
+    public static bool ALLOW_GIFTS = true;
+}
---
+++ dotnetConversion/src/Aion.GameServer/Configs/Main/PricesConfig.cs
@@ -0,0 +1,20 @@
+namespace Aion.GameServer.Configs.Main;
+
+/// <summary>Java parity: configs/main/PricesConfig.</summary>
+public static class PricesConfig
+{
+    /// <summary>Property key: gameserver.prices.default.prices</summary>
+    public static int DEFAULT_PRICES = 100;
+
+    /// <summary>Property key: gameserver.prices.default.modifier</summary>
+    public static int DEFAULT_MODIFIER = 100;
+
+    /// <summary>Property key: gameserver.prices.default.taxes</summary>
+    public static int DEFAULT_TAXES = 100;
+
+    /// <summary>Property key: gameserver.prices.vendor.buymod</summary>
+    public static int VENDOR_BUY_MODIFIER = 100;
+
+    /// <summary>Property key: gameserver.prices.vendor.sellmod</summary>
+    public static int VENDOR_SELL_MODIFIER = 20;
+}
