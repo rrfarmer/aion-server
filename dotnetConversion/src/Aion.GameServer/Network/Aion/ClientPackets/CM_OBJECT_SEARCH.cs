1dc407b21779373e071fe06f4405920040ec6320
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:34:41 -0400

    Port clientpackets CM_TIME_CHECK + CM_OBJECT_SEARCH

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_OBJECT_SEARCH.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Aion.GameServer.DataHolders;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.Spawns;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_OBJECT_SEARCH (Lyahim). Finds the nearest spawn of an npcId in the player's world and shows it on the map. DataManager.SPAWNS_DATA/SM_SHOW_NPC_ON_MAP red-tolerated.</summary>
+public class CM_OBJECT_SEARCH : AionClientPacket
+{
+    private int npcId;
+
+    public CM_OBJECT_SEARCH(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        this.npcId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player activePlayer = GetConnection().GetActivePlayer();
+        if (activePlayer == null)
+        {
+            return;
+        }
+        SpawnSearchResult searchResult = DataManager.SPAWNS_DATA.GetNearestSpawnByNpcId(activePlayer, npcId, activePlayer.GetWorldId());
+        if (searchResult != null)
+            SendPacket(new SM_SHOW_NPC_ON_MAP(activePlayer, npcId, searchResult.GetWorldId(), searchResult.GetSpot().GetX(), searchResult.GetSpot().GetY(),
+                searchResult.GetSpot().GetZ()));
+        else
+            SendPacket(SM_SYSTEM_MESSAGE.STR_FIND_POS_UNKNOWN_NAME());
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TIME_CHECK.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TIME_CHECK (-Nemesiss-). Likely a ping/pong time-sync; echoes nanoTime via SM_TIME_CHECK after SM_AFTER_TIME_CHECK_4_7_5. SM_* red-tolerated.</summary>
+public class CM_TIME_CHECK : AionClientPacket
+{
+    /// <summary>Nano time / 1000000</summary>
+    private int nanoTime;
+
+    public CM_TIME_CHECK(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        nanoTime = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        // int timeNow = (int) (System.nanoTime() / 1000000);
+        // int diff = timeNow - nanoTime;
+        // System.out.println("CM_TIME_CHECK: " + nanoTime + " =?= " + timeNow + " dif: " + diff);
+        AionConnection client = GetConnection();
+        client.SendPacket(new SM_AFTER_TIME_CHECK_4_7_5()); // don't know what is this doing it is send after this on retail
+        client.SendPacket(new SM_TIME_CHECK(nanoTime));
+    }
+}
