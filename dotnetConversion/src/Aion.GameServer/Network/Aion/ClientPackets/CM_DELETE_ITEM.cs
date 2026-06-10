457f78888759dc64024b5b7ab566ed45ad153494
rrfarmer <ryanfarmer@mac.com>
2026-06-10 15:48:40 -0400

    Port clientpackets CM_DELETE_ITEM + CM_DELETE_QUEST

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_DELETE_ITEM.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Items.Storage;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using ItemDeleteType = Aion.GameServer.Services.Item.ItemPacketService.ItemDeleteType;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_DELETE_ITEM (Avol). Discards an inventory item unless it is unbreakable. Storage/ItemDeleteType red-tolerated.</summary>
+public class CM_DELETE_ITEM : AionClientPacket
+{
+    public int itemObjectId;
+
+    public CM_DELETE_ITEM(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        itemObjectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        Storage inventory = player.GetInventory();
+        Item item = inventory.GetItemByObjId(itemObjectId);
+
+        if (item != null)
+        {
+            if (!item.GetItemTemplate().IsBreakable())
+            {
+                PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_UNBREAKABLE_ITEM(item.GetL10n()));
+            }
+            else
+            {
+                inventory.Delete(item, ItemDeleteType.DISCARD);
+            }
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_DELETE_QUEST.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Aion.GameServer.DataHolders;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_DELETE_QUEST. Abandons a quest; cancels the quest timer task and resets its display first if timed. DataManager/QuestService/SM_QUEST_ACTION red-tolerated.</summary>
+public class CM_DELETE_QUEST : AionClientPacket
+{
+    private int questId;
+
+    public CM_DELETE_QUEST(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        questId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        QuestTemplate qt = DataManager.QUEST_DATA.GetQuestById(questId);
+
+        if (qt != null && qt.IsTimer())
+        {
+            player.GetController().CancelTask(TaskId.QUEST_TIMER);
+            SendPacket(new SM_QUEST_ACTION(questId, 0));
+        }
+        QuestService.AbandonQuest(player, questId);
+    }
+}
