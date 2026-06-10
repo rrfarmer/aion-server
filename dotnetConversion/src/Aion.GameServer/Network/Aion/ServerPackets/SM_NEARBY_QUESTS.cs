02fc64049d9465e1d2f87e70fde9cadad0137eda
rrfarmer <ryanfarmer@mac.com>
2026-06-10 13:43:48 -0400

    Port serverpackets SM_PRIVATE_STORE_NAME + SM_NEARBY_QUESTS

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_NEARBY_QUESTS.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_NEARBY_QUESTS (MrPoke, Rolandas, Neon). Sends nearby quest ids (with not-yet-available bit). Map.entrySet->KeyValuePair iteration; -size & 0xFFFF.</summary>
+public class SM_NEARBY_QUESTS : AionServerPacket
+{
+    private const int notYetAvailableBit = 1 << 17;
+    private IDictionary<int, int> nearbyQuestList;
+
+    public SM_NEARBY_QUESTS(IDictionary<int, int> nearbyQuestList)
+    {
+        this.nearbyQuestList = nearbyQuestList;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteC(0);
+        WriteH(-nearbyQuestList.Count & 0xFFFF);
+        foreach (KeyValuePair<int, int> nearbyQuest in nearbyQuestList)
+        {
+            int questId = nearbyQuest.Key;
+            if (nearbyQuest.Value > 0)
+                questId |= notYetAvailableBit; // for transparent/grey quest marker above npc's head
+            WriteD(questId);
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_PRIVATE_STORE_NAME.cs
@@ -0,0 +1,23 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_PRIVATE_STORE_NAME (Simple). Sends a player's private-store message (objId + name). Player red-tolerated.</summary>
+public class SM_PRIVATE_STORE_NAME : AionServerPacket
+{
+    private int playerObjId;
+    private string name;
+
+    public SM_PRIVATE_STORE_NAME(Player player)
+    {
+        this.playerObjId = player.GetObjectId();
+        this.name = player.GetStore().GetStoreMessage();
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(playerObjId);
+        WriteS(name);
+    }
+}
