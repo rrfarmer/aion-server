423afbdb67b307abcfb03324db00ea460441617b
rrfarmer <ryanfarmer@mac.com>
2026-06-10 12:27:00 -0400

    Port serverpackets SM_ITEM_COOLDOWN + SM_INVENTORY_ADD_ITEM

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_INVENTORY_ADD_ITEM.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Items.Storage;
+using Aion.GameServer.Model.Templates.Item;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Iteminfo;
+using ItemAddType = Aion.GameServer.Services.Item.ItemPacketService.ItemAddType;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_INVENTORY_ADD_ITEM (ATracer). Adds items to the inventory view (mask + per-item info blob). getBuf()->GetBuf(); ItemAddType aliased from ItemPacketService. Item/ItemInfoBlob/ItemStorage red-tolerated.</summary>
+public class SM_INVENTORY_ADD_ITEM : AionServerPacket
+{
+    private readonly List<Item> items;
+    private Player player;
+    private ItemAddType addType;
+
+    public SM_INVENTORY_ADD_ITEM(List<Item> items, Player player, ItemAddType addType)
+    {
+        this.player = player;
+        this.items = items;
+        this.addType = addType;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        // TODO: Rework it, who knows where it could be bugged else.
+        int mask = addType.GetMask();
+        if (addType == ItemAddType.ITEM_COLLECT)
+        {
+            // TODO: if size != 1, then it's buy item, should not specify any slot in other places then !!!
+            if (items.Count == 1 && items[0].GetEquipmentSlot() != ItemStorage.FIRST_AVAILABLE_SLOT)
+                mask = ItemAddType.PARTIAL_WITH_SLOT.GetMask();
+        }
+        WriteH(mask); //
+        WriteH(items.Count); // number of entries
+        foreach (Item item in items)
+            WriteItemInfo(item);
+    }
+
+    private void WriteItemInfo(Item item)
+    {
+        ItemTemplate itemTemplate = item.GetItemTemplate();
+
+        WriteD(item.GetObjectId());
+        WriteD(itemTemplate.GetTemplateId());
+        WriteS(itemTemplate.GetL10n());
+
+        ItemInfoBlob.GetFullBlob(player, item).WriteMe(GetBuf());
+
+        WriteH((int)(item.GetEquipmentSlot() & 0xFFFF));
+        WriteC(item.GetItemTemplate().IsCloth() ? 1 : 0);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_ITEM_COOLDOWN.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Aion.GameServer.Model.Items;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_ITEM_COOLDOWN (ATracer). Sends remaining item reuse cooldowns. currentTimeMillis()->DateTimeOffset; Map.entrySet->KeyValuePair iteration. ItemCooldown red-tolerated.</summary>
+public class SM_ITEM_COOLDOWN : AionServerPacket
+{
+    private IDictionary<int, ItemCooldown> cooldowns;
+
+    public SM_ITEM_COOLDOWN(IDictionary<int, ItemCooldown> cooldowns)
+    {
+        this.cooldowns = cooldowns;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteH(cooldowns.Count);
+        long currentTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        foreach (KeyValuePair<int, ItemCooldown> entry in cooldowns)
+        {
+            WriteH(entry.Key);
+            int left = (int)((entry.Value.GetReuseTime() - currentTime) / 1000);
+            WriteD(left > 0 ? left : 0);
+            WriteD(entry.Value.GetUseDelay());
+        }
+    }
+}
