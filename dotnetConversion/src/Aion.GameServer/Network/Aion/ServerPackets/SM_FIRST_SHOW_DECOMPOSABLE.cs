3972d0e2a423a582a53f4d504f4b68738e2262b8
rrfarmer <ryanfarmer@mac.com>
2026-06-10 11:43:28 -0400

    Port serverpacket SM_FIRST_SHOW_DECOMPOSABLE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_FIRST_SHOW_DECOMPOSABLE.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.Templates.Item;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_FIRST_SHOW_DECOMPOSABLE (xTz). Shows the possible results of decomposing an item (objId + indexed ResultedItem list). ResultedItem red-tolerated.</summary>
+public class SM_FIRST_SHOW_DECOMPOSABLE : AionServerPacket
+{
+    private ICollection<ResultedItem> itemsCollections;
+    private int objectId;
+
+    public SM_FIRST_SHOW_DECOMPOSABLE(int objectId, ICollection<ResultedItem> itemsCollections)
+    {
+        this.itemsCollections = itemsCollections;
+        this.objectId = objectId;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(objectId);
+        WriteD(0);
+        WriteC(itemsCollections.Count);
+        int index = 0;
+        foreach (ResultedItem item in itemsCollections)
+        {
+            WriteC(index);
+            WriteD(item.GetItemId());
+            WriteD(item.GetMinCount());
+            WriteC(0);
+            WriteC(0); // rnd stat bonus
+            WriteC(0); // rnd enchant bonus
+            WriteC(1);
+            index++;
+        }
+    }
+}
