8d408f8cbcab918c525d68da5f39b12ac5e60a84
rrfarmer <ryanfarmer@mac.com>
2026-06-10 18:07:47 -0400

    Port clientpackets CM_UI_SETTINGS + CM_L2AUTH_LOGIN_CHECK

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_L2AUTH_LOGIN_CHECK.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.LoginServer;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_L2AUTH_LOGIN_CHECK (-Nemesiss-). Client authenticates with accountId + session-key parts; validated against the login server. LoginServer red-tolerated.</summary>
+// TODO: L2AUTH? Really? :O
+public class CM_L2AUTH_LOGIN_CHECK : AionClientPacket
+{
+    /// <summary>playOk2 is part of session key - its used for security purposes we will check if this is the key what login server sends.</summary>
+    private int playOk2;
+    /// <summary>playOk1 is part of session key - its used for security purposes we will check if this is the key what login server sends.</summary>
+    private int playOk1;
+    /// <summary>accountId is part of session key - its used for authentication we will check if this accountId is matching any waiting account login server side and check if rest of session key is ok.</summary>
+    private int accountId;
+    /// <summary>loginOk is part of session key - its used for security purposes we will check if this is the key what login server sends.</summary>
+    private int loginOk;
+
+    private int unk1;
+    private int unk2;
+
+    public CM_L2AUTH_LOGIN_CHECK(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        playOk2 = ReadD();
+        playOk1 = ReadD();
+        accountId = ReadD();
+        loginOk = ReadD();
+        unk1 = ReadD();
+        unk2 = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        LoginServer.GetInstance().RegisterLoginRequest(accountId, GetConnection(), loginOk, playOk1, playOk2);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_UI_SETTINGS.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_UI_SETTINGS (ATracer). Persists UI settings (0) / shortcuts (1) / house buddies (2). PlayerSettings red-tolerated.</summary>
+public class CM_UI_SETTINGS : AionClientPacket
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(CM_UI_SETTINGS));
+    private byte settingsType;
+    private byte[] data;
+    private int size;
+
+    public CM_UI_SETTINGS(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        settingsType = ReadC();
+        ReadH();
+        size = ReadUH();
+        data = ReadB(GetRemainingBytes());
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        switch (settingsType)
+        {
+            case 0:
+                player.GetPlayerSettings().SetUiSettings(data);
+                break;
+            case 1:
+                player.GetPlayerSettings().SetShortcuts(data);
+                break;
+            case 2:
+                player.GetPlayerSettings().SetHouseBuddies(data);
+                break;
+            default:
+                log.LogWarning(player + " sent unknown type of player settings: " + settingsType);
+                break;
+        }
+    }
+}
