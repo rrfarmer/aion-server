5a66033aabac435174b3a8569fc80e6aa2843be5
rrfarmer <ryanfarmer@mac.com>
2026-06-10 07:51:14 -0400

    Port services/siege: Siege + Assault abstract bases

---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/Assault.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Threading.Tasks;
+using Aion.Commons.Utils;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Siege;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Spawnengine;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/Assault&lt;SiegeType extends Siege&lt;?&gt;&gt; (Luzien, Estrayl) abstract base for Balaur assaults. AtomicBoolean isStarted; startAssault schedules handleAssault after delay; finishAssault cancels tasks + onAssaultFinish(captured && balaur); spawnAssaulter (heading/distance offset trig placement + aggro). Siege&lt;?&gt;->Siege&lt;SiegeLocation&gt; invariance bound; Future->ScheduledTask; method-ref this::handleAssault->ct-lambda; schedule(...,delay,SECONDS)->TimeSpan.FromSeconds; Math.toRadians->x*PI/180; Math.cos/sin->Math.Cos/Sin. Assaulter/SiegeNpc/SpawnEngine red-tolerated.</summary>
+public abstract class Assault<SiegeType> where SiegeType : Siege<SiegeLocation>
+{
+    private readonly AtomicBoolean isStarted = new AtomicBoolean();
+    protected readonly SiegeLocation siegeLocation;
+    protected readonly SiegeNpc boss;
+    protected readonly int locationId;
+    protected readonly int worldId;
+
+    protected ScheduledTask dredgionTask, spawnTask;
+
+    public Assault(SiegeType siege)
+    {
+        this.siegeLocation = siege.GetSiegeLocation();
+        this.boss = siege.GetBoss();
+        this.locationId = siege.GetSiegeLocationId();
+        this.worldId = siege.GetSiegeLocation().GetWorldId();
+    }
+
+    public int GetWorldId()
+    {
+        return worldId;
+    }
+
+    public void StartAssault(int delay)
+    {
+        if (isStarted.CompareAndSet(false, true))
+            dredgionTask = ThreadPoolManager.GetInstance().Schedule(ct => { HandleAssault(); return ValueTask.CompletedTask; }, TimeSpan.FromSeconds(delay));
+    }
+
+    public void FinishAssault(bool captured)
+    {
+        if (dredgionTask != null && !dredgionTask.IsDone())
+            dredgionTask.Cancel(true);
+        if (spawnTask != null && !spawnTask.IsDone())
+            spawnTask.Cancel(true);
+
+        OnAssaultFinish(captured && siegeLocation.GetRace() == SiegeRace.BALAUR);
+    }
+
+    protected abstract void OnAssaultFinish(bool captured);
+
+    protected abstract void HandleAssault();
+
+    protected void SpawnAssaulter(Assaulter a, SiegeNpc target)
+    {
+        int headingOffset = a.GetHeadingOffset() * 10;
+        float randomDirection = Rnd.Get(-headingOffset, headingOffset) / 10f + target.GetSpawn().GetHeading();
+        double radian = randomDirection * 3d * Math.PI / 180;
+        float x1 = (float)(target.GetX() + Math.Cos(radian) * a.GetDistanceOffset());
+        float y1 = (float)(target.GetY() + Math.Sin(radian) * a.GetDistanceOffset());
+
+        Npc spawned = (Npc)SpawnEngine.SpawnObject(SpawnEngine.NewSiegeSpawn(GetWorldId(), a.GetNpcId(), locationId, SiegeRace.BALAUR,
+            SiegeModType.ASSAULT, x1, y1, target.GetZ() + 0.5f, (byte)0), 1);
+        spawned.GetAggroList().AddHate(target, 100000);
+    }
+
+    protected string GetBossNpcL10n()
+    {
+        if (boss != null && boss.GetObjectTemplate() != null)
+            return boss.GetObjectTemplate().GetL10n();
+        return "";
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/Siege.cs
@@ -0,0 +1,251 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.Commons.Utils;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.GameObjects.Siege;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Model.Templates.Npc;
+using Aion.GameServer.Model.Templates.Siegelocation;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.Abyss;
+using Aion.GameServer.Services.Mail;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/Siege&lt;SL extends SiegeLocation&gt; (SoulKeeper, Source) abstract base. C# generic where SL : SiegeLocation; AtomicBoolean finished; synchronized start->lock double-start guard; CompareAndSet stop (+balaur auto-assault); siege counter/boss/timing; abstract onSiegeStart/Finish/isEndless/onAbyssPointsAdded; initSiegeBoss (find BOSS npc), spawn/despawn/broadcast helpers, sendRewardsToParticipants (top-grade GP/kinah/item mail distribution). currentTimeMillis->UtcNow; log.error(msg,ex)->LogError(ex,msg); Math.round(float)->(long)Floor(x+0.5f); getClass().getSimpleName()->GetType().Name. SiegeLocation/SiegeNpc/SiegeResult/templates red-tolerated.</summary>
+public abstract class Siege<SL> where SL : SiegeLocation
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger("SIEGE_LOG");
+    private readonly AtomicBoolean finished = new AtomicBoolean();
+    private readonly SiegeCounter siegeCounter = new SiegeCounter();
+    private readonly SL siegeLocation;
+    private bool bossKilled;
+    private SiegeNpc boss;
+    private long startTime;
+    private bool started;
+
+    public Siege(SL siegeLocation)
+    {
+        this.siegeLocation = siegeLocation;
+    }
+
+    public void StartSiege()
+    {
+        bool doubleStart = false;
+
+        // keeping synchronization as minimal as possible
+        lock (this)
+        {
+            if (started)
+            {
+                doubleStart = true;
+            }
+            else
+            {
+                startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                started = true;
+            }
+        }
+
+        if (doubleStart)
+        {
+            log.LogError(new InvalidOperationException(), "Attempt to start " + this + " twice");
+        }
+        else
+        {
+            OnSiegeStart();
+        }
+    }
+
+    public void StartSiege(int locationId)
+    {
+        SiegeService.GetInstance().StartSiege(locationId);
+    }
+
+    public void StopSiege()
+    {
+        if (finished.CompareAndSet(false, true))
+        {
+            OnSiegeFinish();
+
+            if (SiegeConfig.BALAUR_AUTO_ASSAULT)
+            {
+                BalaurAssaultService.GetInstance().OnSiegeFinish(this);
+            }
+        }
+        else
+        {
+            log.LogError(new InvalidOperationException(), "Attempt to stop " + this + " twice");
+        }
+    }
+
+    public SL GetSiegeLocation()
+    {
+        return siegeLocation;
+    }
+
+    public int GetSiegeLocationId()
+    {
+        return siegeLocation.GetLocationId();
+    }
+
+    public bool IsBossKilled()
+    {
+        return bossKilled;
+    }
+
+    public void SetBossKilled(bool bossKilled)
+    {
+        this.bossKilled = bossKilled;
+    }
+
+    public SiegeNpc GetBoss()
+    {
+        return boss;
+    }
+
+    public void SetBoss(SiegeNpc boss)
+    {
+        this.boss = boss;
+    }
+
+    public SiegeCounter GetSiegeCounter()
+    {
+        return siegeCounter;
+    }
+
+    public SiegeRaceCounter GetWinnerRaceCounter()
+    {
+        return siegeCounter.GetWinnerRaceCounter(GetSiegeLocation().GetRace());
+    }
+
+    protected abstract void OnSiegeStart();
+
+    protected abstract void OnSiegeFinish();
+
+    public abstract bool IsEndless();
+
+    public abstract void OnAbyssPointsAdded(Player player, int abyssPoints);
+
+    public bool IsStarted()
+    {
+        return started;
+    }
+
+    public bool IsFinished()
+    {
+        return finished.Get();
+    }
+
+    public long GetStartTime()
+    {
+        return startTime;
+    }
+
+    protected void InitSiegeBoss()
+    {
+        SiegeNpc boss = null;
+
+        ICollection<SiegeNpc> npcs = World.GetInstance().GetLocalSiegeNpcs(GetSiegeLocationId());
+        foreach (SiegeNpc npc in npcs)
+        {
+            if (npc.GetObjectTemplate().GetAbyssNpcType().Equals(AbyssNpcType.BOSS))
+            {
+                if (boss != null)
+                    throw new SiegeException("Found 2 siege bosses for outpost " + GetSiegeLocationId());
+
+                boss = npc;
+            }
+        }
+        if (boss == null)
+            throw new SiegeException("Siege Boss not found for siege " + GetSiegeLocationId());
+
+        SetBoss(boss);
+    }
+
+    protected void SpawnNpcs(int locationId, SiegeRace race, SiegeModType type)
+    {
+        SiegeService.GetInstance().SpawnNpcs(locationId, race, type);
+    }
+
+    protected void DespawnNpcs(int locationId)
+    {
+        SiegeService.GetInstance().DeSpawnNpcs(locationId);
+    }
+
+    protected void BroadcastState(SiegeLocation location)
+    {
+        PacketSendUtility.BroadcastToWorld(new SM_SIEGE_LOCATION_STATE(location));
+    }
+
+    protected void BroadcastUpdate(SiegeLocation location)
+    {
+        SiegeService.GetInstance().BroadcastUpdate(location);
+    }
+
+    protected void UpdateOutpostStatusByFortress(FortressLocation location)
+    {
+        SiegeService.GetInstance().UpdateOutpostSiegeState(location);
+    }
+
+    protected void SendRewardsToParticipants(SiegeRaceCounter raceCounter, SiegeResult raceResult)
+    {
+        try
+        {
+            Dictionary<int, long> playerAbyssPoints = raceCounter.GetPlayerAbyssPoints();
+            List<int> topPlayersIds = new List<int>(playerAbyssPoints.Keys);
+            bool isWinner = raceResult == SiegeResult.OCCUPY || raceResult == SiegeResult.DEFENDER;
+
+            int playerIndex = 0;
+            int rewardLevel = 0;
+            long timeMillis = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            foreach (SiegeReward topGrade in GetSiegeLocation().GetRewards())
+            {
+                List<int> rewardedGpPlayers = new List<int>();
+                long kinahRewardForRewardLevel = GetSiegeLocation().GetTemplate().GetKinahRewardByRewardLevel(rewardLevel);
+                AbyssSiegeLevel level = AbyssSiegeLevel.GetLevelById(++rewardLevel);
+                int gp = isWinner ? topGrade.GetGpForWin() : topGrade.GetGpForDefeat();
+
+                for (int i = 0; i < topGrade.GetTop() && playerIndex < topPlayersIds.Count; i++, playerIndex++)
+                {
+                    int playerId = topPlayersIds[playerIndex];
+                    long kinahReward = (long)Math.Floor(kinahRewardForRewardLevel / (float)topGrade.GetTop() + 0.5f);
+                    if (kinahReward > 40000000)
+                        kinahReward = 40000000;
+                    if (isWinner && topGrade.HasItemRewardsForWin())
+                        MailFormatter.SendAbyssRewardMail(GetSiegeLocation(), PlayerService.GetOrLoadPlayerCommonData(playerId), level, raceResult, timeMillis,
+                            topGrade.GetItemId(), topGrade.GetItemCount(), kinahReward);
+                    else if (!isWinner && topGrade.HasItemRewardsForDefeat())
+                        MailFormatter.SendCustomAbyssDefeatRewardMail(PlayerService.GetOrLoadPlayerCommonData(playerId), topGrade.GetItemIdDefeat(), topGrade.GetItemCountDefeat());
+
+                    if (gp > 0)
+                    {
+                        rewardedGpPlayers.Add(playerId);
+                        GloryPointsService.AddGp(playerId, gp); // Rates.GP.calcResult() cannot be fairly applied here, because some players could be offline
+                    }
+                }
+                if (LoggingConfig.LOG_SIEGE && rewardedGpPlayers.Count != 0)
+                {
+                    log.LogInformation(this + ": Distributed " + gp + " " + (isWinner ? "winner" : "loser") + " GP each, to the following players (rank " + rewardLevel
+                        + "): " + rewardedGpPlayers);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            log.LogError(e, "Error while distributing rewards for " + this);
+        }
+    }
+
+    public override string ToString()
+    {
+        return GetType().Name + " [locationId=" + GetSiegeLocationId() + "]";
+    }
+}
