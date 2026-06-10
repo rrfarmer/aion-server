74ec0a33d61fcfc468d7dabac240264e349dad56
rrfarmer <ryanfarmer@mac.com>
2026-06-10 07:54:22 -0400

    Port services/siege: ArtifactSiege + OutpostSiege

---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/ArtifactSiege.cs
@@ -0,0 +1,107 @@
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Dao;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Model.Team.Legion;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/ArtifactSiege (SoulKeeper) extends Siege&lt;ArtifactLocation&gt;. Endless artifact siege: onSiegeStart (boss init, initial delay, balaur assault), onSiegeFinish (despawn, onCapture if boss killed, respawn peace, persist, restart), onCapture (winner race/legion + system messages). getWinnerLegionId Integer->int? (??0); keySet().iterator().next()->Keys.First(); forEachPlayer lambda; AP control no-op. ArtifactLocation/Legion/SM_ red-tolerated.</summary>
+public class ArtifactSiege : Siege<ArtifactLocation>
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(ArtifactSiege));
+
+    public ArtifactSiege(ArtifactLocation siegeLocation)
+        : base(siegeLocation)
+    {
+    }
+
+    protected override void OnSiegeStart()
+    {
+        InitSiegeBoss();
+        GetSiegeLocation().SetInitialDelay(GetStartTime());
+        // Check for Balaur Assault
+        if (SiegeConfig.BALAUR_AUTO_ASSAULT)
+            BalaurAssaultService.GetInstance().OnSiegeStart(this);
+    }
+
+    protected override void OnSiegeFinish()
+    {
+        // despawn npcs
+        DespawnNpcs(GetSiegeLocationId());
+
+        // for artifact should be always true
+        if (IsBossKilled())
+            OnCapture();
+        else
+            log.LogError("Artifact siege (artifactId:" + GetSiegeLocationId() + ") ended without killing a boss.");
+
+        // add new spawns
+        SpawnNpcs(GetSiegeLocationId(), GetSiegeLocation().GetRace(), SiegeModType.PEACE);
+
+        // Store siege results in DB
+        SiegeDAO.UpdateSiegeLocation(GetSiegeLocation());
+
+        BroadcastUpdate(GetSiegeLocation());
+        StartSiege(GetSiegeLocationId());
+    }
+
+    protected void OnCapture()
+    {
+        // Update winner counter
+        SiegeRaceCounter wRaceCounter = GetWinnerRaceCounter();
+        GetSiegeLocation().SetRace(wRaceCounter.GetSiegeRace());
+
+        // Update legion
+        int? wLegionId = wRaceCounter.GetWinnerLegionId();
+        GetSiegeLocation().SetLegionId(wLegionId ?? 0);
+
+        // misc stuff to send player system message
+        if (GetSiegeLocation().GetRace() == SiegeRace.BALAUR)
+        {
+            PacketSendUtility.BroadcastToWorld(
+                SM_SYSTEM_MESSAGE.STR_GUILD_EVENT_LOSE_ARTIFACT(GetSiegeLocation().GetL10n(), GetSiegeLocation().GetRace().GetL10n()));
+        }
+        else
+        {
+            // Prepare packet data
+            string wPlayerName = "";
+            Race wRace = wRaceCounter.GetSiegeRace() == SiegeRace.ELYOS ? Race.ELYOS : Race.ASMODIANS;
+            Legion wLegion = wLegionId != null ? LegionService.GetInstance().GetLegion(wLegionId.Value) : null;
+            if (wRaceCounter.GetPlayerDamageCounter().Count != 0)
+            {
+                int wPlayerId = wRaceCounter.GetPlayerDamageCounter().Keys.First();
+                wPlayerName = PlayerService.GetPlayerName(wPlayerId);
+            }
+            string winnerName = wLegion != null ? wLegion.GetName() : wPlayerName;
+
+            // prepare packets, we can use single packet instance
+            AionServerPacket wRacePacket = SM_SYSTEM_MESSAGE.STR_GUILD_EVENT_WIN_ARTIFACT(wRace.GetL10n(), winnerName,
+                GetSiegeLocation().GetL10n());
+            AionServerPacket lRacePacket = SM_SYSTEM_MESSAGE.STR_GUILD_EVENT_LOSE_ARTIFACT(GetSiegeLocation().GetL10n(), wRace.GetL10n());
+
+            // send update to players
+            World.GetInstance().ForEachPlayer(p => PacketSendUtility.SendPacket(p, p.GetRace().Equals(wRace) ? wRacePacket : lRacePacket));
+        }
+    }
+
+    public override bool IsEndless()
+    {
+        return true;
+    }
+
+    public override void OnAbyssPointsAdded(Player player, int abysPoints)
+    {
+        // No need to control AP
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/OutpostSiege.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.Linq;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.GameObjects.Siege;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services.Mail;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Skillengine;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/OutpostSiege (SoulKeeper, Estrayl) extends Siege&lt;OutpostLocation&gt;. Field-abyss outpost (agent boss) siege: onSiegeStart (vulnerable, respawn siege npcs, boss, spawn announce), onSiegeFinish (agent defeated rewards or empty rewards, peace respawn), onAgentDefeated (top-damager announce + winner-race buff), despawnSiegeNpcs, AP accrual when vulnerable+inside. keySet().iterator().next()->Keys.First(); forEachPlayer lambda; Map->Dictionary; loc 2111=light/else dark. OutpostLocation/SiegeNpc/SiegeResult/SM_ red-tolerated.</summary>
+public class OutpostSiege : Siege<OutpostLocation>
+{
+    public OutpostSiege(OutpostLocation siegeLocation)
+        : base(siegeLocation)
+    {
+    }
+
+    protected override void OnSiegeStart()
+    {
+        GetSiegeLocation().SetVulnerable(true);
+        DespawnNpcs(GetSiegeLocationId());
+        SpawnNpcs(GetSiegeLocationId(), GetSiegeLocation().GetRace(), SiegeModType.SIEGE);
+        InitSiegeBoss();
+
+        PacketSendUtility.BroadcastToWorld(
+            GetSiegeLocationId() == 2111 ? SM_SYSTEM_MESSAGE.STR_FIELDABYSS_LIGHTBOSS_SPAWN() : SM_SYSTEM_MESSAGE.STR_FIELDABYSS_DARKBOSS_SPAWN());
+        BroadcastUpdate(GetSiegeLocation());
+    }
+
+    protected override void OnSiegeFinish()
+    {
+        GetSiegeLocation().SetVulnerable(false);
+        DespawnSiegeNpcs();
+
+        if (IsBossKilled())
+        {
+            OnAgentDefeated();
+            SendRewardsToParticipants(GetWinnerRaceCounter(), SiegeResult.OCCUPY);
+            SendRewardsToParticipants(GetSiegeCounter().GetRaceCounter(GetSiegeLocationId() == 2111 ? SiegeRace.ELYOS : SiegeRace.ASMODIANS),
+                SiegeResult.FAIL);
+        }
+        else
+        {
+            PacketSendUtility.BroadcastToWorld(
+                GetSiegeLocationId() == 2111 ? SM_SYSTEM_MESSAGE.STR_FIELDABYSS_LIGHTBOSS_DESPAWN() : SM_SYSTEM_MESSAGE.STR_FIELDABYSS_DARKBOSS_DESPAWN());
+            SendRewardsToParticipants(GetSiegeCounter().GetRaceCounter(SiegeRace.ELYOS), SiegeResult.EMPTY);
+            SendRewardsToParticipants(GetSiegeCounter().GetRaceCounter(SiegeRace.ASMODIANS), SiegeResult.EMPTY);
+        }
+        BroadcastUpdate(GetSiegeLocation());
+        SpawnNpcs(GetSiegeLocationId(), GetSiegeLocation().GetRace(), SiegeModType.PEACE);
+    }
+
+    private void OnAgentDefeated()
+    {
+        SiegeRaceCounter winnerCounter = GetWinnerRaceCounter();
+        Dictionary<int, long> topPlayerDamages = winnerCounter.GetPlayerDamageCounter();
+        if (topPlayerDamages.Count != 0)
+        {
+            int topPlayerId = topPlayerDamages.Keys.First();
+            PlayerCommonData pcd = PlayerService.GetOrLoadPlayerCommonData(topPlayerId);
+            string playerName = pcd.GetName();
+            string playerRace = pcd.GetRace().GetL10n();
+            PacketSendUtility.BroadcastToWorld(GetSiegeLocationId() == 2111 ? SM_SYSTEM_MESSAGE.STR_FIELDABYSS_LIGHTBOSS_KILLED(playerName, playerRace)
+                : SM_SYSTEM_MESSAGE.STR_FIELDABYSS_DARKBOSS_KILLED(playerName, playerRace));
+            Race winnerRace = winnerCounter.GetSiegeRace() == SiegeRace.ELYOS ? Race.ELYOS : Race.ASMODIANS;
+
+            World.GetInstance().ForEachPlayer(p =>
+            {
+                if (p.GetRace().Equals(winnerRace))
+                    SkillEngine.GetInstance().ApplyEffectDirectly(winnerRace == Race.ELYOS ? 12120 : 12119, p, p);
+            });
+        }
+    }
+
+    public void DespawnSiegeNpcs()
+    {
+        ICollection<SiegeNpc> npcs = World.GetInstance().GetLocalSiegeNpcs(GetSiegeLocationId());
+        foreach (SiegeNpc npc in npcs)
+        {
+            if (npc != null)
+                npc.GetController().DeleteIfAliveOrCancelRespawn();
+        }
+    }
+
+    public override bool IsEndless()
+    {
+        return false;
+    }
+
+    public override void OnAbyssPointsAdded(Player player, int abyssPoints)
+    {
+        if (GetSiegeLocation().IsVulnerable() && GetSiegeLocation().IsInsideLocation(player))
+            GetSiegeCounter().AddAbyssPoints(player, abyssPoints);
+    }
+}
