acbb1438accc27b2a78d95ac54f05950dd1fa102
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:24:50 -0400

    Port clientpackets CM_REGISTER_BROKER_ITEM + CM_GET_HOUSE_BIDS

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_GET_HOUSE_BIDS.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.House;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Collections;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_GET_HOUSE_BIDS (Rolandas). Sends the current house-auction bid list, split across packets. HousingBidService/SplitList/SM_HOUSE_BIDS red-tolerated.</summary>
+public class CM_GET_HOUSE_BIDS : AionClientPacket
+{
+    public CM_GET_HOUSE_BIDS(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        List<HouseBids> houseBids = HousingBidService.GetInstance().GetBidInfo(player.GetRace());
+        SplitList<HouseBids> bidsSplitList = new DynamicServerPacketBodySplitList<HouseBids>(houseBids, true, SM_HOUSE_BIDS.STATIC_BODY_SIZE,
+            SM_HOUSE_BIDS.DYNAMIC_BODY_PART_SIZE_CALCULATOR);
+        foreach (var part in bidsSplitList)
+            PacketSendUtility.SendPacket(player, new SM_HOUSE_BIDS(part.IsFirst(), part.IsLast(), part));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_REGISTER_BROKER_ITEM.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_REGISTER_BROKER_ITEM (kosyak). Registers an item for sale on the broker. BrokerService/DialogAction/AuditLogger red-tolerated.</summary>
+public class CM_REGISTER_BROKER_ITEM : AionClientPacket
+{
+    private int brokerObjId;
+    private int itemUniqueId;
+    private long price;
+    private long itemCount;
+    private bool splittingAvailable;
+
+    public CM_REGISTER_BROKER_ITEM(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        brokerObjId = ReadD();
+        itemUniqueId = ReadD();
+        price = ReadQ();
+        itemCount = ReadQ();
+        splittingAvailable = ReadC() == 1;
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        if (player.IsTrading() || itemCount <= 0)
+            return;
+
+        if (player.IsTargetingNpcWithFunction(brokerObjId, DialogAction.OPEN_VENDOR))
+            BrokerService.GetInstance().RegisterItem(player, itemUniqueId, itemCount, price, splittingAvailable);
+        else
+            AuditLogger.Log(player, "tried to register a broker item without targeting a broker");
+    }
+}
