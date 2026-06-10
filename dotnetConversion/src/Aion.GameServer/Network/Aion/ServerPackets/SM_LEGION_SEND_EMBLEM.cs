837dd01ed942a6bfd1c9e110d07bb2a2ce232f3f
rrfarmer <ryanfarmer@mac.com>
2026-06-10 12:15:45 -0400

    Port serverpackets SM_LEGION_UPDATE_EMBLEM + SM_LEGION_SEND_EMBLEM

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LEGION_SEND_EMBLEM.cs
@@ -0,0 +1,45 @@
+using Aion.GameServer.Model.Team.Legion;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LEGION_SEND_EMBLEM (Simple, cura, Neon). Sends a legion emblem header (id/type/dataSize/argb/name) before emblem byte data. LegionEmblem red-tolerated.</summary>
+public class SM_LEGION_SEND_EMBLEM : AionServerPacket
+{
+    private int legionId;
+    private int emblemId;
+    private byte emblemType;
+    private int emblemDataSize; // used when EMBLEM_DATA is sent afterwards, so the client knows the incoming byte length
+    private byte color_a;
+    private byte color_r;
+    private byte color_g;
+    private byte color_b;
+    private string legionName;
+
+    public SM_LEGION_SEND_EMBLEM(int legionId, LegionEmblem emblem, int emblemDataSize, string legionName)
+    {
+        this.legionId = legionId;
+        this.emblemId = emblem.GetEmblemId();
+        this.emblemType = emblem.GetEmblemType().GetValue();
+        this.emblemDataSize = emblemDataSize;
+        this.color_a = emblem.GetColor_a();
+        this.color_r = emblem.GetColor_r();
+        this.color_g = emblem.GetColor_g();
+        this.color_b = emblem.GetColor_b();
+        this.legionName = legionName;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(legionId);
+        WriteC(emblemId);
+        WriteC(emblemType);
+        WriteD(emblemDataSize);
+        WriteC(color_a);
+        WriteC(color_r);
+        WriteC(color_g);
+        WriteC(color_b);
+        WriteS(legionName);
+        WriteC(0x01);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LEGION_UPDATE_EMBLEM.cs
@@ -0,0 +1,38 @@
+using Aion.GameServer.Model.Team.Legion;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LEGION_UPDATE_EMBLEM (Simple, cura, Neon). Updates a legion emblem (id/type/argb). LegionEmblem red-tolerated.</summary>
+public class SM_LEGION_UPDATE_EMBLEM : AionServerPacket
+{
+    private int legionId;
+    private byte emblemId;
+    private byte color_a;
+    private byte color_r;
+    private byte color_g;
+    private byte color_b;
+    private byte emblemType;
+
+    public SM_LEGION_UPDATE_EMBLEM(int legionId, LegionEmblem emblem)
+    {
+        this.legionId = legionId;
+        this.emblemId = emblem.GetEmblemId();
+        this.color_a = emblem.GetColor_a();
+        this.color_r = emblem.GetColor_r();
+        this.color_g = emblem.GetColor_g();
+        this.color_b = emblem.GetColor_b();
+        this.emblemType = emblem.GetEmblemType().GetValue();
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(legionId);
+        WriteC(emblemId);
+        WriteC(emblemType);
+        WriteC(color_a);
+        WriteC(color_r);
+        WriteC(color_g);
+        WriteC(color_b);
+    }
+}
