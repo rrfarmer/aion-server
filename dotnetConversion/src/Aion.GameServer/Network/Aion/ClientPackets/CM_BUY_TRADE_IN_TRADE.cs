05bc1d09c6bfb18af7b163f154f4f3f9c2c3e12a
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:56:46 -0400

    Port clientpackets CM_BUY_TRADE_IN_TRADE + CM_SHOW_BRAND

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_BUY_TRADE_IN_TRADE.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_BUY_TRADE_IN_TRADE (MrPoke, Ritsu). Buys an item from a trade-in NPC, surrendering trade-in items. TradeService red-tolerated.</summary>
+public class CM_BUY_TRADE_IN_TRADE : AionClientPacket
+{
+    private int sellerObjId;
+    private byte mask;
+    private int itemId;
+    private int count;
+    private int tradeInListCount;
+    private List<int> tradeInItemObjIds;
+
+    public CM_BUY_TRADE_IN_TRADE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        tradeInItemObjIds = new List<int>();
+        sellerObjId = ReadD();
+        mask = ReadC(); // NEW - TODO find out what this is!
+        itemId = ReadD();
+        count = ReadD();
+        tradeInListCount = ReadUH();
+        for (int i = 0; i < tradeInListCount; i++)
+            tradeInItemObjIds.Add(ReadD());
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = this.GetConnection().GetActivePlayer();
+        if (count < 1)
+            return;
+
+        TradeService.PerformBuyFromTradeInTrade(player, sellerObjId, itemId, count, tradeInItemObjIds);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_SHOW_BRAND.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Alliance;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_SHOW_BRAND (Sweetkr, Simple). Sets a team brand (marker) on a target; self-only if no team, else leader/captain updates the team. TemporaryPlayerTeam wildcard -> var. SM_SHOW_BRAND red-tolerated.</summary>
+public class CM_SHOW_BRAND : AionClientPacket
+{
+    private int action;
+    private int brandId;
+    private int targetObjectId;
+
+    public CM_SHOW_BRAND(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        action = ReadD();
+        brandId = ReadD();
+        targetObjectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        var team = player.GetCurrentTeam();
+        if (team == null)
+        {
+            PacketSendUtility.SendPacket(player, new SM_SHOW_BRAND(brandId, targetObjectId));
+        }
+        else if (team.IsLeader(player) || team is PlayerAlliance alliance && alliance.IsSomeCaptain(player))
+        {
+            team.UpdateBrand(brandId, targetObjectId);
+        }
+    }
+}
