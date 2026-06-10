df47fd62ef63db83414a6527366b0fb616e63ae9
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:58:43 -0400

    Port clientpackets CM_FUSION_WEAPONS + CM_GODSTONE_SOCKET

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_FUSION_WEAPONS.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_FUSION_WEAPONS (zdead, Wakizashi, Neon). Fuses (compounds) two weapons at an armsfusion officer. ArmsfusionService/DialogAction/AuditLogger red-tolerated.</summary>
+public class CM_FUSION_WEAPONS : AionClientPacket
+{
+    public CM_FUSION_WEAPONS(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    private int npcObjId;
+    private int mainWeaponObjId;
+    private int fuseWeaponObjId;
+
+    protected override void ReadImpl()
+    {
+        npcObjId = ReadD();
+        mainWeaponObjId = ReadD();
+        fuseWeaponObjId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player.IsTargetingNpcWithFunction(npcObjId, DialogAction.COMPOUND_WEAPON))
+            ArmsfusionService.FusionWeapons(GetConnection().GetActivePlayer(), mainWeaponObjId, fuseWeaponObjId);
+        else
+            AuditLogger.Log(player, "tried to fuse weapons without targeting an armsfusion officer");
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_GODSTONE_SOCKET.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Item;
+using Aion.GameServer.Utils;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_GODSTONE_SOCKET (ATracer). Sockets a godstone into an equipped weapon at a target NPC in talk range. ItemSocketService red-tolerated.</summary>
+public class CM_GODSTONE_SOCKET : AionClientPacket
+{
+    private int npcObjectId;
+    private int weaponId;
+    private int stoneId;
+
+    public CM_GODSTONE_SOCKET(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        npcObjectId = ReadD();
+        weaponId = ReadD();
+        stoneId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        VisibleObject npc = player.GetTarget();
+        if (npc is Npc && npc.GetObjectId() == npcObjectId && PositionUtil.IsInTalkRange(player, (Npc)npc))
+            ItemSocketService.SocketGodstone(player, player.GetEquipment().GetEquippedItemByObjId(weaponId), stoneId);
+    }
+}
