f1a2c21ed1968195111009a1665b6ff1a5e5ca93
rrfarmer <ryanfarmer@mac.com>
2026-06-10 11:21:54 -0400

    Port serverpacket SM_ABYSS_ARTIFACT_INFO3

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_ABYSS_ARTIFACT_INFO3.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_ABYSS_ARTIFACT_INFO3. Sends abyss artifact status (locationId*10+1 + status). ArtifactLocation/SiegeService red-tolerated.</summary>
+public class SM_ABYSS_ARTIFACT_INFO3 : AionServerPacket
+{
+    private ICollection<ArtifactLocation> locations;
+
+    public SM_ABYSS_ARTIFACT_INFO3(ICollection<ArtifactLocation> collection)
+    {
+        this.locations = collection;
+    }
+
+    public SM_ABYSS_ARTIFACT_INFO3(int loc)
+    {
+        locations = new List<ArtifactLocation>();
+        locations.Add(SiegeService.GetInstance().GetArtifact(loc));
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteH(locations.Count);
+        foreach (ArtifactLocation artifact in locations)
+        {
+            WriteD(artifact.GetLocationId() * 10 + 1);
+            WriteC(artifact.GetStatus().GetValue());
+            WriteD(0);
+        }
+    }
+}
