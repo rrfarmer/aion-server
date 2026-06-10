e355e5070450eb9f0d50bc94baa88b760e587311
rrfarmer <ryanfarmer@mac.com>
2026-06-10 12:31:40 -0400

    Port serverpackets SM_INFLUENCE_RATIO + SM_INSTANCE_COUNT_INFO

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_INFLUENCE_RATIO.cs
@@ -0,0 +1,27 @@
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_INFLUENCE_RATIO (Nemiroff). Faction influence rates + per-world influence (subset of SM_FORTRESS_STATUS). Influence/SiegeRace/SiegeService red-tolerated.</summary>
+public class SM_INFLUENCE_RATIO : AionServerPacket
+{
+    protected override void WriteImpl(AionConnection con)
+    {
+        Influence inf = Influence.GetInstance();
+
+        WriteD(SiegeService.GetInstance().GetSecondsUntilNextFortressState());
+        WriteF(inf.GetElyosInfluenceRate());
+        WriteF(inf.GetAsmodianInfluenceRate());
+        WriteF(inf.GetBalaurInfluenceRate());
+        WriteH(inf.GetInfluenceRelevantWorldIds().Count);
+        foreach (int worldId in inf.GetInfluenceRelevantWorldIds())
+        {
+            WriteD(worldId);
+            WriteF(inf.GetInfluence(worldId, SiegeRace.ELYOS));
+            WriteF(inf.GetInfluence(worldId, SiegeRace.ASMODIANS));
+            WriteF(inf.GetInfluence(worldId, SiegeRace.BALAUR));
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_INSTANCE_COUNT_INFO.cs
@@ -0,0 +1,23 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_INSTANCE_COUNT_INFO (xTz). Instance entry-count info (map/instance id + group-type marker).</summary>
+public class SM_INSTANCE_COUNT_INFO : AionServerPacket
+{
+    private int mapId;
+    private int instanceId;
+
+    public SM_INSTANCE_COUNT_INFO(int mapId, int instanceId)
+    {
+        this.mapId = mapId;
+        this.instanceId = instanceId;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(mapId);
+        WriteD(instanceId);
+        WriteD(1); // 1 solo 31 group 61 alliance unk for league
+    }
+}
