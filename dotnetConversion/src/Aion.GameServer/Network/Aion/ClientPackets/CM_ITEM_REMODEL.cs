de5ca82cc820b60a2a157e388a0543a97cb61457
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:56:43 -0400

    Port clientpackets CM_UNWRAP_ITEM + CM_ITEM_REMODEL

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_ITEM_REMODEL.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Item;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_ITEM_REMODEL (Sarynth). Remodels an item (keep appearance of one, extract from another). ItemRemodelService red-tolerated.</summary>
+public class CM_ITEM_REMODEL : AionClientPacket
+{
+    private int keepItemId;
+    private int extractItemId;
+
+    public CM_ITEM_REMODEL(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        ReadD(); // npcId
+        keepItemId = ReadD();
+        extractItemId = ReadD();
+        ReadD(); // unk 0
+    }
+
+    protected override void RunImpl()
+    {
+        Player activePlayer = GetConnection().GetActivePlayer();
+        ItemRemodelService.RemodelItem(activePlayer, keepItemId, extractItemId);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_UNWRAP_ITEM.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using PersistentState = Aion.GameServer.Model.GameObjects.Persistable.PersistentState;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_UNWRAP_ITEM (xTz). Unwraps a packed item (negates pack count, flags update). SM_UNWRAP_ITEM/SM_INVENTORY_UPDATE_ITEM red-tolerated.</summary>
+public class CM_UNWRAP_ITEM : AionClientPacket
+{
+    private int objectId;
+
+    public CM_UNWRAP_ITEM(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        objectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+        {
+            return;
+        }
+        Item item = player.GetInventory().GetItemByObjId(objectId);
+        if (item != null)
+        {
+            if (item.GetPackCount() > 0)
+            {
+                SendPacket(new SM_UNWRAP_ITEM(objectId, item.GetPackCount()));
+                item.SetPackCount(item.GetPackCount() * -1);
+                item.SetPersistentState(PersistentState.UPDATE_REQUIRED);
+                PacketSendUtility.SendPacket(player, new SM_INVENTORY_UPDATE_ITEM(player, item));
+            }
+        }
+    }
+}
