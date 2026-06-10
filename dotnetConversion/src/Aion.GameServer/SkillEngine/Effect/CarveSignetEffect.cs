1373a6bf1ee7e144e4afab2c0e1838ac5e404893
rrfarmer <ryanfarmer@mac.com>
2026-06-10 02:33:10 -0400

    Port Signet DamageEffect subfamily: CarveSignet/SignetBurst

---
+++ dotnetConversion/src/Aion.GameServer/SkillEngine/Effect/CarveSignetEffect.cs
@@ -0,0 +1,41 @@
+using System;
+using Aion.Commons.Utils;
+using System.Xml.Serialization;
+using Aion.GameServer.Skillengine;
+using Aion.GameServer.Skillengine.Model;
+
+namespace Aion.GameServer.Skillengine.Effect;
+
+/// <summary>Java parity: skillengine/effect/CarveSignetEffect (ATracer) : DamageEffect. @XmlAttribute signet_increment/signet_cap/signet_id/signet/prob (required dropped); applyEffect: Rnd.chance>=prob→return; getAbnormalEffect(signet); active→endEffect + nextSignetLevel=min(carved+incr, max(cap, carved)); applyEffect(signetId+next-1)→setCarvedSignet. Local Effect `signet`→`signetEffect` (C# forbids local shadowing field of same name). SkillEngine red-tolerated.</summary>
+[XmlType("CarveSignetEffect")]
+public class CarveSignetEffect : DamageEffect
+{
+    [XmlAttribute("signet_increment")]
+    protected int signetIncrement = 1;
+    [XmlAttribute("signet_cap")]
+    protected int signetCap;
+    [XmlAttribute("signet_id")]
+    protected int signetId;
+    [XmlAttribute]
+    protected string signet;
+    [XmlAttribute]
+    protected int prob = 100;
+
+    public override void ApplyEffect(Effect effect)
+    {
+        base.ApplyEffect(effect);
+
+        if (Rnd.Chance() >= prob)
+            return;
+
+        int nextSignetLevel = signetIncrement;
+        Effect activeSignet = effect.GetEffected().GetEffectController().GetAbnormalEffect(signet);
+        if (activeSignet != null)
+        {
+            activeSignet.EndEffect();
+            nextSignetLevel = Math.Min(activeSignet.GetCarvedSignet() + signetIncrement, Math.Max(signetCap, activeSignet.GetCarvedSignet()));
+        }
+        Effect signetEffect = SkillEngine.GetInstance().ApplyEffect(signetId + nextSignetLevel - 1, effect.GetEffector(), effect.GetEffected());
+        signetEffect.SetCarvedSignet(nextSignetLevel);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/SkillEngine/Effect/SignetBurstEffect.cs
@@ -0,0 +1,63 @@
+using System;
+using Aion.Commons.Utils;
+using System.Xml.Serialization;
+using Aion.GameServer.Controllers.Attack;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model;
+using Aion.GameServer.Skillengine.Model;
+
+namespace Aion.GameServer.Skillengine.Effect;
+
+/// <summary>Java parity: skillengine/effect/SignetBurstEffect (ATracer, kecimis) : DamageEffect. @XmlAttribute signetlvl/signet; @XmlAttribute(name="add_effect_prob_multi"); calculateDamage: base value, element!=NONE→*knowledge/100f (lossy int*=float preserved); SIGNET_DATA_TEMPLATES.getSignetData(SignetEnum.valueOf(signet)→Enum.Parse, lvl); *damageMultiplier, effectProb*=multi; AttackUtil.calculateSkillResult; setLaunchSubEffect(Rnd.chance<effectProb); endEffect. calculate: base.Calculate(effect,null,null) false→endEffect. SignetData/SignetEnum red-tolerated.</summary>
+[XmlType("SignetBurstEffect")]
+public class SignetBurstEffect : DamageEffect
+{
+    [XmlAttribute]
+    protected int signetlvl;
+    [XmlAttribute]
+    protected string signet;
+    [XmlAttribute("add_effect_prob_multi")]
+    protected int addEffectProbMultiplier = 0;
+
+    public override void CalculateDamage(Effect effect)
+    {
+        Effect signetEffect = effect.GetEffected().GetEffectController().GetAbnormalEffect(signet);
+        int valueWithDelta = CalculateBaseValue(effect);
+        if (element != SkillElement.NONE)
+            valueWithDelta = (int)(valueWithDelta * (effect.GetEffector().GetGameStats().GetKnowledge().GetCurrent() / 100f));
+
+        int effectProb = 0;
+        SignetData signetData = DataManager.SIGNET_DATA_TEMPLATES.GetSignetData(Enum.Parse<SignetEnum>(signet), signetEffect == null ? 0 : signetEffect.GetSkillLevel());
+        if (signetData != null)
+        {
+            valueWithDelta = (int)(valueWithDelta * signetData.GetDamageMultiplier());
+            effectProb = signetData.GetAddEffectProb() * addEffectProbMultiplier;
+        }
+        AttackUtil.CalculateSkillResult(effect, valueWithDelta, this, false);
+        effect.SetLaunchSubEffect(Rnd.Chance() < effectProb);
+        if (signetEffect != null)
+            signetEffect.EndEffect();
+    }
+
+    public override void Calculate(Effect effect)
+    {
+        Effect signetEffect = effect.GetEffected().GetEffectController().GetAbnormalEffect(signet);
+        if (!base.Calculate(effect, null, null))
+        {
+            if (signetEffect != null)
+            {
+                signetEffect.EndEffect();
+            }
+        }
+    }
+
+    public int GetSignetlvl()
+    {
+        return signetlvl;
+    }
+
+    public string GetSignet()
+    {
+        return signet;
+    }
+}
