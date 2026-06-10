f0fc3793a88de19ce7c8314a5ed5a9c811e282b7
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:10:40 -0400

    Port clientpackets CM_START_LOOT + CM_LOOT_ITEM

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_LOOT_ITEM.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Drop;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_LOOT_ITEM (alexa026, ATracer). Requests looting a specific drop-list item by index. DropService red-tolerated.</summary>
+public class CM_LOOT_ITEM : AionClientPacket
+{
+    private int targetObjectId;
+    private int index;
+
+    public CM_LOOT_ITEM(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        targetObjectId = ReadD();
+        index = ReadUC();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+            return;
+        DropService.GetInstance().RequestDropItem(player, targetObjectId, index);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_START_LOOT.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Drop;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_START_LOOT (alexa026, Metos, ATracer). Opens (0) or closes (1) a corpse drop list. DropService red-tolerated.</summary>
+public class CM_START_LOOT : AionClientPacket
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(CM_START_LOOT));
+    /// <summary>Target object id that client wants to TALK WITH or 0 if wants to unselect</summary>
+    private int targetObjectId;
+    private byte action;
+
+    public CM_START_LOOT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        targetObjectId = ReadD(); // empty
+        action = ReadC();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        switch (action)
+        {
+            case 0: // open
+                DropService.GetInstance().RequestDropList(player, targetObjectId);
+                break;
+            case 1: // close
+                DropService.GetInstance().CloseDropList(player, targetObjectId);
+                break;
+            default:
+                log.LogWarning(player + " sent unknown loot action type " + action);
+                break;
+        }
+    }
+}
