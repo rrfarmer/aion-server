12e50d72b39986fbfe70f251b40e944a8fa2d188
rrfarmer <ryanfarmer@mac.com>
2026-06-10 11:39:42 -0400

    Port serverpackets SM_ACCOUNT_PROPERTIES + SM_CUSTOM_SETTINGS

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_ACCOUNT_PROPERTIES.cs
@@ -0,0 +1,27 @@
+using Aion.GameServer.Configs.Administration;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_ACCOUNT_PROPERTIES (pixfid, Rolandas, Yeats, Neon). Account flags incl. GM panel enable; mostly fixed/zero fields + account status. AdminConfig red-tolerated.</summary>
+public class SM_ACCOUNT_PROPERTIES : AionServerPacket
+{
+    public SM_ACCOUNT_PROPERTIES()
+    {
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteC(con.GetAccount().GetAccessLevel() >= AdminConfig.GM_PANEL ? 1 : 0); // enables GM panel and other windows, also disables client-side faction restriction for char creation
+        WriteD(0); // always 0
+        WriteH(0); // 0 or 52168 (C8 CB)
+        WriteC(0); // 0 or 1
+        WriteH(0); // 0, 4 or 5
+        WriteC(0); // 0, 4 or 124
+        WriteH(0); // can be 1
+        WriteD(0); // 0 or 16 (or 31 = strong energy of repose)
+        WriteD(0); // always 0
+        WriteD(0); // purchased packet (8 = gold pack)
+        WriteD(4); // account status (0 = gold-user, 1/2 = starter, 3/4 = veteran)
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_CUSTOM_SETTINGS.cs
@@ -0,0 +1,39 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_CUSTOM_SETTINGS (Sweetkr). Player display/deny settings (HIDE_* bitmask). Player red-tolerated.</summary>
+public class SM_CUSTOM_SETTINGS : AionServerPacket
+{
+    public const int HIDE_LEGION_CLOAK = 1;
+    public const int HIDE_LEGION_CLOAK_BY_WEAPON_PRIORITY = 2;
+    public const int HIDE_HELMET = 4;
+    public const int HIDE_PLUME = 8;
+
+    private int objectId;
+    private int unk = 0;
+    private int display; // bitmask of HIDE_* values
+    private int deny;
+
+    public SM_CUSTOM_SETTINGS(Player player)
+        : this(player.GetObjectId(), 1, player.GetPlayerSettings().GetDisplay(), player.GetPlayerSettings().GetDeny())
+    {
+    }
+
+    public SM_CUSTOM_SETTINGS(int objectId, int unk, int display, int deny)
+    {
+        this.objectId = objectId;
+        this.display = display;
+        this.deny = deny;
+        this.unk = unk;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(objectId);
+        WriteC(unk); // unk
+        WriteH(display);
+        WriteH(deny);
+    }
+}
