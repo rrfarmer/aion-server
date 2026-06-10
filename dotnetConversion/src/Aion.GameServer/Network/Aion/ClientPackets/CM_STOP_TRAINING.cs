8c2150b538967b853d6abbcc07610c634be98cd5
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:02:42 -0400

    Port clientpackets CM_STOP_TRAINING + CM_TOGGLE_SKILL_DEACTIVATE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_STOP_TRAINING.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_STOP_TRAINING (xTz). Notifies the instance handler that the player stopped training. WorldMapInstance/InstanceHandler red-tolerated.</summary>
+public class CM_STOP_TRAINING : AionClientPacket
+{
+    public CM_STOP_TRAINING(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        // nothing to read
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        player.GetPosition().GetWorldMapInstance().GetInstanceHandler().OnStopTraining(player);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TOGGLE_SKILL_DEACTIVATE.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Aion.GameServer.DataHolders;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.SkillEngine.Model;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TOGGLE_SKILL_DEACTIVATE (ATracer). Removes a toggle/stance skill effect (audits non-toggle attempts). DataManager.SKILL_DATA/SkillTemplate red-tolerated.</summary>
+public class CM_TOGGLE_SKILL_DEACTIVATE : AionClientPacket
+{
+    private int skillId;
+
+    public CM_TOGGLE_SKILL_DEACTIVATE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        skillId = ReadUH();
+        ReadH();
+        ReadH();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        SkillTemplate skillTemplate = DataManager.SKILL_DATA.GetSkillTemplate(skillId);
+        if (skillTemplate == null || (!skillTemplate.IsToggle() && !skillTemplate.IsStance()))
+        {
+            AuditLogger.Log(player, "tried to remove non-toggle skill effect (" + skillId + ") through CM_TOGGLE_SKILL_DEACTIVATE");
+            return;
+        }
+        player.GetEffectController().RemoveEffect(skillId);
+
+        if (player.GetController().GetStanceSkillId() == skillId)
+            player.GetController().StopStance();
+    }
+}
