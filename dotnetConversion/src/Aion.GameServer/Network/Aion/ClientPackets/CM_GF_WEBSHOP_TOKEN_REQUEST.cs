c55bbd6ba15a2175cc2099175f80a3fd34d3a9b3
rrfarmer <ryanfarmer@mac.com>
2026-06-10 18:02:53 -0400

    Port clientpackets CM_GF_WEBSHOP_TOKEN_REQUEST + CM_IN_GAME_SHOP_INFO

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_GF_WEBSHOP_TOKEN_REQUEST.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_GF_WEBSHOP_TOKEN_REQUEST (Artur). Sent when the client is started with -st; replies with a (currently empty) webshop token. SM_GF_WEBSHOP_TOKEN_RESPONSE red-tolerated.</summary>
+public class CM_GF_WEBSHOP_TOKEN_REQUEST : AionClientPacket
+{
+    public CM_GF_WEBSHOP_TOKEN_REQUEST(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+    }
+
+    protected override void RunImpl()
+    {
+        SendPacket(new SM_GF_WEBSHOP_TOKEN_RESPONSE("")); // TODO
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_IN_GAME_SHOP_INFO.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.InGameShop;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_IN_GAME_SHOP_INFO (xTz, KID). In-game-shop actions (item info / category / list / balance / buy / gift). InGameShopEn/SM_IN_GAME_SHOP_* red-tolerated.</summary>
+public class CM_IN_GAME_SHOP_INFO : AionClientPacket
+{
+    private int actionId;
+    private int categoryId;
+    private int listInCategory;
+    private string senderName;
+    private string senderMessage;
+
+    public CM_IN_GAME_SHOP_INFO(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        actionId = ReadUC();
+        categoryId = ReadD();
+        listInCategory = ReadD();
+        senderName = ReadS();
+        senderMessage = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        if (InGameShopConfig.ENABLE_IN_GAME_SHOP)
+        {
+            Player player = GetConnection().GetActivePlayer();
+
+            switch (actionId)
+            {
+                case 0x01: // item info
+                    PacketSendUtility.SendPacket(player, new SM_IN_GAME_SHOP_ITEM(player, categoryId));
+                    break;
+                case 0x02: // change category
+                    PacketSendUtility.SendPacket(player, new SM_IN_GAME_SHOP_CATEGORY_LIST(2, categoryId));
+                    player.inGameShop.SetCategory((byte)categoryId);
+                    break;
+                case 0x04: // category list
+                    PacketSendUtility.SendPacket(player, new SM_IN_GAME_SHOP_CATEGORY_LIST(0, categoryId));
+                    break;
+                case 0x08: // showcat
+                    if (categoryId > 1)
+                        player.inGameShop.SetSubCategory((byte)categoryId);
+
+                    PacketSendUtility.SendPacket(player, new SM_IN_GAME_SHOP_LIST(player, listInCategory, 1));
+                    PacketSendUtility.SendPacket(player, new SM_IN_GAME_SHOP_LIST(player, listInCategory, 0));
+                    break;
+                case 0x10: // balance
+                    PacketSendUtility.SendPacket(player, new SM_TOLL_INFO(player.GetClientConnection().GetAccount().GetToll()));
+                    break;
+                case 0x20: // buy
+                    InGameShopEn.GetInstance().BuyItemRequest(player, categoryId);
+                    break;
+                case 0x40: // gift
+                    InGameShopEn.GetInstance().GiftItemRequest(player, senderName, senderMessage, categoryId);
+                    break;
+            }
+        }
+    }
+}
