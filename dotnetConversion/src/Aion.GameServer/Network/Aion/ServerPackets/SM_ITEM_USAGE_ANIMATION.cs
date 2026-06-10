f82fdd53d23013ed585a691dcf7542a13e0e27bd
rrfarmer <ryanfarmer@mac.com>
2026-06-10 08:43:23 -0400

    Port serverpackets SM_ITEM_USAGE_ANIMATION + SM_RIFT_ANNOUNCE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_ITEM_USAGE_ANIMATION.cs
@@ -0,0 +1,101 @@
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.World;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_ITEM_USAGE_ANIMATION (ATracer). Broadcasts an item-use cast animation (player/target/item ids, time, end/unk flags). On a timed use it marks the player's using-item. Converges item/skill service usages (ItemActionService, ItemSocketService, PlayerReviveService). AionServerPacket/write* red-tolerated.</summary>
+public class SM_ITEM_USAGE_ANIMATION : AionServerPacket
+{
+    private int playerObjId;
+    private int targetObjId;
+    private int itemObjId;
+    private int itemId;
+    private int time;
+    private int end;
+    private int unk;
+    private int unk1;
+    private int unk2 = 1;
+    private int unk3;
+
+    public SM_ITEM_USAGE_ANIMATION(int playerObjId, int itemObjId, int itemId)
+    {
+        this.playerObjId = playerObjId;
+        this.targetObjId = playerObjId;
+        this.itemObjId = itemObjId;
+        this.itemId = itemId;
+        this.time = 0;
+        this.end = 1;
+        this.unk3 = 1;
+    }
+
+    public SM_ITEM_USAGE_ANIMATION(int playerObjId, int itemObjId, int itemId, int time, int end)
+    {
+        this.playerObjId = playerObjId;
+        this.targetObjId = playerObjId;
+        this.itemObjId = itemObjId;
+        this.itemId = itemId;
+        this.time = time;
+        this.end = end;
+    }
+
+    public SM_ITEM_USAGE_ANIMATION(int playerObjId, int itemObjId, int itemId, int time, int end, int unk)
+    {
+        this.playerObjId = playerObjId;
+        this.targetObjId = playerObjId;
+        this.itemObjId = itemObjId;
+        this.itemId = itemId;
+        this.time = time;
+        this.end = end;
+        this.unk3 = unk;
+    }
+
+    public SM_ITEM_USAGE_ANIMATION(int playerObjId, int targetObjId, int itemObjId, int itemId, int time, int end, int unk)
+    {
+        this.playerObjId = playerObjId;
+        this.targetObjId = targetObjId;
+        this.itemObjId = itemObjId;
+        this.itemId = itemId;
+        this.time = time;
+        this.end = end;
+        this.unk3 = unk;
+    }
+
+    public SM_ITEM_USAGE_ANIMATION(int playerObjId, int targetObjId, int itemObjId, int itemId, int time, int end, int unk, int unk1, int unk2, int unk3)
+    {
+        this.playerObjId = playerObjId;
+        this.targetObjId = targetObjId;
+        this.itemObjId = itemObjId;
+        this.itemId = itemId;
+        this.time = time;
+        this.end = end;
+        this.unk = unk;
+        this.unk1 = unk1;
+        this.unk2 = unk2;
+        this.unk3 = unk3;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        if (time > 0)
+        {
+            Player player = World.GetInstance().GetPlayer(playerObjId);
+            Item item = player.GetInventory().GetItemByObjId(itemObjId);
+            player.SetUsingItem(item);
+        }
+
+        WriteD(playerObjId); // player obj id
+        WriteD(targetObjId); // target obj id
+
+        WriteD(itemObjId); // itemObjId
+        WriteD(itemId); // item id
+
+        WriteD(time); // unk
+        WriteC(end); // unk
+        WriteC(unk); // unk
+        WriteC(unk1);
+        WriteC(unk2);// unk
+        WriteD(unk3);// mb cd?
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_RIFT_ANNOUNCE.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using Aion.GameServer.Controllers;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_RIFT_ANNOUNCE (Sweetkr, -Enomine-). Rift map announce (12-slot counts), silentera (gelk/ingg), rift spawn master/slave info, and despawn, keyed by actionId. Converges RiftInformer SM_RIFT_ANNOUNCE ctors. Map->Dictionary; rifts.values()->Values; switch-on-actionId; writeRiftType helper. RVController/AionServerPacket/write* red-tolerated.</summary>
+public class SM_RIFT_ANNOUNCE : AionServerPacket
+{
+    private int actionId;
+    private RVController rift;
+    private Dictionary<int, int> rifts;
+    private int objectId;
+    private int gelkmaros, inggison;
+
+    public SM_RIFT_ANNOUNCE(Dictionary<int, int> rifts)
+    {
+        this.actionId = 0;
+        this.rifts = rifts;
+    }
+
+    public SM_RIFT_ANNOUNCE(bool gelkmaros, bool inggison)
+    {
+        this.gelkmaros = gelkmaros ? 1 : 0;
+        this.inggison = inggison ? 1 : 0;
+        this.actionId = 1;
+    }
+
+    public SM_RIFT_ANNOUNCE(RVController rift, bool isMaster)
+    {
+        this.rift = rift;
+        this.actionId = isMaster ? 2 : 3;
+    }
+
+    public SM_RIFT_ANNOUNCE(int objectId)
+    {
+        this.objectId = objectId;
+        this.actionId = 4;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        switch (actionId)
+        {
+            case 0: // announce
+                WriteH(1 + (rifts.Values.Count * 4)); // following byte length
+                WriteC(actionId);
+                foreach (int value in rifts.Values)
+                    WriteD(value);
+                break;
+            case 1: // silentera
+                WriteH(9); // following byte length
+                WriteC(actionId);
+                WriteD(gelkmaros);
+                WriteD(inggison);
+                break;
+            case 2:
+                WriteH(35); // following byte length
+                WriteC(actionId);
+                WriteD(rift.GetOwner().GetObjectId());
+                WriteD(rift.GetMaxEntries());
+                WriteD(rift.GetRemainTime());
+                WriteD(rift.GetMinLevel());
+                WriteD(rift.GetMaxLevel());
+                WriteF(rift.GetOwner().GetX());
+                WriteF(rift.GetOwner().GetY());
+                WriteF(rift.GetOwner().GetZ());
+                WriteRiftType();
+                WriteC(rift.IsMaster() ? 1 : 0); // display | hide
+                break;
+            case 3:
+                WriteH(15); // following byte length
+                WriteC(actionId);
+                WriteD(rift.GetOwner().GetObjectId());
+                WriteD(rift.GetUsedEntries());
+                WriteD(rift.GetRemainTime());
+                WriteRiftType();
+                WriteC(0); // unk
+                break;
+            case 4: // rift despawn
+                WriteH(5); // following byte length
+                WriteC(actionId);
+                WriteD(objectId);
+                break;
+        }
+    }
+
+    private void WriteRiftType()
+    {
+        // 1 vortex, 2, concert hall, 3 pangaea, 4 chaos rift, 5 infiltration rift
+        if (rift.IsVortex())
+        {
+            WriteC(1);
+        }
+        else if (rift.IsVolatile())
+        {
+            WriteC(4);
+        }
+        else if (rift.IsInvasion())
+        {
+            WriteC(5);
+        }
+        else
+        {
+            WriteC(0);
+        }
+    }
+}
