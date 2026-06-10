188b6259c43c8ef9a30a42fce7bc406ac5dd5159
rrfarmer <ryanfarmer@mac.com>
2026-06-10 13:20:44 -0400

    Port serverpackets SM_PING_RESPONSE + SM_LOGIN_QUEUE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LOGIN_QUEUE.cs
@@ -0,0 +1,25 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LOGIN_QUEUE (Simple). Login queue position/time/count (fixed sample values).</summary>
+public class SM_LOGIN_QUEUE : AionServerPacket
+{
+    private int waitingPosition; // What is the player's position in line
+    private int waitingTime; // Per waiting position in seconds
+    private int waitingCount; // How many are waiting in line
+
+    private SM_LOGIN_QUEUE()
+    {
+        this.waitingPosition = 5;
+        this.waitingTime = 60;
+        this.waitingCount = 50;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(waitingPosition);
+        WriteD(waitingTime);
+        WriteD(waitingCount);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_PING_RESPONSE.cs
@@ -0,0 +1,12 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_PING_RESPONSE (dragoon112). Ping response (0x04).</summary>
+public class SM_PING_RESPONSE : AionServerPacket
+{
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteC(0x04);
+    }
+}
