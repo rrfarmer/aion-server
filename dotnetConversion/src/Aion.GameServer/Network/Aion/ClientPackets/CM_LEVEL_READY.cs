ff6635947c3bb49dec2bbe843ffc158f2c0307fc
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:17:02 -0400

    Port clientpackets CM_LEVEL_READY + CM_PLAY_MOVIE_END

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_LEVEL_READY.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.DataHolders;
+using Aion.GameServer.Model.Animations;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.GameObjects.State;
+using Aion.GameServer.Model.Items.Storage;
+using Aion.GameServer.Model.Templates.Windstreams;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.QuestEngine;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.ConquerorAndProtectorSystem;
+using Aion.GameServer.Services.Event;
+using Aion.GameServer.Services.Instance;
+using Aion.GameServer.Services.Rift;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_LEVEL_READY (-Nemesiss-, Kwazar). Client signals the map is loaded; spawns the player and fires the full enter-world sequence. Many services red-tolerated.</summary>
+public class CM_LEVEL_READY : AionClientPacket
+{
+    public CM_LEVEL_READY(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+    }
+
+    protected override void RunImpl()
+    {
+        Player activePlayer = GetConnection().GetActivePlayer();
+
+        if (activePlayer.GetActiveHouse() != null)
+            SendPacket(new SM_HOUSE_OBJECTS(activePlayer.GetActiveHouse().GetRegistry().GetSpawnedObjects()));
+        if (activePlayer.IsInInstance())
+        {
+            SendPacket(new SM_INSTANCE_COUNT_INFO(activePlayer.GetWorldId(), activePlayer.GetInstanceId()));
+        }
+        SendPacket(new SM_PLAYER_INFO(activePlayer));
+        activePlayer.GetController().StartProtectionActiveTask();
+        SendPacket(new SM_ACCOUNT_PROPERTIES());
+        SendPacket(new SM_MOTION(activePlayer.GetObjectId(), activePlayer.GetMotions().GetActiveMotions()));
+
+        WindstreamTemplate template = DataManager.WINDSTREAM_DATA.GetStreamTemplate(activePlayer.GetPosition().GetMapId());
+        if (template != null)
+            foreach (Location2D location in template.GetLocations().GetLocation())
+            {
+                SendPacket(new SM_WINDSTREAM_ANNOUNCE(location.GetFlyPathType().GetId(), template.GetMapId(), location.GetId(), location.GetState()));
+            }
+
+        // Spawn player into the world.
+        World.GetInstance().Spawn(activePlayer);
+
+        if (activePlayer.IsInFlyState(FlyState.FLYING)) // notify client if we are still flying (client always ends flying after teleport)
+            activePlayer.GetFlyController().StartFly(true, true);
+
+        // SM_SHIELD_EFFECT, SM_ABYSS_ARTIFACT_INFO3
+        if (activePlayer.IsInSiegeWorld())
+        {
+            SiegeService.GetInstance().OnEnterSiegeWorld(activePlayer);
+        }
+
+        // SM_CONQUEROR_PROTECTOR
+        ConquerorAndProtectorService.GetInstance().OnEnterMap(activePlayer);
+
+        // SM_RIFT_ANNOUNCE
+        RiftInformer.SendRiftsInfo(activePlayer);
+
+        // SM_UPGRADE_ARCADE
+        if (EventsConfig.ENABLE_EVENT_ARCADE)
+            SendPacket(new SM_UPGRADE_ARCADE(true));
+
+        // SM_NEARBY_QUESTS
+        activePlayer.GetController().UpdateNearbyQuests();
+
+        // SM_QUEST_REPEAT
+        activePlayer.GetController().UpdateRepeatableQuests();
+
+        // Loading weather for the player's region
+        WeatherService.GetInstance().LoadWeather(activePlayer);
+
+        QuestEngine.GetInstance().OnEnterWorld(activePlayer);
+
+        activePlayer.GetController().OnEnterWorld();
+        InstanceService.OnEnterInstance(activePlayer);
+        activePlayer.GetEffectController().UpdatePlayerEffectIcons(null);
+        SendPacket(SM_CUBE_UPDATE.CubeSize(StorageType.CUBE, activePlayer));
+
+        Pet pet = activePlayer.GetPet();
+        if (pet != null && !pet.IsSpawned())
+            World.GetInstance().Spawn(pet);
+        activePlayer.SetPortAnimation(ArrivalAnimation.NONE);
+
+        TownService.GetInstance().OnEnterWorld(activePlayer);
+        EventService.GetInstance().OnEnterMap(activePlayer);
+
+        var team = activePlayer.GetCurrentTeam();
+        if (team != null)
+            ThreadPoolManager.GetInstance().Schedule(() => team.SendBrands(activePlayer), 100); // delayed to fix brands when returning from studios/houses
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_PLAY_MOVIE_END.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.QuestEngine;
+using Aion.GameServer.QuestEngine.Model;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_PLAY_MOVIE_END (MrPoke). Returns SM_PLAY_MOVIE data after a cutscene finishes/skips; validates server-initiated cutscenes (book auto-movies allowed). QuestEngine red-tolerated.</summary>
+public class CM_PLAY_MOVIE_END : AionClientPacket
+{
+    private byte type;
+    private int targetObjectId;
+    private int questId;
+    private int movieId;
+    private bool canSkip;
+
+    public CM_PLAY_MOVIE_END(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        type = ReadC(); // 1: CutSceneMovies, otherwise CutScenes
+        targetObjectId = ReadD();
+        questId = ReadD();
+        movieId = ReadD();
+        ReadC(); // unknown
+        canSkip = ReadC() == 0;
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (!player.IsInCustomState(CustomPlayerState.WATCHING_CUTSCENE))
+        {
+            // the client automatically plays movies when reading certain books (3: 730079/730091, 4: 730092, 5: 730085)
+            ISet<int> bookMovieIds = type == 1 ? new HashSet<int> { 3, 4, 5 } : new HashSet<int>();
+            if (questId != 0 || !bookMovieIds.Contains(movieId))
+                AuditLogger.Log(player, "sent " + GetPacketName() + " for cutscene " + movieId + " that wasn't sent by the server");
+            return;
+        }
+        player.UnsetCustomState(CustomPlayerState.WATCHING_CUTSCENE);
+        VisibleObject target = player.IsTargeting(targetObjectId) ? player.GetTarget() : null;
+        QuestEngine.GetInstance().OnMovieEnd(new QuestEnv(target, player, questId), movieId);
+        player.GetPosition().GetWorldMapInstance().GetInstanceHandler().OnPlayMovieEnd(player, movieId);
+    }
+}
