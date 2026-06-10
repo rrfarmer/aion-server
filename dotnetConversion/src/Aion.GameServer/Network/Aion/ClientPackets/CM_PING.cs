cdd9bdf871f3f9014da27885d6f77239f216cef6
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:30:39 -0400

    Port clientpackets CM_PING + CM_SHOW_DIALOG

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_PING.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_PING (-Nemesiss-, Undertrey, Neon). Heartbeat ping; replies SM_PONG and detects time/speed hacks via ping-interval analysis. SM_PONG/AuditLogger red-tolerated.</summary>
+public class CM_PING : AionClientPacket
+{
+    public const int CLIENT_PING_INTERVAL = 180 * 1000; // client sends this packet every 180 seconds
+
+    public CM_PING(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        ReadH(); // unk
+    }
+
+    protected override void RunImpl()
+    {
+        long lastPingMillis = GetConnection().GetLastPingTime();
+        long nowMillis = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        GetConnection().SetLastPingTime(nowMillis);
+        SendPacket(new SM_PONG());
+
+        if (lastPingMillis > 0)
+        {
+            long pingInterval = nowMillis - lastPingMillis;
+            if (pingInterval + 2000 < CLIENT_PING_INTERVAL)
+            { // client timer cheat
+                if (GetConnection().IncreaseAndGetPingFailCount() == 3)
+                {
+                    if (SecurityConfig.PINGCHECK_KICK)
+                    {
+                        AuditLogger.Log(GetConnection().GetActivePlayer(),
+                                "possibly using time/speed hack (client ping interval: " + pingInterval + "/" + CLIENT_PING_INTERVAL + "), kicking player");
+                        GetConnection().Close();
+                    }
+                    else
+                    {
+                        AuditLogger.Log(GetConnection().GetActivePlayer(), "possibly using time/speed hack (client ping interval: " + pingInterval + "/" + CLIENT_PING_INTERVAL + ")");
+                        GetConnection().ResetPingFailCount();
+                    }
+                }
+            }
+            else
+            {
+                GetConnection().ResetPingFailCount();
+            }
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_SHOW_DIALOG.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.Npc;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_SHOW_DIALOG (alexa026, Avol, ATracer). Requests an NPC dialog; drops hide effects if the NPC can't be talked to while invisible. Npc/TalkInfo red-tolerated.</summary>
+public class CM_SHOW_DIALOG : AionClientPacket
+{
+    private int targetObjectId;
+
+    public CM_SHOW_DIALOG(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        targetObjectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player.IsProtectionActive())
+            player.GetController().StopProtectionActiveTask();
+
+        if (player.IsTrading())
+            return;
+
+        if (player.GetKnownList().GetObject(targetObjectId) is Npc target)
+        {
+            TalkInfo talkInfo = target.GetObjectTemplate().GetTalkInfo();
+            if (talkInfo != null && !talkInfo.IsCanTalkInvisible() && player.IsInAnyHide())
+                player.GetEffectController().RemoveHideEffects();
+            target.GetController().OnDialogRequest(player);
+        }
+    }
+}
