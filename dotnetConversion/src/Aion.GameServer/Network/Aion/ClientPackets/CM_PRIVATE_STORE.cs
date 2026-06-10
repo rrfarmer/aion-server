e3a7b4b134de314301762ddb36f3a1a0c446e077
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:48:43 -0400

    Port clientpackets CM_PRIVATE_STORE + CM_PRIVATE_STORE_NAME

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_PRIVATE_STORE.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Trade;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_PRIVATE_STORE (Simple). Sets the private-store item list (empty closes the store). PrivateStoreService/TradePSItem red-tolerated.</summary>
+public class CM_PRIVATE_STORE : AionClientPacket
+{
+    private TradePSItem[] tradePSItems;
+
+    public CM_PRIVATE_STORE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        int itemCount = ReadUH();
+        tradePSItems = new TradePSItem[itemCount];
+        for (int i = 0; i < itemCount; i++)
+        {
+            int itemObjId = ReadD();
+            int itemId = ReadD();
+            int count = ReadUH();
+            long price = ReadQ();
+            tradePSItems[i] = new TradePSItem(itemObjId, itemId, count, price);
+        }
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (tradePSItems.Length <= 0)
+            PrivateStoreService.ClosePrivateStore(player);
+        else
+            PrivateStoreService.CreateStoreWithItems(player, tradePSItems);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_PRIVATE_STORE_NAME.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_PRIVATE_STORE_NAME (Simple). Opens a private store with the given name. PrivateStoreService red-tolerated.</summary>
+public class CM_PRIVATE_STORE_NAME : AionClientPacket
+{
+    private string name;
+
+    public CM_PRIVATE_STORE_NAME(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        name = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        Player activePlayer = GetConnection().GetActivePlayer();
+        PrivateStoreService.OpenPrivateStore(activePlayer, name);
+    }
+}
