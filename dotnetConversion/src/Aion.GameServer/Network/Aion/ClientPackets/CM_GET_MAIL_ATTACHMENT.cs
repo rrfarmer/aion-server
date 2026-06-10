2e569371d1d98a1962922fae3ca547d3bd71e5ad
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:52:53 -0400

    Port clientpackets CM_GET_MAIL_ATTACHMENT + CM_SEND_MAIL

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_GET_MAIL_ATTACHMENT.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Mail;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_GET_MAIL_ATTACHMENT (kosyachok). Retrieves a mail's item (0) or kinah (1) attachment. MailService red-tolerated.</summary>
+public class CM_GET_MAIL_ATTACHMENT : AionClientPacket
+{
+    private int mailObjId;
+    private byte attachmentType;
+
+    public CM_GET_MAIL_ATTACHMENT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        mailObjId = ReadD();
+        attachmentType = ReadC(); // 0 - item , 1 - kinah
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        MailService.GetAttachments(player, mailObjId, attachmentType);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_SEND_MAIL.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Mail;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_SEND_MAIL (Aion Gates, xTz). Sends a mail with optional item + kinah. MailService/LetterType red-tolerated.</summary>
+public class CM_SEND_MAIL : AionClientPacket
+{
+    private string recipientName;
+    private string title;
+    private string message;
+    private int itemObjId;
+    private long itemCount;
+    private long kinahCount;
+    private int idLetterType;
+
+    public CM_SEND_MAIL(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        recipientName = ReadS();
+        title = ReadS();
+        message = ReadS();
+        itemObjId = ReadD();
+        itemCount = ReadQ();
+        kinahCount = ReadQ();
+        idLetterType = ReadUC();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        MailService.SendMail(player, recipientName, title, message, itemObjId, itemCount, kinahCount, LetterTypeExtensions.GetLetterTypeById(idLetterType));
+    }
+}
