7c7636692e7acd2fb8907c0ddf05cec203fc091a
rrfarmer <ryanfarmer@mac.com>
2026-06-10 15:12:54 -0400

    Port clientpackets CM_CHAT_MESSAGE_PUBLIC + CM_CHAT_MESSAGE_WHISPER

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_CHAT_MESSAGE_PUBLIC.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Restrictions;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.ChatHandlers;
+using Aion.GameServer.Utils.Stats;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_CHAT_MESSAGE_PUBLIC (SoulKeeper). Reads normal chat messages and broadcasts by ChatType (group/alliance/legion/league/normal/shout/command). ChatProcessor/PlayerChatService/SM_MESSAGE red-tolerated.</summary>
+public class CM_CHAT_MESSAGE_PUBLIC : AionClientPacket
+{
+    /// <summary>Chat type</summary>
+    private ChatType type;
+
+    /// <summary>Chat message</summary>
+    private string message;
+
+    public CM_CHAT_MESSAGE_PUBLIC(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        type = ChatType.GetChatType(ReadC());
+        message = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        if (ChatProcessor.GetInstance().HandleChatCommand(player, message))
+            return;
+
+        if (!PlayerRestrictions.CanChat(player))
+            return;
+
+        PlayerChatService.LogMessage(player, type, message);
+        message = NameRestrictionService.FilterMessage(message);
+
+        switch (type)
+        {
+            case ChatType.GROUP:
+                if (!player.IsInTeam())
+                    return;
+                BroadcastToGroupMembers(player);
+                break;
+            case ChatType.ALLIANCE:
+                if (!player.IsInAlliance())
+                    return;
+                BroadcastToAllianceMembers(player);
+                break;
+            case ChatType.GROUP_LEADER:
+                if (!player.IsInTeam())
+                    return;
+                // Alert must go to entire group or alliance.
+                if (player.IsInGroup())
+                    BroadcastToGroupMembers(player);
+                else
+                    BroadcastToAllianceMembers(player);
+                break;
+            case ChatType.LEGION:
+                if (!player.IsLegionMember())
+                    return;
+                BroadcastToLegionMembers(player);
+                break;
+            case ChatType.LEAGUE:
+            case ChatType.LEAGUE_ALERT:
+                if (!player.IsInLeague())
+                    return;
+                BroadcastToLeagueMembers(player);
+                break;
+            case ChatType.NORMAL:
+            case ChatType.SHOUT:
+                BroadcastToPlayers(player);
+                break;
+            case ChatType.COMMAND:
+                if (player.GetAbyssRank().GetRank() == AbyssRankEnum.COMMANDER || player.GetAbyssRank().GetRank() == AbyssRankEnum.SUPREME_COMMANDER)
+                    BroadcastFromCommander(player);
+                break;
+            default:
+                if (!player.IsStaff())
+                    return;
+                BroadcastToPlayers(player);
+                break;
+        }
+    }
+
+    private void BroadcastFromCommander(Player player)
+    {
+        int senderRace = player.GetRace().GetRaceId();
+        PacketSendUtility.BroadcastPacket(player, new SM_MESSAGE(player, message, type), true,
+            p => senderRace == p.GetRace().GetRaceId() || player.IsStaff() || p.IsStaff());
+    }
+
+    /// <summary>Sends message to all players that are not in blocklist (except GMs)</summary>
+    private void BroadcastToPlayers(Player player)
+    {
+        PacketSendUtility.BroadcastPacket(player, new SM_MESSAGE(player, message, type), true,
+            p => !p.GetBlockList().Contains(player.GetObjectId()) || player.IsStaff() || p.IsStaff());
+    }
+
+    /// <summary>Sends message to all group members.</summary>
+    private void BroadcastToGroupMembers(Player player)
+    {
+        player.GetCurrentGroup().SendPackets(new SM_MESSAGE(player, message, type));
+    }
+
+    /// <summary>Sends message to all alliance members</summary>
+    private void BroadcastToAllianceMembers(Player player)
+    {
+        player.GetPlayerAlliance().SendPackets(new SM_MESSAGE(player, message, type));
+    }
+
+    /// <summary>Sends message to all league members</summary>
+    private void BroadcastToLeagueMembers(Player player)
+    {
+        player.GetPlayerAlliance().GetLeague().SendPackets(new SM_MESSAGE(player, message, type));
+    }
+
+    /// <summary>Sends message to all legion members</summary>
+    private void BroadcastToLegionMembers(Player player)
+    {
+        PacketSendUtility.BroadcastToLegion(player.GetLegion(), new SM_MESSAGE(player, message, type));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_CHAT_MESSAGE_WHISPER.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Globalization;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Restrictions;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_CHAT_MESSAGE_WHISPER (SoulKeeper). Reads whisper chat messages and routes to the named receiver with refusal/level/block/faction guards. World/SM_MESSAGE/SM_SYSTEM_MESSAGE red-tolerated.</summary>
+public class CM_CHAT_MESSAGE_WHISPER : AionClientPacket
+{
+    /// <summary>To whom this message is sent</summary>
+    private string name;
+
+    /// <summary>Message text</summary>
+    private string message;
+
+    public CM_CHAT_MESSAGE_WHISPER(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        name = ReadS();
+        message = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        string realName = ChatUtil.GetRealCharName(name);
+        Player sender = GetConnection().GetActivePlayer();
+        Player receiver = World.GetInstance().GetPlayer(realName);
+
+        if (receiver == null)
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_NO_SUCH_USER(realName));
+        }
+        else if (receiver.IsInCustomState(CustomPlayerState.NO_WHISPERS_MODE) && !sender.IsStaff())
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_WHISPER_REFUSE(receiver.GetName(true)));
+        }
+        else if (sender.GetLevel() < CustomConfig.LEVEL_TO_WHISPER && !receiver.IsStaff())
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_CANT_WHISPER_LEVEL(CustomConfig.LEVEL_TO_WHISPER.ToString(CultureInfo.InvariantCulture)));
+        }
+        else if (receiver.GetBlockList().Contains(sender.GetObjectId()))
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_YOU_EXCLUDED(receiver.GetName()));
+        }
+        else if (sender.GetRace() != receiver.GetRace() && !CustomConfig.SPEAKING_BETWEEN_FACTIONS && !sender.IsStaff() && !receiver.IsStaff())
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_CANT_WHISPER_OTHER_RACE());
+        }
+        else
+        {
+            if (!PlayerRestrictions.CanChat(sender))
+                return;
+            PlayerChatService.LogWhisper(sender, receiver, message);
+            PacketSendUtility.SendPacket(receiver, new SM_MESSAGE(sender, NameRestrictionService.FilterMessage(message), ChatType.WHISPER));
+        }
+    }
+}
