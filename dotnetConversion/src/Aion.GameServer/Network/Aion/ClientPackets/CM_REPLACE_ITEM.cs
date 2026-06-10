b870eee136ca5b9b1d58c6b1b25c0a2f89d05013
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:19:50 -0400

    Port clientpackets CM_TARGET_SELECT + CM_REPLACE_ITEM

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_REPLACE_ITEM.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Item;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_REPLACE_ITEM (kosyachok). Swaps two items between storages. ItemMoveService red-tolerated.</summary>
+public class CM_REPLACE_ITEM : AionClientPacket
+{
+    private byte sourceStorageType;
+    private int sourceItemObjId;
+    private byte replaceStorageType;
+    private int replaceItemObjId;
+
+    public CM_REPLACE_ITEM(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        sourceStorageType = ReadC();
+        sourceItemObjId = ReadD();
+        replaceStorageType = ReadC();
+        replaceItemObjId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        ItemMoveService.SwitchItemsInStorages(player, sourceStorageType, sourceItemObjId, replaceStorageType, replaceItemObjId);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TARGET_SELECT.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TARGET_SELECT (SoulKeeper, Sweetkr, KID). Selects a target via click/hotkey/chat, including assist (target-of-target). VisibleObject/AuditLogger red-tolerated.</summary>
+public class CM_TARGET_SELECT : AionClientPacket
+{
+    /// <summary>Target object id that client wants to select or 0 if wants to unselect</summary>
+    private int targetObjectId;
+    private bool selectTargetOfTarget;
+
+    public CM_TARGET_SELECT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        targetObjectId = ReadD();
+        selectTargetOfTarget = ReadC() == 1;
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+
+        VisibleObject newTarget;
+        if (selectTargetOfTarget)
+        {
+            if (player.GetTarget() == null)
+            {
+                SendPacket(SM_SYSTEM_MESSAGE.STR_ASSISTKEY_THIS_IS_ASSISTKEY());
+                return;
+            }
+            newTarget = player.GetTarget().GetTarget();
+            if (newTarget == null)
+            {
+                SendPacket(SM_SYSTEM_MESSAGE.STR_ASSISTKEY_NO_USER());
+                return;
+            }
+            if (!newTarget.Equals(player) && !player.GetKnownList().Sees(newTarget))
+            {
+                SendPacket(player.GetKnownList().Knows(newTarget) ? SM_SYSTEM_MESSAGE.STR_ASSISTKEY_NO_USER() : SM_SYSTEM_MESSAGE.STR_ASSISTKEY_TOO_FAR());
+                return;
+            }
+        }
+        else if (targetObjectId == 0)
+        {
+            newTarget = null;
+        }
+        else if (targetObjectId == player.GetObjectId())
+        {
+            newTarget = player;
+        }
+        else
+        {
+            newTarget = player.GetKnownList().GetObject(targetObjectId);
+            if (newTarget == null && player.IsInTeam() && player.GetCurrentTeam().HasMember(targetObjectId))
+                newTarget = player.GetCurrentTeam().GetMember(targetObjectId).GetObject();
+            else if (newTarget != null && !player.Equals(newTarget) && !player.GetKnownList().Sees(newTarget))
+            {
+                AuditLogger.Log(player, "possibly used radar hack: trying to target invisible " + newTarget);
+                newTarget = null;
+            }
+        }
+        player.SetTarget(newTarget);
+    }
+}
