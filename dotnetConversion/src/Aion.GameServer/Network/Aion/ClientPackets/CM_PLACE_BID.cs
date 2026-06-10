e56a0b6c69e40188cd85afcfe72f0a55c26caa59
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:22:51 -0400

    Port clientpackets CM_USE_CHARGE_SKILL + CM_PLACE_BID

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_PLACE_BID.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_PLACE_BID (Rolandas). Places a bid on a house auction. HousingBidService red-tolerated.</summary>
+public class CM_PLACE_BID : AionClientPacket
+{
+    private int listIndex;
+    private long bidOffer;
+
+    public CM_PLACE_BID(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        listIndex = ReadD();
+        bidOffer = ReadQ();
+    }
+
+    protected override void RunImpl()
+    {
+        if (HousingConfig.ENABLE_HOUSE_AUCTIONS)
+        {
+            Player player = GetConnection().GetActivePlayer();
+            HousingBidService.GetInstance().Bid(player, listIndex, bidOffer);
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_USE_CHARGE_SKILL.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.SkillEngine.Model;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_USE_CHARGE_SKILL (Cheatkiller). Releases a charge skill with the accumulated charge time. Skill/controller red-tolerated.</summary>
+public class CM_USE_CHARGE_SKILL : AionClientPacket
+{
+    public CM_USE_CHARGE_SKILL(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        Skill chargeCastingSkill = player.GetCastingSkill();
+        if (chargeCastingSkill == null || !chargeCastingSkill.GetSkillTemplate().IsCharge())
+            return;
+        long chargeTimeMillis = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - chargeCastingSkill.GetCastStartTime();
+        player.GetController().UseChargeSkill(chargeCastingSkill, chargeTimeMillis);
+    }
+}
