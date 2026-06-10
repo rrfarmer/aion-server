0f48953d891b1c5b3d3e747cf45104a2dcd77562
rrfarmer <ryanfarmer@mac.com>
2026-06-10 14:35:56 -0400

    Port serverpackets SM_UPDATE_PLAYER_APPEARANCE + SM_VIEW_PLAYER_DETAILS

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_UPDATE_PLAYER_APPEARANCE.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_UPDATE_PLAYER_APPEARANCE (Avol, ATracer, Neon). Updates a player's equipped appearance via AbstractPlayerInfoPacket.WriteEquippedItems. Item red-tolerated.</summary>
+public class SM_UPDATE_PLAYER_APPEARANCE : AbstractPlayerInfoPacket
+{
+    private int playerId;
+    private List<Item> items;
+
+    public SM_UPDATE_PLAYER_APPEARANCE(int playerId, List<Item> items)
+    {
+        this.playerId = playerId;
+        this.items = items;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(playerId);
+        WriteEquippedItems(items);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_VIEW_PLAYER_DETAILS.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.Item;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Iteminfo;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_VIEW_PLAYER_DETAILS (Avol, xTz). Shows another player's equipped items (per-item info blob). getBuf()->GetBuf(). Item/ItemInfoBlob red-tolerated.</summary>
+public class SM_VIEW_PLAYER_DETAILS : AionServerPacket
+{
+    private List<Item> items;
+    private int itemSize;
+    private int targetObjId;
+    private Player player;
+
+    public SM_VIEW_PLAYER_DETAILS(List<Item> items, Player player)
+    {
+        this.player = player;
+        this.targetObjId = player.GetObjectId();
+        this.items = items;
+        this.itemSize = items.Count;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(targetObjId);
+        WriteC(11);
+        WriteH(itemSize);
+        foreach (Item item in items)
+        {
+            WriteItemInfo(item);
+        }
+    }
+
+    private void WriteItemInfo(Item item)
+    {
+        ItemTemplate template = item.GetItemTemplate();
+
+        WriteD(0);
+        WriteD(template.GetTemplateId());
+        WriteS(template.GetL10n());
+
+        ItemInfoBlob itemInfoBlob = ItemInfoBlob.GetFullBlob(player, item);
+        itemInfoBlob.WriteMe(GetBuf());
+    }
+}
