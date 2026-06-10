225fe98e96410f8327aef7a03fbc1cc76b7284ea
rrfarmer <ryanfarmer@mac.com>
2026-06-10 12:24:42 -0400

    Port serverpackets SM_LEARN_RECIPE + SM_ICON_INFO

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_ICON_INFO.cs
@@ -0,0 +1,23 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_ICON_INFO (xTz). Toggles a buff icon display (buffId + display flag).</summary>
+public class SM_ICON_INFO : AionServerPacket
+{
+    private int buffId;
+    private bool display;
+
+    public SM_ICON_INFO(int buffId, bool display)
+    {
+        this.buffId = buffId;
+        this.display = display;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(0); // unk
+        WriteD(buffId);
+        WriteC(display ? 1 : 0);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LEARN_RECIPE.cs
@@ -0,0 +1,20 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LEARN_RECIPE (lord_rex). Confirms a learned recipe (recipeId).</summary>
+public class SM_LEARN_RECIPE : AionServerPacket
+{
+    private int recipeId;
+
+    public SM_LEARN_RECIPE(int recipeId)
+    {
+        this.recipeId = recipeId;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(recipeId);
+        WriteC(0); // 4.0
+    }
+}
