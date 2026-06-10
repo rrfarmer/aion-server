dd9cb40d95f1ff45d51cfa6dadb2a56ad3e28a51
rrfarmer <ryanfarmer@mac.com>
2026-06-10 13:01:01 -0400

    Port serverpackets SM_KEY + SM_L2AUTH_LOGIN_CHECK

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_KEY.cs
@@ -0,0 +1,12 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_KEY (-Nemesiss-). Sends the crypt-enable key. enableCryptKey()->EnableCryptKey() red-tolerated.</summary>
+public class SM_KEY : AionServerPacket
+{
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(con.EnableCryptKey());
+    }
+}
