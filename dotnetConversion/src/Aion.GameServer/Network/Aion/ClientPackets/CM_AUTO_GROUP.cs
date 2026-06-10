a69ef08bf921affad0be3ddaf310a95a970acc09
rrfarmer <ryanfarmer@mac.com>
2026-06-10 15:00:02 -0400

    Port clientpackets CM_AUTO_GROUP + CM_BONUS_TITLE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_AUTO_GROUP.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.Autogroup;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using Aion.GameServer.Services.Instance;
+using Aion.GameServer.Utils;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_AUTO_GROUP (Shepper, Guapo, nrg). Auto-group window actions (100 start / 101 cancel-reg / 102 enter / 103 cancel-enter / 104 icon). EntryRequestType/AutoGroupService/PeriodicInstanceManager red-tolerated.</summary>
+public class CM_AUTO_GROUP : AionClientPacket
+{
+    private int instanceMaskId;
+    private byte windowId;
+    private byte entryRequestId;
+
+    public CM_AUTO_GROUP(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        instanceMaskId = ReadD();
+        windowId = ReadC();
+        entryRequestId = ReadC();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (!AutoGroupConfig.AUTO_GROUP_ENABLE)
+        {
+            PacketSendUtility.SendMessage(player, "Auto Group is disabled");
+            return;
+        }
+        switch (windowId)
+        {
+            case 100:
+                EntryRequestType ert = EntryRequestType.GetTypeById(entryRequestId);
+                if (ert == null)
+                {
+                    return;
+                }
+                AutoGroupService.GetInstance().StartLooking(player, instanceMaskId, ert);
+                break;
+            case 101:
+                AutoGroupService.GetInstance().CancelRegistration(player, instanceMaskId);
+                break;
+            case 102:
+                AutoGroupService.GetInstance().PressEnter(player, instanceMaskId);
+                break;
+            case 103:
+                AutoGroupService.GetInstance().CancelEnter(player, instanceMaskId);
+                break;
+            case 104:
+                // is sent if a player clicks the icon
+                PeriodicInstanceManager.GetInstance().HandleRequest(player, instanceMaskId);
+                break;
+            case 105:
+                // DredgionRegService.getInstance().failedEnterDredgion(player);
+                break;
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_BONUS_TITLE.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_BONUS_TITLE (-Enomine-). Sets the player's bonus title (0xFFFF clears). Player red-tolerated.</summary>
+public class CM_BONUS_TITLE : AionClientPacket
+{
+    private int bonusTitleId;
+
+    public CM_BONUS_TITLE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        bonusTitleId = ReadUH();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (bonusTitleId != 0xFFFF)
+            if (!player.GetTitleList().Contains(bonusTitleId))
+                return;
+
+        player.GetTitleList().SetBonusTitle(bonusTitleId);
+    }
+}
