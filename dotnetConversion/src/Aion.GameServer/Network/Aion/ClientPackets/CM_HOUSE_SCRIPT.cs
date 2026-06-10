7bba19acd200059ad03c708c33cb45945741b5cf
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:33:20 -0400

    Port clientpackets CM_HOUSE_SETTINGS + CM_HOUSE_SCRIPT

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_HOUSE_SCRIPT.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.House;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_HOUSE_SCRIPT (Rolandas, Neon, Sykra). Uploads/removes a house script (sign script) with size limits. PlayerScripts/SM_HOUSE_SCRIPTS red-tolerated.</summary>
+public class CM_HOUSE_SCRIPT : AionClientPacket
+{
+    private int address;
+    private int scriptId;
+    private int totalSize;
+    private int compressedSize;
+    private int uncompressedSize;
+    private byte[] scriptContent;
+
+    public CM_HOUSE_SCRIPT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        address = ReadD();
+        scriptId = ReadUC();
+        totalSize = ReadUH();
+        if (totalSize > 0)
+        {
+            compressedSize = ReadD();
+            if (compressedSize <= SM_HOUSE_SCRIPTS.MAX_COMPRESSED_SCRIPT_SIZE)
+            {
+                uncompressedSize = ReadD();
+                scriptContent = ReadB(compressedSize);
+            }
+        }
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (compressedSize > SM_HOUSE_SCRIPTS.MAX_COMPRESSED_SCRIPT_SIZE)
+        {
+            PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_SCRIPT_OVERFLOW());
+            return;
+        }
+        House house = player.GetActiveHouse();
+        if (house == null || house.GetAddress().GetId() != address)
+        {
+            AuditLogger.Log(player, "tried to modify script of house they don't own (address " + address + ")");
+            return;
+        }
+        PlayerScripts scripts = house.GetPlayerScripts();
+        if (totalSize == 0)
+            scripts.Remove(scriptId);
+        else
+            scripts.Set(scriptId, scriptContent, uncompressedSize);
+        PacketSendUtility.BroadcastPacket(player, new SM_HOUSE_SCRIPTS(address, scripts.Get(scriptId)));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_HOUSE_SETTINGS.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.House;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_HOUSE_SETTINGS (Rolandas). Updates house door state / owner-name display / sign notice and notifies. AbstractHouseInfoPacket.SIGN_NOTICE_MAX_LENGTH; HouseDoorState red-tolerated.</summary>
+public class CM_HOUSE_SETTINGS : AionClientPacket
+{
+    private byte doorState;
+    private bool showOwnerName;
+    private string signNotice;
+
+    public CM_HOUSE_SETTINGS(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        doorState = ReadC();
+        showOwnerName = ReadC() == 1;
+        signNotice = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+            return;
+
+        if (signNotice.Length > AbstractHouseInfoPacket.SIGN_NOTICE_MAX_LENGTH)
+        { // client limits sign notices to 64 chars but technically it supports more
+            AuditLogger.Log(player, "sent string with more than 64 chars for house notice: " + signNotice);
+            signNotice = signNotice.Substring(0, AbstractHouseInfoPacket.SIGN_NOTICE_MAX_LENGTH);
+        }
+        HouseDoorState? doorState = HouseDoorStates.Get((sbyte)this.doorState);
+        House house = player.GetActiveHouse();
+        if (doorState == null)
+            NullLoggerFactory.Instance.CreateLogger(GetType_().Name).LogWarning("{Player} sent unknown door state {DoorState} for {House}", player, this.doorState, house);
+        else
+            house.SetDoorState(doorState.Value);
+        house.SetShowOwnerName(showOwnerName);
+        house.SetSignNotice(signNotice);
+
+        SendPacket(new SM_HOUSE_ACQUIRE(player.GetObjectId(), house.GetAddress().GetId(), true));
+        house.GetController().UpdateAppearance();
+
+        if (doorState == HouseDoorState.OPEN)
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_ORDER_OPEN_DOOR());
+        else if (doorState == HouseDoorState.CLOSED_EXCEPT_FRIENDS)
+        {
+            house.GetController().KickVisitors(player, false, false);
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_ORDER_CLOSE_DOOR_WITHOUT_FRIENDS());
+        }
+        else if (doorState == HouseDoorState.CLOSED)
+        {
+            house.GetController().KickVisitors(player, true, false);
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_ORDER_CLOSE_DOOR_ALL());
+        }
+    }
+}
