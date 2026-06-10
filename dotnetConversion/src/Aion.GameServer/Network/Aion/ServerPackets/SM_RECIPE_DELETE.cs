7368bacedd5e45958c7b4f0d538eb2ddc5366f92
rrfarmer <ryanfarmer@mac.com>
2026-06-10 13:26:48 -0400

    Port serverpackets SM_RIDE_ROBOT + SM_RECIPE_DELETE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_RECIPE_DELETE.cs
@@ -0,0 +1,19 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_RECIPE_DELETE (namedrisk). Removes a recipe from the client list (recipeId).</summary>
+public class SM_RECIPE_DELETE : AionServerPacket
+{
+    private int recipeId;
+
+    public SM_RECIPE_DELETE(int recipeId)
+    {
+        this.recipeId = recipeId;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(recipeId);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_RIDE_ROBOT.cs
@@ -0,0 +1,28 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_RIDE_ROBOT (Cheatkiller). Player mounts/dismounts a robot (objId + robotId). Player red-tolerated.</summary>
+public class SM_RIDE_ROBOT : AionServerPacket
+{
+    private int robotId;
+    private int objectId;
+
+    public SM_RIDE_ROBOT(Player player)
+        : this(player, player.GetRobotId())
+    {
+    }
+
+    public SM_RIDE_ROBOT(Player player, int robotId)
+    {
+        this.objectId = player.GetObjectId();
+        this.robotId = robotId;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(objectId);
+        WriteD(robotId);
+    }
+}
