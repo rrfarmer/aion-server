84b68d2a916f188f73479d9f9f84eb8798fa5e2d
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:12:43 -0400

    Port clientpackets CM_MACRO_CREATE + CM_MACRO_DELETE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_MACRO_CREATE.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services.Player;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_MACRO_CREATE (SoulKeeper). Stores a macro at a position and replies SM_MACRO_CREATED. PlayerService/SM_MACRO_RESULT red-tolerated.</summary>
+public class CM_MACRO_CREATE : AionClientPacket
+{
+    /// <summary>Macro number. Fist is 1, second is 2. Starting from 1, not from 0</summary>
+    private int macroPosition;
+
+    /// <summary>XML that represents the macro</summary>
+    private string macroXML;
+
+    public CM_MACRO_CREATE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        macroPosition = ReadUC();
+        macroXML = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        PlayerService.AddMacro(GetConnection().GetActivePlayer(), macroPosition, macroXML);
+        SendPacket(SM_MACRO_RESULT.SM_MACRO_CREATED);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_MACRO_DELETE.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services.Player;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_MACRO_DELETE (SoulKeeper). Removes a macro by list position (subsequent macros shift down) and replies SM_MACRO_DELETED. PlayerService/SM_MACRO_RESULT red-tolerated.</summary>
+public class CM_MACRO_DELETE : AionClientPacket
+{
+    /// <summary>Macro id that has to be deleted</summary>
+    private int macroPosition;
+
+    public CM_MACRO_DELETE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        macroPosition = ReadUC();
+    }
+
+    protected override void RunImpl()
+    {
+        PlayerService.RemoveMacro(GetConnection().GetActivePlayer(), macroPosition);
+        SendPacket(SM_MACRO_RESULT.SM_MACRO_DELETED);
+    }
+}
