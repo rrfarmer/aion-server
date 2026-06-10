76b12d3ebcdd8ab24c6ec2712205ecb3e7cfe66c
rrfarmer <ryanfarmer@mac.com>
2026-06-10 08:00:39 -0400

    Port services/siege: MercenaryLocation + BalaurAssaultService

---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/BalaurAssaultService.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.Commons.Utils;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.Assemblednpc;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Model.Templates.Assemblednpc;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Idfactory;
+using Aion.GameServer.World;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/BalaurAssaultService (synchro2, Luzien, Estrayl). Schedules/tracks Balaur assaults on fortresses (1-15 min) and artifacts (3-48 h): onSiegeStart/Finish, calculateFortressAssault (vulnerability/per-map limits/influence chance), startAssault, newAssault (build FortressAssault/ArtifactAssault), spawnDredgion (assembled-npc carrier broadcast). ConcurrentHashMap->ConcurrentDictionary; map.remove(k).call->TryRemove(out)+call; Siege<?>/Siege<? extends SiegeLocation>->Siege<SiegeLocation>; instanceof X x->is X x; getClass().getSimpleName()->GetType().Name; forEach->foreach; IllegalArgument->Argument. FortressAssault/FortressSiege/Influence/AssembledNpc red-tolerated.</summary>
+public class BalaurAssaultService
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger("SIEGE_LOG");
+    private readonly ConcurrentDictionary<int, FortressAssault> fortressAssaults = new ConcurrentDictionary<int, FortressAssault>();
+    private readonly ConcurrentDictionary<int, ArtifactAssault> artifactAssaults = new ConcurrentDictionary<int, ArtifactAssault>();
+
+    public static BalaurAssaultService GetInstance()
+    {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public void OnSiegeStart(Siege<SiegeLocation> siege)
+    {
+        if (siege is FortressSiege)
+        {
+            if (!CalculateFortressAssault(((FortressSiege)siege).GetSiegeLocation()))
+                return;
+            NewAssault(siege, Rnd.Get(60, 900)); // between 1 and 15 minutes
+        }
+        else if (siege is ArtifactSiege)
+        {
+            if (artifactAssaults.ContainsKey(siege.GetSiegeLocation().GetLocationId()) || siege.GetSiegeLocation().GetRace() == SiegeRace.BALAUR)
+                return;
+            NewAssault(siege, Rnd.Get(10800, 172800)); // between 3 and 48 hours
+        }
+    }
+
+    public void OnSiegeFinish(Siege<SiegeLocation> siege)
+    {
+        int locId = siege.GetSiegeLocationId();
+        bool isBossKilled = siege.IsBossKilled();
+        if (fortressAssaults.ContainsKey(locId))
+        {
+            fortressAssaults.TryRemove(locId, out FortressAssault fa);
+            fa.FinishAssault(isBossKilled);
+            if (isBossKilled && siege.GetSiegeLocation().GetRace().Equals(SiegeRace.BALAUR))
+                log.LogInformation(siege + " has been captured by Balaur assault!");
+            else
+                log.LogInformation(siege + " Balaur assault finished without capture!");
+        }
+        else if (artifactAssaults.ContainsKey(locId))
+        {
+            artifactAssaults.TryRemove(locId, out ArtifactAssault aa);
+            aa.FinishAssault(isBossKilled);
+            if (isBossKilled && siege.GetSiegeLocation().GetRace().Equals(SiegeRace.BALAUR))
+                log.LogInformation(siege + " has been captured by Balaur assault!");
+            else
+                log.LogInformation(siege + " Balaur assault finished without capture!");
+        }
+    }
+
+    private bool CalculateFortressAssault(FortressLocation fortress)
+    {
+        if (fortress.GetRace() == SiegeRace.BALAUR || !fortress.IsVulnerable())
+            return false;
+
+        bool isBalaurea = fortress.GetWorldId() == 210050000 || fortress.GetWorldId() == 220070000;
+
+        if (fortressAssaults.ContainsKey(fortress.GetLocationId()))
+            return false;
+
+        int count = 0;
+        foreach (FortressAssault fa in fortressAssaults.Values)
+        {
+            if (fa.GetWorldId() == fortress.GetWorldId())
+                count++;
+        }
+        if (count >= (isBalaurea ? 1 : 2)) // Allow only 2 Balaur attacks per map, 1 per Balaurea map
+            return false;
+
+        float influence = fortress.GetRace() == SiegeRace.ASMODIANS ? Influence.GetInstance().GetAsmodianInfluenceRate()
+            : Influence.GetInstance().GetElyosInfluenceRate();
+
+        return Rnd.Chance() < influence * 100f * SiegeConfig.BALAUR_ASSAULT_RATE;
+    }
+
+    public bool StartAssault(int location, int delay)
+    {
+        Siege<SiegeLocation> siege = SiegeService.GetInstance().GetSiege(location);
+        if (siege == null || fortressAssaults.ContainsKey(location) || artifactAssaults.ContainsKey(location))
+        {
+            return false;
+        }
+        NewAssault(siege, delay);
+        return true;
+    }
+
+    private void NewAssault(Siege<SiegeLocation> siege, int delay)
+    {
+        if (siege is FortressSiege fortressSiege)
+        {
+            FortressAssault assault = new FortressAssault(fortressSiege);
+            assault.StartAssault(delay);
+            fortressAssaults[siege.GetSiegeLocationId()] = assault;
+        }
+        else if (siege is ArtifactSiege artifactSiege)
+        {
+            ArtifactAssault assault = new ArtifactAssault(artifactSiege);
+            assault.StartAssault(delay);
+            artifactAssaults[siege.GetSiegeLocationId()] = assault;
+        }
+        else
+        {
+            throw new ArgumentException("Unsupported fortress siege type: " + siege.GetType().Name);
+        }
+        if (LoggingConfig.LOG_SIEGE)
+            log.LogInformation("Scheduled assault of " + siege + " in " + delay + " seconds");
+    }
+
+    public void SpawnDredgion(int spawnId)
+    {
+        AssembledNpcTemplate template = DataManager.ASSEMBLED_NPC_DATA.GetAssembledNpcTemplate(spawnId);
+        List<AssembledNpcPart> assembledParts = new List<AssembledNpcPart>();
+        foreach (var part in template.GetAssembledNpcPartTemplates())
+            assembledParts.Add(new AssembledNpcPart(IDFactory.GetInstance().NextId(), part));
+
+        AssembledNpc npc = new AssembledNpc(template.GetRouteId(), template.GetMapId(), template.GetLiveTime(), assembledParts);
+        World.GetInstance().ForEachPlayer(p =>
+        {
+            PacketSendUtility.SendPacket(p, new SM_NPC_ASSEMBLER(npc));
+            PacketSendUtility.SendPacket(p, SM_SYSTEM_MESSAGE.STR_ABYSS_CARRIER_SPAWN());
+        });
+    }
+
+    /// <summary>
+    /// Returns the FortressAssault object or null if none is active
+    /// </summary>
+    public FortressAssault GetFortressAssaultBySiegeId(int siegeId)
+    {
+        return fortressAssaults.GetValueOrDefault(siegeId);
+    }
+
+    private static class SingletonHolder
+    {
+        internal static readonly BalaurAssaultService INSTANCE = new BalaurAssaultService();
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Services/Siege/MercenaryLocation.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.Siege;
+using Aion.GameServer.Model.Templates.Siegelocation;
+using Aion.GameServer.Model.Templates.Spawns;
+using Aion.GameServer.Model.Templates.Spawns.Mercenaries;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using Aion.GameServer.Spawnengine;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Services.Siege;
+
+/// <summary>Java parity: services/siege/MercenaryLocation (Whoop). A purchasable mercenary spawn zone tied to a fortress siege: spawn (despawn old, spawn from MercenaryZone, announce), despawnCurrentMercs, isRequestValid (cooldown + &lt;50% alive), getSpawnZone (resolve merc zone by siege/race/zone id). currentTimeMillis->UtcNow; Race.getRaceByString(siegeRace.name())->GetRaceByString(ToString()); instanceof Npc->is Npc; forEachPlayer lambda. MercenaryZone/SiegeMercenaryZone/SpawnEngine red-tolerated.</summary>
+public class MercenaryLocation
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(MercenaryLocation));
+    private List<VisibleObject> spawnedMercs = new List<VisibleObject>();
+    private MercenaryZone spawns; // TODO: Change this to SpawnGroup
+    private SiegeMercenaryZone smz;
+    private Race race;
+    private long lastSpawn;
+    private int siegeId;
+
+    public MercenaryLocation(SiegeMercenaryZone template, SiegeRace race, int siegeId)
+    {
+        this.smz = template;
+        this.siegeId = siegeId;
+        this.race = Race.GetRaceByString(race.ToString());
+        if (this.race != null)
+            spawns = GetSpawnZone();
+    }
+
+    public void Spawn()
+    {
+        DespawnCurrentMercs();
+        if (spawns == null)
+            return;
+        List<VisibleObject> mercs = new List<VisibleObject>();
+        foreach (Spawn spawn in spawns.GetSpawns())
+        {
+            foreach (SpawnSpotTemplate sst in spawn.GetSpawnSpotTemplates())
+            {
+                SpawnTemplate spawnTemplate = SpawnEngine.NewSiegeSpawn(spawns.GetWorldId(), spawn.GetNpcId(), siegeId, SiegeRace.GetByRace(race),
+                    SiegeModType.SIEGE, sst.GetX(), sst.GetY(), sst.GetZ(), sst.GetHeading());
+                spawnTemplate.SetStaticId(sst.GetStaticId());
+                VisibleObject newMerc = SpawnEngine.SpawnObject(spawnTemplate, 1);
+                mercs.Add(newMerc);
+            }
+        }
+        spawnedMercs.AddRange(mercs);
+        lastSpawn = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        SiegeService.GetInstance().GetFortress(siegeId).ForEachPlayer(p => PacketSendUtility.SendPacket(p, new SM_SYSTEM_MESSAGE(smz.GetAnnounceId())));
+    }
+
+    public void DespawnCurrentMercs()
+    {
+        if (spawnedMercs.Count == 0)
+            return;
+        foreach (VisibleObject merc in spawnedMercs)
+        {
+            merc.GetController().DeleteIfAliveOrCancelRespawn();
+        }
+        spawnedMercs.Clear();
+    }
+
+    /// <summary>
+    /// Returns true if cooldown is expired and enough mercs are dead
+    /// </summary>
+    public bool IsRequestValid()
+    {
+        return spawns != null && (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - lastSpawn) > smz.GetCooldown() && !HasEnoughMercsAlive();
+    }
+
+    /// <summary>
+    /// Check if enough mercs are still alive. Returns false if 50% are alive.
+    /// </summary>
+    private bool HasEnoughMercsAlive()
+    {
+        int totalMercs = spawnedMercs.Count;
+        int livingMercs = 0;
+        foreach (VisibleObject vo in spawnedMercs)
+        {
+            if (vo is Npc)
+            {
+                if (vo.IsSpawned() && !((Npc)vo).IsDead())
+                    livingMercs++;
+            }
+        }
+        return livingMercs < (totalMercs / 2);
+    }
+
+    private MercenaryZone GetSpawnZone()
+    {
+        MercenaryZone tempZone = null;
+        MercenarySpawn spawn = DataManager.SPAWNS_DATA.GetMercenarySpawnBySiegeId(siegeId);
+        if (spawn == null)
+        {
+            log.LogError("[MERC] There is no mercenaries spawns for siege " + siegeId + " and zone" + smz.GetId());
+            return tempZone;
+        }
+        MercenaryRace targetRace = null;
+        foreach (MercenaryRace mrace in spawn.GetMercenaryRaces())
+        {
+            if (mrace.GetRace() == race)
+            {
+                targetRace = mrace;
+                break;
+            }
+        }
+        if (targetRace == null)
+        {
+            log.LogError("[MERC] There is no mercenary race for siege " + siegeId + ", zone" + smz.GetId() + ", race:" + race.ToString());
+            return tempZone;
+        }
+        foreach (MercenaryZone mzone in targetRace.GetMercenaryZones())
+        {
+            if (mzone.GetZoneId() == smz.GetId())
+            {
+                tempZone = mzone;
+                break;
+            }
+        }
+        if (tempZone == null)
+        {
+            log.LogError("[MERC] There is no mercenary zone for siege " + siegeId + ", zone" + smz.GetId() + ", race:" + race.ToString());
+            return tempZone;
+        }
+        return tempZone;
+    }
+
+    public int GetCosts()
+    {
+        return smz.GetCosts();
+    }
+
+    public int GetMsgId()
+    {
+        return smz.GetMsgId();
+    }
+}
