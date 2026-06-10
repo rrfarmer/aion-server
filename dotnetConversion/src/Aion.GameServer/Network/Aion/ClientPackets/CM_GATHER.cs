41020caea618bedd5fae04ddc98c16f6836a741d
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:10:51 -0400

    Port clientpackets CM_GATHER + CM_INVITE_TO_GROUP

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_GATHER.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_GATHER (ATracer). Starts (-1 cancel / 0,128 start) gathering from a Gatherable target. Gatherable/AuditLogger red-tolerated.</summary>
+public class CM_GATHER : AionClientPacket
+{
+    private int actionId;
+
+    public CM_GATHER(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        actionId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        switch (actionId)
+        {
+            case -1:
+                CancelGathering(player);
+                break;
+            case 0:
+            case 128:
+                StartGathering(player); // 128 is sent when using /attack chat command
+                break;
+            default:
+                NullLoggerFactory.Instance.CreateLogger(GetType_().Name).LogWarning("Unhandled gathering action ID {ActionId} (sent by {Player} at {Position})", actionId, player, player.GetPosition());
+                break;
+        }
+    }
+
+    private void StartGathering(Player player)
+    {
+        if (player.GetTarget() is Gatherable gatherable)
+            gatherable.GetController().StartGathering(player);
+        else
+            AuditLogger.Log(player, "tried to gather from " + player.GetTarget());
+    }
+
+    private void CancelGathering(Player player)
+    {
+        // player can switch targets during gathering, so the target is not guaranteed to be the correct gatherable
+        Gatherable gatherable = player.GetTarget() is Gatherable g && g.GetController().GetGatheringPlayerId() == player.GetObjectId() ? g : null;
+        if (gatherable == null)
+        {
+            gatherable = player.GetKnownList()
+                .Where(o => o.Get() is Gatherable gg && gg.GetController().GetGatheringPlayerId() == player.GetObjectId())
+                .Select(o => (Gatherable)o.Get())
+                .FirstOrDefault();
+        }
+        if (gatherable != null)
+            gatherable.GetController().CancelGathering();
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_INVITE_TO_GROUP.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Alliance;
+using Aion.GameServer.Model.Team.Group;
+using Aion.GameServer.Model.Team.League;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_INVITE_TO_GROUP (Lyahim, ATracer, Simple, Neon). Invites a player to group (0) / alliance (12) / league (28) with dead/offline/deny guards. PlayerGroupService/PlayerAllianceService/LeagueService red-tolerated.</summary>
+public class CM_INVITE_TO_GROUP : AionClientPacket
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(CM_INVITE_TO_GROUP));
+    private string playerName;
+    private int inviteType;
+
+    public CM_INVITE_TO_GROUP(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        inviteType = ReadUC();
+        playerName = ReadS();
+    }
+
+    protected override void RunImpl()
+    {
+        Player inviter = GetConnection().GetActivePlayer();
+        if (inviter.IsDead())
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_PARTY_CANT_INVITE_WHEN_DEAD());
+            return;
+        }
+
+        Player invited = World.GetInstance().GetPlayer(ChatUtil.GetRealCharName(playerName));
+        if (invited == null)
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_NO_SUCH_USER(playerName));
+            return;
+        }
+
+        if (invited.GetPlayerSettings().IsInDeniedStatus(DeniedStatus.GROUP))
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_REJECTED_INVITE_PARTY(invited.GetName(true)));
+            return;
+        }
+
+        switch (inviteType)
+        {
+            case 0:
+                PlayerGroupService.InviteToGroup(inviter, invited);
+                break;
+            case 12: // 2.5
+                PlayerAllianceService.InviteToAlliance(inviter, invited);
+                break;
+            case 28:
+                LeagueService.InviteToLeague(inviter, invited);
+                break;
+            default:
+                log.LogWarning("Received unknown invite type from player " + inviter.GetName() + ": " + inviteType);
+                break;
+        }
+    }
+}
