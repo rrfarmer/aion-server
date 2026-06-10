0681cfb341afb450b9d02a7c3492787e4c41f336
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:39:17 -0400

    Port clientpackets CM_HOUSE_DECORATE + CM_REGISTER_HOUSE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_HOUSE_DECORATE.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.House;
+using Aion.GameServer.Model.Templates.Housing;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.QuestEngine;
+using Aion.GameServer.QuestEngine.Model;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_HOUSE_DECORATE (Rolandas). Applies/clears a wall/floor/etc decoration part by line number. PartType/HouseDecoration/SM_HOUSE_EDIT red-tolerated.</summary>
+public class CM_HOUSE_DECORATE : AionClientPacket
+{
+    private int objectId;
+    private int lineNo, roomNo; // Line number (starts from 1 in 3.0 and from 2 in 3.5) of part in House render/update packet
+
+    public CM_HOUSE_DECORATE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        objectId = ReadD();
+        ReadD(); // templateId (already known by objectId)
+        lineNo = ReadUH();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+            return;
+
+        House house = player.GetActiveHouse();
+
+        PartType? partType = PartTypeExtensions.GetForLineNr(lineNo);
+        if (partType == null) // client may send lineNos which are not even implemented on client side (like 20-26)
+            return;
+        int roomNo = lineNo - partType.Value.GetStartLineNr();
+
+        if (objectId == 0)
+        { // change appearance to default and delete any applied custom decor
+            house.GetRegistry().DiscardDecor(partType.Value, roomNo);
+        }
+        else
+        { // apply decor and remove it from registry
+            HouseDecoration decor = house.GetRegistry().GetDecorByObjId(objectId);
+            house.GetRegistry().SetUsed(decor, roomNo);
+            SendPacket(new SM_HOUSE_EDIT(4, 2, objectId)); // yes, in retail it's sent twice!
+        }
+
+        SendPacket(new SM_HOUSE_EDIT(4, 2, objectId));
+        house.GetController().UpdateAppearance();
+        QuestEngine.GetInstance().OnHouseItemUseEvent(new QuestEnv(null, player, 0));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_REGISTER_HOUSE.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.House;
+using Aion.GameServer.Model.Templates.Housing;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_REGISTER_HOUSE (Rolandas). Registers the player's house for auction with a starting bid (charges a registration fee). HousingBidService red-tolerated.</summary>
+public class CM_REGISTER_HOUSE : AionClientPacket
+{
+    private long bidKinah;
+
+    public CM_REGISTER_HOUSE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        bidKinah = ReadQ();
+        ReadQ(); // always 100000
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (!HousingBidService.GetInstance().IsRegisteringAllowed())
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_CANT_AUCTION_TIMEOUT());
+            return;
+        }
+
+        House house = player.GetActiveHouse();
+        if (house == null || house.GetHouseType() == HouseType.STUDIO)
+            return; // should not happen
+
+        if (house.GetBids() != null)
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_AUCTION_FAIL_ALREADY_REGISTED());
+            return;
+        }
+
+        if (!house.IsFeePaid() && HousingConfig.ENABLE_HOUSE_PAY)
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_CANT_AUCTION_OVERDUE());
+            return;
+        }
+
+        long fee = (long)(bidKinah * HousingConfig.AUCTION_REGISTRATION_FEE_PERCENT);
+
+        if (!player.GetInventory().TryDecreaseKinah(fee))
+        {
+            // client has it's own validation, so we only get here if AUCTION_REGISTRATION_FEE_PERCENT is higher than the default in the client (30%)
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_NOT_ENOUGH_KINA(fee));
+            return;
+        }
+        if (HousingBidService.GetInstance().Auction(house, bidKinah))
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_AUCTION_MY_HOUSE(house.GetAddress().GetId()));
+            SendPacket(new SM_RECEIVE_BIDS(0));
+        }
+        else
+        {
+            SendPacket(SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_CANT_AUCTION_TIMEOUT());
+            player.GetInventory().IncreaseKinah(fee);
+        }
+    }
+}
