03b20cbb61c8460c19f5fa9736be84291bc55a81
rrfarmer <ryanfarmer@mac.com>
2026-06-10 15:03:01 -0400

    Port clientpackets CM_BREAK_WEAPONS + CM_CHANGE_CHANNEL

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_BREAK_WEAPONS.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_BREAK_WEAPONS (zdead). Decompound (defuse) a fused weapon at an armsfusion officer. DialogAction/ArmsfusionService/AuditLogger red-tolerated.</summary>
+public class CM_BREAK_WEAPONS : AionClientPacket
+{
+    public CM_BREAK_WEAPONS(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    private int npcObjId;
+    private int weaponObjId;
+
+    protected override void ReadImpl()
+    {
+        npcObjId = ReadD();
+        weaponObjId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player.IsTargetingNpcWithFunction(npcObjId, DialogAction.DECOMPOUND_WEAPON))
+            ArmsfusionService.BreakWeapons(player, weaponObjId);
+        else
+            AuditLogger.Log(player, "tried to defuse a weapon without targeting an armsfusion officer");
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_CHANGE_CHANNEL.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Aion.GameServer.Configs.Main;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.World;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Teleport;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_CHANGE_CHANNEL (ATracer). Requests a world-channel change (FastTrack twin-count offset). WorldMapInstance/TeleportService red-tolerated.</summary>
+public class CM_CHANGE_CHANNEL : AionClientPacket
+{
+    private int channel;
+
+    public CM_CHANGE_CHANNEL(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        channel = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player activePlayer = GetConnection().GetActivePlayer();
+        WorldMapInstance instance = activePlayer.GetPosition().GetWorldMapInstance();
+        if (WorldConfig.WORLD_EMULATE_FASTTRACK && !instance.IsBeginnerInstance())
+        {
+            WorldMapTemplate template = instance.GetTemplate();
+            // channel index starts from there
+            channel += template.GetTwinCount() - 1;
+        }
+        TeleportService.ChangeChannel(activePlayer, channel);
+    }
+}
