f85ff5ae9c1ecf4051bac86fe93ce668b40d4a9e
rrfarmer <ryanfarmer@mac.com>
2026-06-10 17:41:47 -0400

    Port clientpackets CM_INSTANCE_INFO + CM_INSTANCE_LEAVE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_INSTANCE_INFO.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils.Collections;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_INSTANCE_INFO (nrg, Neon). Sends instance score info for the team leader and (updateType 1) team members in groups of 3. SM_INSTANCE_INFO/SplitList red-tolerated.</summary>
+public class CM_INSTANCE_INFO : AionClientPacket
+{
+    private byte updateType; // 0 = reset to client default values and overwrite, 1 = update team member info, 2 = overwrite only
+
+    public CM_INSTANCE_INFO(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        ReadD(); // unk (always 0)
+        updateType = ReadC();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        Player firstObject = player.IsInTeam() ? player.GetCurrentTeam().GetLeaderObject() : player; // always the team leader
+        SendPacket(new SM_INSTANCE_INFO(updateType, firstObject));
+        if (updateType == 1 && player.IsInTeam())
+        {
+            List<Player> filteredTeamMembers = player.GetCurrentTeam().FilterMembers(Predicates.Players.AllExcept(firstObject));
+            SplitList<Player> playersSplitList = new FixedElementCountSplitList<Player>(filteredTeamMembers, false, 3);
+            foreach (var part in playersSplitList)
+                SendPacket(new SM_INSTANCE_INFO((byte)2, part));
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_INSTANCE_LEAVE.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_INSTANCE_LEAVE (xTz). Asks the instance handler to remove the player from the current instance. WorldMapInstance/InstanceHandler red-tolerated.</summary>
+public class CM_INSTANCE_LEAVE : AionClientPacket
+{
+    public CM_INSTANCE_LEAVE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        // nothing to read
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player.IsInInstance())
+        {
+            player.GetPosition().GetWorldMapInstance().GetInstanceHandler().LeaveInstance(player);
+        }
+    }
+}
