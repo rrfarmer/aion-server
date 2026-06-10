ad40dfe6c674399780b1991eb9e1bb01f70ce7cc
rrfarmer <ryanfarmer@mac.com>
2026-06-10 16:54:48 -0400

    Port clientpackets CM_TUNE + CM_TUNE_RESULT

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TUNE.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.Item.Actions;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Item;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TUNE (xTz). Identifies an unidentified item or applies a tuning scroll. ItemActionService/TuningAction red-tolerated.</summary>
+public class CM_TUNE : AionClientPacket
+{
+    private int itemObjectId, tuningScrollObjectId;
+
+    public CM_TUNE(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        itemObjectId = ReadD();
+        tuningScrollObjectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+            return;
+
+        Item item = player.GetInventory().GetItemByObjId(itemObjectId);
+        if (item == null)
+            return;
+
+        if (!item.IsIdentified())
+        {
+            ItemActionService.IdentifyItem(player, item);
+        }
+        else if (tuningScrollObjectId != 0)
+        {
+            Item tuningScroll = player.GetInventory().GetItemByObjId(tuningScrollObjectId);
+            if (tuningScroll == null)
+                return;
+
+            TuningAction action = tuningScroll.GetItemTemplate().GetActions().GetTuningAction();
+            if (action != null && action.CanAct(player, tuningScroll, item))
+                action.Act(player, tuningScroll, item);
+        }
+        else
+        {
+            AuditLogger.Log(player, "attempted to tune an already identified item without tuning scroll.");
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TUNE_RESULT.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services.Item;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TUNE_RESULT (Estrayl). Accepts/rejects a pending re-identification result. ItemActionService/SM_INVENTORY_UPDATE_ITEM red-tolerated.</summary>
+public class CM_TUNE_RESULT : AionClientPacket
+{
+    private int itemObjectId;
+    private bool hasAccepted;
+
+    public CM_TUNE_RESULT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        itemObjectId = ReadD();
+        hasAccepted = ReadC() == 1;
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        Item itemToTune = player.GetInventory().GetItemByObjId(itemObjectId);
+        if (itemToTune != null)
+        {
+            bool auditInvalidEvent = !hasAccepted && itemToTune.GetPendingTuneResult() != null && itemToTune.GetPendingTuneResult().IsAttributeOnly();
+            if (hasAccepted || auditInvalidEvent)
+            {
+                if (auditInvalidEvent)
+                    AuditLogger.Log(player, "tried to cancel a attribute re-identification which is not possible by default");
+                ItemActionService.ApplyTuneResult(player, itemToTune);
+                PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_ITEM_REIDENTIFY_APPLY_YES(itemToTune.GetL10n()));
+            }
+            else
+            {
+                itemToTune.SetPendingTuneResult(null);
+                PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_ITEM_REIDENTIFY_APPLY_NO());
+            }
+            PacketSendUtility.SendPacket(player, new SM_INVENTORY_UPDATE_ITEM(player, itemToTune));
+        }
+    }
+}
