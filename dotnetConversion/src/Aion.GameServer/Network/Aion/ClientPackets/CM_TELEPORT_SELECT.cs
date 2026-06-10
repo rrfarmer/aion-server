1cefa1356fe8af5cfcdb0f1efaf5988864ca8752
rrfarmer <ryanfarmer@mac.com>
2026-06-10 18:12:50 -0400

    Port clientpackets CM_USE_HOUSE_OBJECT + CM_TELEPORT_SELECT

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_TELEPORT_SELECT.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.Animations;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Templates.Teleport;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Services.Teleport;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Audit;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_TELEPORT_SELECT (ATracer, orz, KID). Teleports the player to a selected location via a teleporter NPC. TeleportService/TeleporterTemplate red-tolerated.</summary>
+public class CM_TELEPORT_SELECT : AionClientPacket
+{
+    /// <summary>NPC object ID</summary>
+    private int targetObjId;
+
+    /// <summary>Destination of teleport</summary>
+    private int locId;
+
+    public CM_TELEPORT_SELECT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        targetObjId = ReadD();
+        locId = ReadD(); // locationId
+        ReadH();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player.IsDead())
+            return;
+
+        AionObject obj = player.GetKnownList().GetObject(targetObjId);
+        if (!(obj is Npc npc))
+        {
+            if (obj == null)
+                obj = World.GetInstance().FindVisibleObject(targetObjId);
+            AuditLogger.Log(player, "tried to teleport to locId " + locId + " via " + (obj == null ? "unknown npc (objId " + targetObjId + ")" : obj)
+                + " at " + player.GetPosition());
+            return;
+        }
+        TeleporterTemplate template = TeleportService.ValidateTeleporterAndGetTemplate(player, npc);
+        if (template == null)
+            return;
+        TeleportLocation location = template.GetTeleLocIdData().GetTeleportLocation(locId);
+        if (location == null)
+        {
+            AuditLogger.Log(player, "tried to teleport to invalid locId " + locId + " via " + npc + " at " + player.GetPosition());
+            PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_CANNOT_MOVE_TO_AIRPORT_NO_ROUTE());
+            return;
+        }
+        TeleportService.Teleport(player, location, npc.HasStatic() ? TeleportAnimation.JUMP_IN_STATUE : TeleportAnimation.JUMP_IN);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_USE_HOUSE_OBJECT.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_USE_HOUSE_OBJECT (Rolandas). Triggers a house object's dialog. HouseObject&lt;?&gt; -> &lt;PlaceableHouseObject&gt;. World red-tolerated.</summary>
+public class CM_USE_HOUSE_OBJECT : AionClientPacket
+{
+    int itemObjectId;
+
+    public CM_USE_HOUSE_OBJECT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        itemObjectId = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (player == null)
+            return;
+
+        VisibleObject visObject = World.GetInstance().FindVisibleObject(itemObjectId);
+        if (visObject == null)
+            return;
+        if (visObject is HouseObject<PlaceableHouseObject>)
+        {
+            ((HouseObject<PlaceableHouseObject>)visObject).GetController().OnDialogRequest(player);
+        }
+    }
+}
