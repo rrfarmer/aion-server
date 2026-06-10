5fc128a1493b7491351d85b2cf2f4d4d97d950c2
rrfarmer <ryanfarmer@mac.com>
2026-06-10 18:21:50 -0400

    Port clientpackets CM_POSITION_SELF + CM_MOVE_IN_AIR

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_MOVE_IN_AIR.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.GameObjects.State;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.World;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_MOVE_IN_AIR (-Nemesiss-, Sweetkr, KID). Player flying-teleport movement update. CreatureState PascalCase; World red-tolerated.</summary>
+public class CM_MOVE_IN_AIR : AionClientPacket
+{
+    private int worldId;
+    private float x, y, z;
+    private byte heading;
+    private int distance;
+
+    public CM_MOVE_IN_AIR(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        worldId = ReadD();
+        x = ReadF();
+        y = ReadF();
+        z = ReadF();
+        heading = ReadC();
+        distance = ReadD();
+    }
+
+    protected override void RunImpl()
+    {
+        Player player = GetConnection().GetActivePlayer();
+        if (!player.IsSpawned())
+            return;
+        if (!player.IsInState(CreatureState.Flying))
+            return;
+
+        if (player.GetFlightPath() != null)
+            player.GetFlightPath().SetDistance(distance);
+
+        if (player.IsProtectionActive())
+            player.GetController().StopProtectionActiveTask();
+
+        World.GetInstance().UpdatePosition(player, x, y, z, heading);
+        player.GetMoveController().OnMoveFromClient();
+        player.GetController().OnMove();
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_POSITION_SELF.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Aion.GameServer.Network.Aion;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_POSITION_SELF. Client reply to SM_POSITION_SELF; no-op.</summary>
+public class CM_POSITION_SELF : AionClientPacket
+{
+    public CM_POSITION_SELF(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+    }
+
+    protected override void RunImpl()
+    {
+    }
+}
