36fc9e862a8445eff5dd9a7b12bceece158fcad9
rrfarmer <ryanfarmer@mac.com>
2026-06-10 12:17:41 -0400

    Port serverpackets SM_LEGION_SEND_EMBLEM_DATA + SM_LEGION_UPDATE_TITLE

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LEGION_SEND_EMBLEM_DATA.cs
@@ -0,0 +1,22 @@
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LEGION_SEND_EMBLEM_DATA (cura). Streams legion emblem byte data (size + bytes), following SM_LEGION_SEND_EMBLEM.</summary>
+public class SM_LEGION_SEND_EMBLEM_DATA : AionServerPacket
+{
+    private int size;
+    private byte[] data;
+
+    public SM_LEGION_SEND_EMBLEM_DATA(int size, byte[] data)
+    {
+        this.size = size;
+        this.data = data;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(size);
+        WriteB(data);
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ServerPackets/SM_LEGION_UPDATE_TITLE.cs
@@ -0,0 +1,29 @@
+using Aion.GameServer.Model.Team.Legion;
+using Aion.GameServer.Network.Aion;
+
+namespace Aion.GameServer.Network.Aion.Serverpackets;
+
+/// <summary>Java parity: network/aion/serverpackets/SM_LEGION_UPDATE_TITLE (sweetkr). Updates a player's legion title (objId/legionId/name/rank). LegionRank red-tolerated.</summary>
+public class SM_LEGION_UPDATE_TITLE : AionServerPacket
+{
+    private readonly int playerObjectId;
+    private readonly int legionId;
+    private readonly string legionName;
+    private readonly LegionRank rank;
+
+    public SM_LEGION_UPDATE_TITLE(int playerObjectId, int legionId, string legionName, LegionRank rank)
+    {
+        this.playerObjectId = playerObjectId;
+        this.legionId = legionId;
+        this.legionName = legionName;
+        this.rank = rank;
+    }
+
+    protected override void WriteImpl(AionConnection con)
+    {
+        WriteD(playerObjectId);
+        WriteD(legionId);
+        WriteS(legionName);
+        WriteC(rank.GetRankId());
+    }
+}
