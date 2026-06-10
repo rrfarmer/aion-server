34f11a5813f074d6281b3ffcec5881a3ccb7e399
rrfarmer <ryanfarmer@mac.com>
2026-06-10 15:32:23 -0400

    Port AbstractCharacterEditPacket + CM_CHARACTER_EDIT

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/AbstractCharacterEditPacket.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using Aion.GameServer.Model;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Utils;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/AbstractCharacterEditPacket (Neon). Base for character create/edit packets; reads name/gender/race/class and the full appearance block. PlayerAppearance/PlayerClass red-tolerated.</summary>
+public abstract class AbstractCharacterEditPacket : AionClientPacket
+{
+    protected string characterName;
+    protected Gender gender;
+    protected Race race;
+    protected PlayerClass? playerClass;
+    protected PlayerAppearance playerAppearance;
+
+    public AbstractCharacterEditPacket(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected void ReadBasicInfo(bool ignoreInvalidPlayerClass)
+    {
+        characterName = Util.ConvertName(ReadS(25)); // client leaks random data here when entering char creation screen for the first time
+        gender = ReadD() == 0 ? Gender.MALE : Gender.FEMALE;
+        race = ReadD() == 0 ? Race.ELYOS : Race.ASMODIANS;
+        playerClass = PlayerClass.GetPlayerClassById((byte)ReadD(), ignoreInvalidPlayerClass);
+    }
+
+    protected void ReadAppearance()
+    {
+        playerAppearance = new PlayerAppearance();
+
+        playerAppearance.SetVoice(ReadD());
+        playerAppearance.SetSkinRGB(ReadD());
+        playerAppearance.SetHairRGB(ReadD());
+        playerAppearance.SetEyeRGB(ReadD());
+        playerAppearance.SetLipRGB(ReadD());
+        playerAppearance.SetFace(ReadUC());
+        playerAppearance.SetHair(ReadUC());
+        playerAppearance.SetDeco(ReadUC());
+        playerAppearance.SetTattoo(ReadUC());
+        playerAppearance.SetFaceContour(ReadUC());
+        playerAppearance.SetExpression(ReadUC());
+        ReadC(); // always 4 o0 // 5 in 1.5.x
+        playerAppearance.SetJawLine(ReadUC());
+        playerAppearance.SetForehead(ReadUC());
+
+        playerAppearance.SetEyeHeight(ReadUC());
+        playerAppearance.SetEyeSpace(ReadUC());
+        playerAppearance.SetEyeWidth(ReadUC());
+        playerAppearance.SetEyeSize(ReadUC());
+        playerAppearance.SetEyeShape(ReadUC());
+        playerAppearance.SetEyeAngle(ReadUC());
+
+        playerAppearance.SetBrowHeight(ReadUC());
+        playerAppearance.SetBrowAngle(ReadUC());
+        playerAppearance.SetBrowShape(ReadUC());
+
+        playerAppearance.SetNose(ReadUC());
+        playerAppearance.SetNoseBridge(ReadUC());
+        playerAppearance.SetNoseWidth(ReadUC());
+        playerAppearance.SetNoseTip(ReadUC());
+
+        playerAppearance.SetCheek(ReadUC());
+        playerAppearance.SetLipHeight(ReadUC());
+        playerAppearance.SetMouthSize(ReadUC());
+        playerAppearance.SetLipSize(ReadUC());
+        playerAppearance.SetSmile(ReadUC());
+        playerAppearance.SetLipShape(ReadUC());
+        playerAppearance.SetJawHeigh(ReadUC());
+        playerAppearance.SetChinJut(ReadUC());
+        playerAppearance.SetEarShape(ReadUC());
+        playerAppearance.SetHeadSize(ReadUC());
+
+        playerAppearance.SetNeck(ReadUC());
+        playerAppearance.SetNeckLength(ReadUC());
+
+        playerAppearance.SetShoulderSize(ReadUC());
+
+        playerAppearance.SetTorso(ReadUC());
+        playerAppearance.SetChest(ReadUC()); // only woman
+        playerAppearance.SetWaist(ReadUC());
+        playerAppearance.SetHips(ReadUC());
+
+        playerAppearance.SetArmThickness(ReadUC());
+
+        playerAppearance.SetHandSize(ReadUC());
+        playerAppearance.SetLegThickness(ReadUC());
+
+        playerAppearance.SetFootSize(ReadUC());
+        playerAppearance.SetFacialRate(ReadUC());
+
+        ReadC(); // always 0
+        playerAppearance.SetArmLength(ReadUC());
+        playerAppearance.SetLegLength(ReadUC()); // wrong??
+        playerAppearance.SetShoulders(ReadUC()); // 1.5.x May be ShoulderSize
+        playerAppearance.SetFaceShape(ReadUC());
+        ReadC();
+        ReadC();
+        ReadC();
+        playerAppearance.SetHeight(ReadF());
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/ClientPackets/CM_CHARACTER_EDIT.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Aion.GameServer.Dao;
+using Aion.GameServer.Model.Account;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Network.Aion;
+using Aion.GameServer.Services.Player;
+using Aion.GameServer.Utils.Audit;
+using State = Aion.GameServer.Network.Aion.AionConnection.State;
+
+namespace Aion.GameServer.Network.Aion.Clientpackets;
+
+/// <summary>Java parity: network/aion/clientpackets/CM_CHARACTER_EDIT (IlBuono, Neon). Client requests plastic-surgery / gender-switch edit of an existing character (in edit mode), consuming the appropriate ticket. PlayerEnterWorldService/PlayerAppearanceDAO red-tolerated.</summary>
+public class CM_CHARACTER_EDIT : AbstractCharacterEditPacket
+{
+    private int objectId;
+
+    public CM_CHARACTER_EDIT(int opcode, ISet<State> validStates)
+        : base(opcode, validStates)
+    {
+    }
+
+    protected override void ReadImpl()
+    {
+        objectId = ReadD();
+        ReadBasicInfo(false);
+        ReadAppearance();
+    }
+
+    protected override void RunImpl()
+    {
+        AionConnection client = GetConnection();
+        PlayerAccountData playerAccData = client.GetAccount().GetPlayerAccountData(objectId);
+        if (playerAccData == null || !playerAccData.GetPlayerCommonData().IsInEditMode())
+            return;
+        PlayerEnterWorldService.EnterWorld(client, objectId);
+        Player player = client.GetActivePlayer();
+
+        bool isGenderSwitch = player.GetGender() != gender;
+        if (CheckOrRemoveTicket(player, isGenderSwitch, true))
+        {
+            bool spawnedBeforeAttributesChanged = player.IsSpawned(); // just in case CM_LEVEL_READY was sent early
+            if (isGenderSwitch)
+                player.GetCommonData().SetGender(gender); // no need to save gender here, will be saved periodically and on logout
+            player.SetPlayerAppearance(playerAppearance);
+            PlayerAppearanceDAO.Store(player); // save new appearance
+            if (spawnedBeforeAttributesChanged)
+                player.GetController().OnChangedPlayerAttributes();
+        }
+        else
+        { // can only happen if you illegally enter the character edit screen
+            AuditLogger.Log(player, "tried to apply their plastic surgery without a ticket.");
+        }
+    }
+
+    public static bool CheckOrRemoveTicket(Player player, bool isGenderSwitch, bool removeTicket)
+    {
+        int[] ticketIds = isGenderSwitch ? new int[] { 169660000, 169660001, 169660002, 169660003, 169660004 } : new int[] { 169650000, 169650001, 169650002, 169650003, 169650004, 169650005, 169650006, 169650007, 169650008 };
+        foreach (int ticketId in ticketIds)
+        {
+            if (removeTicket && player.GetInventory().DecreaseByItemId(ticketId, 1) || !removeTicket && player.GetInventory().GetItemCountByItemId(ticketId) > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
