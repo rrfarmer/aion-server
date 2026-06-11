e406e15e61dc1e1646017098a7851a2b0b0033fc
rrfarmer <ryanfarmer@mac.com>
2026-06-10 21:09:14 -0400

    Port instanceinfo EternalBastion/LegionDominion/TheShugoEmperorsVault ScoreWriters

---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/Instanceinfo/EternalBastionScoreWriter.cs
@@ -0,0 +1,33 @@
+using Aion.GameServer.Commons.Nio;
+using Aion.GameServer.Model.Instance.Instancescore;
+
+namespace Aion.GameServer.Network.Aion.Instanceinfo;
+
+/// <summary>
+/// Java parity: network/aion/instanceinfo/EternalBastionScoreWriter (xTz, Estrayl) : InstanceScoreWriter&lt;NormalScore&gt;. Writes
+/// points/rank/AP + 4 reward item/count pairs. NormalScore red-tolerated.
+/// </summary>
+public class EternalBastionScoreWriter : InstanceScoreWriter<NormalScore>
+{
+    public EternalBastionScoreWriter(NormalScore reward)
+        : base(reward)
+    {
+    }
+
+    protected override void WriteMe(ByteBuffer buf)
+    {
+        WriteD(buf, instanceScore.GetPoints());
+        WriteQ(buf, 0); // unk
+        WriteD(buf, instanceScore.GetRank());
+        WriteD(buf, 0); // unk
+        WriteD(buf, instanceScore.GetFinalAp());
+        WriteD(buf, instanceScore.GetRewardItem1());
+        WriteD(buf, instanceScore.GetRewardItem1Count());
+        WriteD(buf, instanceScore.GetRewardItem2());
+        WriteD(buf, instanceScore.GetRewardItem2Count());
+        WriteD(buf, instanceScore.GetRewardItem3());
+        WriteD(buf, instanceScore.GetRewardItem3Count());
+        WriteD(buf, instanceScore.GetRewardItem4());
+        WriteD(buf, instanceScore.GetRewardItem4Count());
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Network/Aion/Instanceinfo/TheShugoEmperorsVaultScoreWriter.cs
@@ -0,0 +1,34 @@
+using Aion.GameServer.Commons.Nio;
+using Aion.GameServer.Model.Instance.Instancescore;
+
+namespace Aion.GameServer.Network.Aion.Instanceinfo;
+
+/// <summary>
+/// Java parity: network/aion/instanceinfo/TheShugoEmperorsVaultScoreWriter (Yeats) : InstanceScoreWriter&lt;NormalScore&gt;. Writes
+/// points/AP/rank + 4 reward item/count pairs. NormalScore red-tolerated.
+/// </summary>
+public class TheShugoEmperorsVaultScoreWriter : InstanceScoreWriter<NormalScore>
+{
+    public TheShugoEmperorsVaultScoreWriter(NormalScore reward)
+        : base(reward)
+    {
+    }
+
+    protected override void WriteMe(ByteBuffer buf)
+    {
+        WriteD(buf, instanceScore.GetPoints());
+        WriteD(buf, instanceScore.GetBasicAp());
+        WriteD(buf, 0);
+        WriteD(buf, instanceScore.GetRank());
+        WriteD(buf, instanceScore.GetFinalAp());
+        WriteD(buf, 0);
+        WriteD(buf, instanceScore.GetRewardItem1());
+        WriteD(buf, instanceScore.GetRewardItem1Count());
+        WriteD(buf, instanceScore.GetRewardItem2());
+        WriteD(buf, instanceScore.GetRewardItem2Count());
+        WriteD(buf, instanceScore.GetRewardItem3());
+        WriteD(buf, instanceScore.GetRewardItem3Count());
+        WriteD(buf, instanceScore.GetRewardItem4());
+        WriteD(buf, instanceScore.GetRewardItem4Count());
+    }
+}
