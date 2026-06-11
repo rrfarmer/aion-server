eb6d155ad9005a79b5284f819d510e6a9da7e756
rrfarmer <ryanfarmer@mac.com>
2026-06-10 20:42:19 -0400

    Port alliance/events ChangeMemberGroupEvent + CheckAllianceReadyEvent

---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Alliance/Events/ChangeMemberGroupEvent.cs
@@ -0,0 +1,66 @@
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Model.Team.Alliance;
+using Aion.GameServer.Model.Team.Common.Events;
+using Aion.GameServer.Network.Aion.Serverpackets;
+
+namespace Aion.GameServer.Model.Team.Alliance.Events;
+
+/// <summary>
+/// Java parity: model/team/alliance/events/ChangeMemberGroupEvent (ATracer) : AlwaysTrueTeamEvent. Swaps two members between alliance
+/// groups, or moves one to a target group. PlayerAllianceEvent.MEMBER_GROUP_CHANGE -> MemberGroupChange; alliance.sendPackets(varargs)
+/// -> SendPackets(params). PlayerAllianceGroup/SM_ALLIANCE_MEMBER_INFO red-tolerated.
+/// </summary>
+public class ChangeMemberGroupEvent : AlwaysTrueTeamEvent
+{
+    private readonly PlayerAlliance alliance;
+    private readonly int firstMemberId;
+    private readonly int secondMemberId;
+    private readonly int allianceGroupId;
+
+    public ChangeMemberGroupEvent(PlayerAlliance alliance, int firstMemberId, int secondMemberId, int allianceGroupId)
+    {
+        this.alliance = alliance;
+        this.firstMemberId = firstMemberId;
+        this.secondMemberId = secondMemberId;
+        this.allianceGroupId = allianceGroupId;
+    }
+
+    public override void HandleEvent()
+    {
+        PlayerAllianceMember firstMember = alliance.GetMember(firstMemberId);
+        if (firstMember == null) // probably left or got kicked right before handleEvent() was called
+            return;
+        if (secondMemberId != 0)
+        {
+            PlayerAllianceMember secondMember = alliance.GetMember(secondMemberId);
+            if (secondMember == null) // probably left or got kicked right before handleEvent() was called
+                return;
+            SwapMembersInGroup(firstMember, secondMember);
+        }
+        else
+        {
+            MoveMemberToGroup(firstMember, allianceGroupId);
+        }
+    }
+
+    private void SwapMembersInGroup(PlayerAllianceMember firstMember, PlayerAllianceMember secondMember)
+    {
+        PlayerAllianceGroup firstAllianceGroup = firstMember.GetPlayerAllianceGroup();
+        PlayerAllianceGroup secondAllianceGroup = secondMember.GetPlayerAllianceGroup();
+        firstAllianceGroup.RemoveMember(firstMember);
+        secondAllianceGroup.RemoveMember(secondMember);
+        firstAllianceGroup.AddMember(secondMember);
+        secondAllianceGroup.AddMember(firstMember);
+        alliance.SendPackets(new SM_ALLIANCE_MEMBER_INFO(firstMember, PlayerAllianceEvent.MemberGroupChange),
+            new SM_ALLIANCE_MEMBER_INFO(secondMember, PlayerAllianceEvent.MemberGroupChange));
+    }
+
+    private void MoveMemberToGroup(PlayerAllianceMember firstMember, int allianceGroupId)
+    {
+        PlayerAllianceGroup firstAllianceGroup = firstMember.GetPlayerAllianceGroup();
+        firstAllianceGroup.RemoveMember(firstMember);
+        PlayerAllianceGroup newAllianceGroup = alliance.GetAllianceGroup(allianceGroupId);
+        newAllianceGroup.AddMember(firstMember);
+        alliance.SendPackets(new SM_ALLIANCE_MEMBER_INFO(firstMember, PlayerAllianceEvent.MemberGroupChange));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Alliance/Events/CheckAllianceReadyEvent.cs
@@ -0,0 +1,77 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Alliance;
+using Aion.GameServer.Model.Team.Common.Events;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Model.Team.Alliance.Events;
+
+/// <summary>
+/// Java parity: model/team/alliance/events/CheckAllianceReadyEvent (ATracer) : AlwaysTrueTeamEvent. Drives the alliance ready-check
+/// state machine and broadcasts SM_ALLIANCE_READY_CHECK per member. TeamCommand SCREAMING (ALLIANCE_CHECKREADY_*). SM_* red-tolerated.
+/// </summary>
+public class CheckAllianceReadyEvent : AlwaysTrueTeamEvent
+{
+    private readonly PlayerAlliance alliance;
+    private readonly Player player;
+    private readonly TeamCommand eventCode;
+
+    public CheckAllianceReadyEvent(PlayerAlliance alliance, Player player, TeamCommand eventCode)
+    {
+        this.alliance = alliance;
+        this.player = player;
+        this.eventCode = eventCode;
+    }
+
+    public override void HandleEvent()
+    {
+        int readyStatus = alliance.GetAllianceReadyStatus();
+        switch (eventCode)
+        {
+            case TeamCommand.ALLIANCE_CHECKREADY_CANCEL:
+                readyStatus = 0;
+                break;
+            case TeamCommand.ALLIANCE_CHECKREADY_START:
+                readyStatus = alliance.GetOnlineMembers().Count - 1;
+                break;
+            case TeamCommand.ALLIANCE_CHECKREADY_AUTOCANCEL:
+                readyStatus = 0;
+                break;
+            case TeamCommand.ALLIANCE_CHECKREADY_READY:
+            case TeamCommand.ALLIANCE_CHECKREADY_NOTREADY:
+                readyStatus -= 1;
+                break;
+        }
+        alliance.SetAllianceReadyStatus(readyStatus);
+        alliance.ForEach(member =>
+        {
+            switch (eventCode)
+            {
+                case TeamCommand.ALLIANCE_CHECKREADY_CANCEL:
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 0));
+                    break;
+                case TeamCommand.ALLIANCE_CHECKREADY_START:
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 5));
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 1));
+                    break;
+                case TeamCommand.ALLIANCE_CHECKREADY_AUTOCANCEL:
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 2));
+                    break;
+                case TeamCommand.ALLIANCE_CHECKREADY_READY:
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 5));
+                    if (alliance.GetAllianceReadyStatus() == 0)
+                    {
+                        PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(0, 3));
+                    }
+                    break;
+                case TeamCommand.ALLIANCE_CHECKREADY_NOTREADY:
+                    PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(player.GetObjectId(), 4));
+                    if (alliance.GetAllianceReadyStatus() == 0)
+                    {
+                        PacketSendUtility.SendPacket(member, new SM_ALLIANCE_READY_CHECK(0, 3));
+                    }
+                    break;
+            }
+        });
+    }
+}
