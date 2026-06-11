19d643f66d2cf2684977fa844b6ff6aaa28352b2
rrfarmer <ryanfarmer@mac.com>
2026-06-10 20:33:10 -0400

    Port group/events PlayerConnected + PlayerDisconnected (completes group/events)

---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Group/Events/PlayerConnectedEvent.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Common.Events;
+using Aion.GameServer.Model.Team.Common.Legacy;
+using Aion.GameServer.Model.Team.Group;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Model.Team.Group.Events;
+
+/// <summary>
+/// Java parity: model/team/group/events/PlayerConnectedEvent (ATracer) : AlwaysTrueTeamEvent. Re-adds a reconnecting member, restores
+/// leadership if needed, rebroadcasts group/member info. slf4j {} -> string concat. PlayerGroupMember/SM_* red-tolerated.
+/// </summary>
+public class PlayerConnectedEvent : AlwaysTrueTeamEvent
+{
+    private static readonly ILogger log = NullLoggerFactory.Instance.CreateLogger(nameof(PlayerConnectedEvent));
+    private readonly PlayerGroup group;
+    private readonly Player player;
+
+    public PlayerConnectedEvent(PlayerGroup group, Player player)
+    {
+        this.group = group;
+        this.player = player;
+    }
+
+    public override void HandleEvent()
+    {
+        group.RemoveMember(player.GetObjectId());
+        group.AddMember(new PlayerGroupMember(player));
+        if (player.Equals(group.GetLeader().GetObject()))
+        {
+            log.LogWarning("[TEAM] leader (" + player + ") reconnected, but should have lost leadership on disconnect");
+            group.ChangeLeader(new PlayerGroupMember(player));
+        }
+        PacketSendUtility.SendPacket(player, new SM_GROUP_INFO(group));
+        PacketSendUtility.SendPacket(player, new SM_GROUP_MEMBER_INFO(group, player, GroupEvent.JOIN));
+        group.ForEach(member =>
+        {
+            if (!player.Equals(member))
+            {
+                PacketSendUtility.SendPacket(member, new SM_GROUP_MEMBER_INFO(group, player, GroupEvent.ENTER));
+                PacketSendUtility.SendPacket(player, new SM_GROUP_MEMBER_INFO(group, member, GroupEvent.ENTER));
+            }
+        });
+        // change leader to player logging in first if all players are disconnected
+        if (group.GetLeader() != null && !group.HasMember(group.GetLeader().GetObjectId()))
+            group.OnEvent(new ChangeGroupLeaderEvent(group, player));
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Group/Events/PlayerDisconnectedEvent.cs
@@ -0,0 +1,55 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team;
+using Aion.GameServer.Model.Team.Common.Legacy;
+using Aion.GameServer.Model.Team.Group;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Model.Team.Group.Events;
+
+/// <summary>
+/// Java parity: model/team/group/events/PlayerDisconnectedEvent (ATracer) : ITeamEvent. Disbands if no online members remain,
+/// otherwise transfers leadership and broadcasts offline/disconnected member info. PlayerGroupService/SM_SYSTEM_MESSAGE.STR_* red-tolerated.
+/// </summary>
+public class PlayerDisconnectedEvent : ITeamEvent
+{
+    private readonly PlayerGroup group;
+    private readonly Player player;
+
+    public PlayerDisconnectedEvent(PlayerGroup group, Player player)
+    {
+        this.group = group;
+        this.player = player;
+    }
+
+    /// <summary>Player should be in group before disconnection</summary>
+    public bool CheckCondition()
+    {
+        return group.HasMember(player.GetObjectId());
+    }
+
+    public void HandleEvent()
+    {
+        if (group.GetOnlineMembers().Count == 0)
+        {
+            PlayerGroupService.Disband(group);
+        }
+        else
+        {
+            if (player.Equals(group.GetLeader().GetObject()))
+            {
+                group.OnEvent(new ChangeGroupLeaderEvent(group));
+            }
+            group.ForEach(member =>
+            {
+                if (!member.Equals(player))
+                {
+                    PacketSendUtility.SendPacket(member, SM_SYSTEM_MESSAGE.STR_PARTY_HE_BECOME_OFFLINE(player.GetName()));
+                    PacketSendUtility.SendPacket(member, new SM_GROUP_MEMBER_INFO(group, player, GroupEvent.DISCONNECTED));
+                    // disconnect other group members on logout? check
+                    PacketSendUtility.SendPacket(player, new SM_GROUP_MEMBER_INFO(group, member, GroupEvent.DISCONNECTED));
+                }
+            });
+        }
+    }
+}
