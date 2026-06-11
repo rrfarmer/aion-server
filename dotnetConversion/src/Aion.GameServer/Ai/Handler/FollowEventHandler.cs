39cf94615f5981ed7d4259fd88de8cbdc84edd28
rrfarmer <ryanfarmer@mac.com>
2026-06-10 23:57:03 -0400

    Port ai/handler/FollowEventHandler

---
+++ dotnetConversion/src/Aion.GameServer/Ai/Handler/FollowEventHandler.cs
@@ -0,0 +1,64 @@
+using Aion.GameServer.Ai;
+using Aion.GameServer.Ai.Event;
+using Aion.GameServer.Ai.Manager;
+using Aion.GameServer.Model.GameObjects;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Ai.Handler;
+
+/// <summary>
+/// Java parity: ai/handler/FollowEventHandler (ATracer). Follow/creatureMoved/stopFollow dispatch. AIState.FOLLOWING/IDLE -> Following/
+/// Idle; AIEventType.TARGET_TOOFAR -> AiEventType.TargetToofar; isInRange(AbstractAI&lt;? extends Creature&gt;) -> generic method
+/// IsInRange&lt;T&gt;(AbstractAI&lt;T&gt;) where T : Creature (NpcAI=AbstractAI&lt;Npc&gt; can't satisfy an invariant AbstractAI&lt;Creature&gt; param).
+/// EmoteManager/AIActions red-tolerated where unported.
+/// </summary>
+public class FollowEventHandler
+{
+    public static void Follow(NpcAI npcAI, Creature creature)
+    {
+        if (npcAI.SetStateIfNot(AiState.Following))
+        {
+            npcAI.GetOwner().SetTarget(creature);
+            EmoteManager.EmoteStartFollowing(npcAI.GetOwner());
+        }
+    }
+
+    public static void CreatureMoved(NpcAI npcAI, Creature creature)
+    {
+        if (npcAI.IsInState(AiState.Following))
+        {
+            if (npcAI.GetOwner().IsTargeting(creature.GetObjectId()) && !creature.IsDead())
+            {
+                CheckFollowTarget(npcAI, creature);
+            }
+        }
+    }
+
+    public static void CheckFollowTarget(NpcAI npcAI, Creature creature)
+    {
+        if (!IsInRange(npcAI, creature))
+        {
+            npcAI.OnGeneralEvent(AiEventType.TargetToofar);
+        }
+    }
+
+    public static bool IsInRange<T>(AbstractAI<T> ai, VisibleObject obj) where T : Creature
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        return PositionUtil.IsInRange(ai.GetOwner(), obj, 2, false);
+    }
+
+    public static void StopFollow(NpcAI npcAI, Creature creature)
+    {
+        if (npcAI.SetStateIfNot(AiState.Idle))
+        {
+            npcAI.GetOwner().SetTarget(null);
+            npcAI.GetOwner().GetMoveController().AbortMove();
+            AIActions.ScheduleRespawn(npcAI);
+            AIActions.DeleteOwner(npcAI);
+        }
+    }
+}
