eaeb88eca320ab44d3d6f03b01e219f01a7d14ec
rrfarmer <ryanfarmer@mac.com>
2026-06-10 20:30:42 -0400

    Port group/events PlayerGroupEnteredEvent + PlayerStartMentoringEvent

---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Group/Events/PlayerGroupEnteredEvent.cs
@@ -0,0 +1,41 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Common.Events;
+using Aion.GameServer.Model.Team.Common.Legacy;
+using Aion.GameServer.Model.Team.Group;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+
+namespace Aion.GameServer.Model.Team.Group.Events;
+
+/// <summary>
+/// Java parity: model/team/group/events/PlayerGroupEnteredEvent (ATracer) : PlayerEnteredEvent&lt;PlayerGroup&gt;. Adds player to group
+/// and broadcasts join/enter member-info + system messages. team/player are protected base fields. PlayerGroupService/SM_* red-tolerated.
+/// </summary>
+public class PlayerGroupEnteredEvent : PlayerEnteredEvent<PlayerGroup>
+{
+    public PlayerGroupEnteredEvent(PlayerGroup group, Player player)
+        : base(group, player)
+    {
+    }
+
+    public override void HandleEvent()
+    {
+        PlayerGroupService.AddPlayerToGroup(team, player);
+        PacketSendUtility.SendPacket(player, new SM_GROUP_INFO(team));
+        PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_PARTY_ENTERED_PARTY());
+        PacketSendUtility.SendPacket(player, new SM_GROUP_MEMBER_INFO(team, player, GroupEvent.JOIN));
+        team.SendBrands(player);
+        team.ForEach(member =>
+        {
+            if (!member.Equals(player))
+            {
+                // TODO probably here JOIN event
+                PacketSendUtility.SendPacket(member, new SM_GROUP_MEMBER_INFO(team, player, GroupEvent.ENTER));
+                PacketSendUtility.SendPacket(member, SM_SYSTEM_MESSAGE.STR_PARTY_HE_ENTERED_PARTY(player.GetName()));
+                PacketSendUtility.SendPacket(player, new SM_GROUP_MEMBER_INFO(team, member, GroupEvent.ENTER));
+            }
+        });
+        PacketSendUtility.BroadcastPacket(player, new SM_ABYSS_RANK_UPDATE(1, player), true);
+        base.HandleEvent();
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Model/Team/Group/Events/PlayerStartMentoringEvent.cs
@@ -0,0 +1,49 @@
+using Aion.GameServer.Model.GameObjects.Player;
+using Aion.GameServer.Model.Team.Common.Events;
+using Aion.GameServer.Model.Team.Common.Legacy;
+using Aion.GameServer.Model.Team.Group;
+using Aion.GameServer.Network.Aion.Serverpackets;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.Audit;
+using Aion.GameServer.Utils.Collections;
+
+namespace Aion.GameServer.Model.Team.Group.Events;
+
+/// <summary>
+/// Java parity: model/team/group/events/PlayerStartMentoringEvent (ATracer) : AlwaysTrueTeamEvent. Java `implements Consumer&lt;Player&gt;`
+/// -> C# plain Accept(Player) method passed to ForEach as a method group (group.forEach(this) -> group.ForEach(Accept)). Predicates.Players
+/// .canBeMentoredBy / AuditLogger / SM_* red-tolerated.
+/// </summary>
+public class PlayerStartMentoringEvent : AlwaysTrueTeamEvent
+{
+    private readonly PlayerGroup group;
+    private readonly Player player;
+
+    public PlayerStartMentoringEvent(PlayerGroup group, Player player)
+    {
+        this.group = group;
+        this.player = player;
+    }
+
+    public override void HandleEvent()
+    {
+        if (group.FilterMembers(Predicates.Players.CanBeMentoredBy(player)).Count == 0)
+        {
+            AuditLogger.Log(player, "sent fake start mentoring packet");
+            return;
+        }
+        player.SetMentor(true);
+        PacketSendUtility.SendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_MENTOR_START());
+        group.ForEach(Accept);
+        PacketSendUtility.BroadcastPacketAndReceive(player, new SM_ABYSS_RANK_UPDATE(2, player));
+    }
+
+    public void Accept(Player member)
+    {
+        if (!player.Equals(member))
+        {
+            PacketSendUtility.SendPacket(member, SM_SYSTEM_MESSAGE.STR_MSG_MENTOR_START_PARTYMSG(player.GetName()));
+        }
+        PacketSendUtility.SendPacket(member, new SM_GROUP_MEMBER_INFO(group, player, GroupEvent.MOVEMENT));
+    }
+}
