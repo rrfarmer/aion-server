ce069c4afd7524c0aca6cc43b79cb0cc27ded1df
rrfarmer <ryanfarmer@mac.com>
2026-06-13 13:49:16 -0400

    Player AccessLevel bridge: faithful (byte)playerAccount.GetAccessLevel() (vein-V; byte-cast to match SmPlayerAuth byte param + flight-access int comparison); clears 3 CS1061, zero residual (754->751)

--- dotnetConversion/src/Aion.GameServer/Model/GameObjects/Player.cs
+++ dotnetConversion/src/Aion.GameServer/Model/GameObjects/Player.cs
@@ -44,6 +44,7 @@ public partial class Player : Creature
     public Aion.GameServer.Model.GameObjects.Players.Npcfaction.NpcFactions NpcFactions { get => GetNpcFactions(); set => SetNpcFactions(value); }
     public byte AccountMembership => (byte)playerAccount.GetMembership();
     public Aion.GameServer.Model.Templates.Ride.RideInfo RideInfo { get => ride; set => ride = value; }
+    public byte AccessLevel => (byte)playerAccount.GetAccessLevel();
 
     public volatile Aion.GameServer.Model.Templates.Ride.RideInfo ride;
     public volatile Aion.GameServer.Model.GameObjects.Players.InRoll inRoll;
