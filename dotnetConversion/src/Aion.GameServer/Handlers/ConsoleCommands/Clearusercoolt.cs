443a330cb3319bff94944d094ec36e31cfcba753
rrfarmer <ryanfarmer@mac.com>
2026-06-14 17:58:15 -0400

    Content handlers: consolecommands batch 8 (Teleport/Clearusercoolt) 1:1
    Faithful 1:1: Teleport (GM map-coords teleport: WORLD_MAPS_DATA.GetWorldIdByCName + GeoService.GetZ
    for world-map-click z=10000 + InstanceService.GetOrRegisterInstance + TeleportService.TeleportTo),
    Clearusercoolt (clear a named player's portal/instance cooldowns: capture keys before nulling
    the map, then SendEntryInfo each). All deps verified. All 4 builds 0, golden 105/105.
    
    consolecommands 25/35. Remaining deferred (heavier/unported subsystems): Teleport_to_named,
    Attrbonus (IStatOwner), Set_enchantcount (enchant lifecycle), Addcskill/Combineskill/Deleteskill
    (skill learn + JAXB xml), Wish (245L), Skill/Guild (SplitList framework), Teleportto (admincommand GoTo).

---
+++ dotnetConversion/src/Aion.GameServer/Handlers/ConsoleCommands/Clearusercoolt.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using Aion.GameServer.Model.GameObjects.Players;
+using Aion.GameServer.Network.Aion.ServerPackets;
+using Aion.GameServer.Utils;
+using Aion.GameServer.Utils.ChatHandlers;
+
+namespace Aion.GameServer.Handlers.ConsoleCommands;
+
+/// <summary>Java parity: data/handlers/consolecommands/Clearusercoolt (ginho1). Clears instance (portal) cooldowns for a named player.</summary>
+public class Clearusercoolt : ConsoleCommand
+{
+    public Clearusercoolt()
+        : base("clearusercoolt", "Clears cooldowns for instances.")
+    {
+    }
+
+    protected override void Execute(Player admin, params string[] paramsArr)
+    {
+        string playerName = ChatUtil.GetRealCharName(paramsArr[0], true);
+        Player player = Aion.GameServer.World.World.GetInstance().GetPlayer(playerName);
+        if (player == null)
+        {
+            PacketSendUtility.SendPacket(admin, SM_SYSTEM_MESSAGE.STR_NO_SUCH_USER(playerName));
+            return;
+        }
+        ClearAllInstanceCooldowns(admin, player);
+    }
+
+    public static void ClearAllInstanceCooldowns(Player admin, Player player)
+    {
+        if (player.GetPortalCooldownList().GetPortalCoolDowns() == null)
+        {
+            PacketSendUtility.SendMessage(admin, (player.Equals(admin) ? "You have" : player.GetName() + " has") + " no instance cooldowns to remove.");
+            return;
+        }
+
+        List<int> worldIds = player.GetPortalCooldownList().GetPortalCoolDowns().Keys.ToList();
+        player.GetPortalCooldownList().SetPortalCoolDowns(null);
+        foreach (int worldId in worldIds)
+            player.GetPortalCooldownList().SendEntryInfo(worldId);
+
+        if (player.Equals(admin))
+        {
+            PacketSendUtility.SendMessage(admin, "Your instance cooldowns were removed.");
+        }
+        else
+        {
+            PacketSendUtility.SendMessage(admin, "You have removed instance cooldowns of " + player.GetName() + '.');
+            PacketSendUtility.SendMessage(player, admin.GetName(true) + " removed your instance cooldowns.");
+        }
+    }
+}
---
+++ dotnetConversion/src/Aion.GameServer/Handlers/ConsoleCommands/Teleport.cs
@@ -0,0 +1,51 @@
+using Aion.GameServer.Dataholders;
+using Aion.GameServer.Model.GameObjects.Players;
+using Aion.GameServer.Services.Instance;
+using Aion.GameServer.Services.Teleport;
+using Aion.GameServer.Utils.ChatHandlers;
+using Aion.GameServer.World;
+using Aion.GameServer.World.Geo;
+
+namespace Aion.GameServer.Handlers.ConsoleCommands;
+
+/// <summary>Java parity: data/handlers/consolecommands/Teleport (Yeats). Moves the player to map coordinates (GM dialog / world-map click).</summary>
+public class Teleport : ConsoleCommand
+{
+    public Teleport()
+        : base("teleport", "Moves you to any location.")
+    {
+        SetSyntaxInfo("[mapCName] <x> <y> <z> - Moves you to the specified coordinates on the given map (default: current map).");
+    }
+
+    protected override void Execute(Player player, params string[] paramsArr)
+    {
+        if (paramsArr.Length < 3)
+        {
+            SendInfo(player);
+            return;
+        }
+        int i = 0;
+        int worldId = paramsArr.Length == 3 ? player.GetWorldId() : DataManager.WORLD_MAPS_DATA.GetWorldIdByCName(paramsArr[i++]);
+        if (worldId != 0)
+        {
+            int x = int.Parse(paramsArr[i++]);
+            int y = int.Parse(paramsArr[i++]);
+            int inputZ = int.Parse(paramsArr[i++]);
+            float z = inputZ;
+            WorldMapInstance instance = GetOrCreateWorldMapInstance(player, worldId);
+            if (inputZ == 10_000) // default value in client when selecting a point on the world map
+                z = GeoService.GetInstance().GetZ(worldId, x, y, 4000f, 0f, instance.GetInstanceId());
+            TeleportService.TeleportTo(player, instance, x, y, z);
+        }
+    }
+
+    private WorldMapInstance GetOrCreateWorldMapInstance(Player player, int worldId)
+    {
+        if (player.GetWorldId() == worldId)
+            return player.GetPosition().GetWorldMapInstance();
+        else if (Aion.GameServer.World.World.GetInstance().GetWorldMap(worldId).IsInstanceType())
+            return InstanceService.GetOrRegisterInstance(worldId, player);
+        else
+            return Aion.GameServer.World.World.GetInstance().GetWorldMap(worldId).GetMainWorldMapInstance();
+    }
+}
